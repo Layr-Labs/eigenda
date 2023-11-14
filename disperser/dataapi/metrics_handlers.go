09adeb3fb799009d66a9df2ce6dee955f1ae0449
Jian Xiao <iamjianxiao@gmail.com>
2023-11-14 18:08:51 +0000

    fix

--- disperser/dataapi/metrics_handlers.go
+++ disperser/dataapi/metrics_handlers.go
@@ -97,9 +97,7 @@ func (s *server) calculateTotalStake(operators []*Operator, blockNumber uint32) 
 			totalStake := big.NewInt(0)
 			s.logger.Debug("Operator state:", "operatorId", operatorId, "num quorums", len(operatorState.Totals))
 			for quorumId, total := range operatorState.Totals {
-				stake := big.NewInt(0)
-				stake.Add(stake, total.Stake)
-				s.logger.Debug("Operator stake:", "operatorId", operatorId, "quorum", quorumId, "stake", stake.Int64())
+				s.logger.Debug("Operator stake:", "operatorId", operatorId, "quorum", quorumId, "stake", (*total.Stake).Int64())
 				totalStake.Add(totalStake, total.Stake)
 			}
 			totalStakeByOperatorChan <- totalStake
