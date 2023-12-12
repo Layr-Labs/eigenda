e9e7096e5f58c49089a25138d55a360bdc22b71d
Jian Xiao <iamjianxiao@gmail.com>
2023-12-12 00:38:19 +0000

    polish message

--- churner/churner.go
+++ churner/churner.go
@@ -221,19 +221,19 @@ func (c *churner) getOperatorsToChurn(ctx context.Context, quorumIDs []uint8, op
 		// register needs to have 1.1 times the stake of the lowest-stake operator.
 		if new(big.Int).Mul(lowestStake, churnBIPsOfOperatorStake).Cmp(new(big.Int).Mul(operatorToRegisterStake, bipMultiplier)) >= 0 {
 			c.metrics.IncrementFailedRequestNum("getOperatorsToChurn", FailReasonInsufficientStakeToRegister)
-			return nil, fmt.Errorf("registering operator must have more than %f times of the stake of the lowest-stake operator", float64(operatorSetParams.ChurnBIPsOfOperatorStake)/10000.0)
+			return nil, fmt.Errorf("registering operator must have %f%% more than the stake of the lowest-stake operator", float64(operatorSetParams.ChurnBIPsOfOperatorStake)/100.0-100.0)
 		}
 
 		// verify the lowest stake against the total stake
 		// make sure that: lowestStake * bipMultiplier < totalStake * churnBIPsOfTotalStake
-		// This for the lowest-stake operator to be churned out, it must have less than
+		// For the lowest-stake operator to be churned out, it must have less than
 		// churnBIPsOfTotalStake/10000 of the total stake.
 		// For example, when churnBIPsOfTotalStake=1001, the operator to be churned out
 		// (i.e. the lowest-stake operator) needs to have less than 10.01% of the total
 		// stake.
 		if new(big.Int).Mul(lowestStake, bipMultiplier).Cmp(new(big.Int).Mul(totalStake, churnBIPsOfTotalStake)) >= 0 {
 			c.metrics.IncrementFailedRequestNum("getOperatorsToChurn", FailReasonInsufficientStakeToChurn)
-			return nil, fmt.Errorf("operator to churn out must have less than %f percentage of the total stake", float64(operatorSetParams.ChurnBIPsOfTotalStake)/100.0)
+			return nil, fmt.Errorf("operator to churn out must have less than %f%% of the total stake", float64(operatorSetParams.ChurnBIPsOfTotalStake)/100.0)
 		}
 
 		operatorToChurnAddress, err := c.Transactor.OperatorIDToAddress(ctx, lowestStakeOperatorId)
