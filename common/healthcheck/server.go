197f266df2da6e689d595e72b8601c15a9528421
Daniel Mancia <21249320+dmanc@users.noreply.github.com>
2023-12-26 18:09:48 -0600

    Set serving status before registering

--- common/healthcheck/server.go
+++ common/healthcheck/server.go
@@ -6,10 +6,10 @@ import (
 	"google.golang.org/grpc/health/grpc_health_v1"
 )
 
-// RegisterHealthServer registers the generic gRPC health check server implementation
+// RegisterHealthServer registers the default gRPC health check server implementation
 // with the given gRPC server.
 func RegisterHealthServer(name string, server *grpc.Server) {
 	healthServer := health.NewServer()
-	grpc_health_v1.RegisterHealthServer(server, health.NewServer())
 	healthServer.SetServingStatus(name, grpc_health_v1.HealthCheckResponse_SERVING)
+	grpc_health_v1.RegisterHealthServer(server, health.NewServer())
 }
