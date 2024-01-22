639f6f6d0d3503157c371c40103f88fd0095d6fe
Daniel Mancia <21249320+dmanc@users.noreply.github.com>
2024-01-22 13:27:55 -0800

    Expand TestQueryIndexCount to query the count of two statuses

--- common/aws/dynamodb/client_test.go
+++ common/aws/dynamodb/client_test.go
@@ -306,10 +306,10 @@ func TestQueryIndexCount(t *testing.T) {
 	indexName := "StatusIndex"
 
 	ctx := context.Background()
-	numItems := 30
-	items := make([]commondynamodb.Item, numItems)
-	for i := 0; i < numItems; i += 1 {
-		items[i] = commondynamodb.Item{
+	numItemsProcessing := 10
+	items1 := make([]commondynamodb.Item, numItemsProcessing)
+	for i := 0; i < numItemsProcessing; i += 1 {
+		items1[i] = commondynamodb.Item{
 			"MetadataKey": &types.AttributeValueMemberS{Value: fmt.Sprintf("key%d", i)},
 			"BlobKey":     &types.AttributeValueMemberS{Value: fmt.Sprintf("blob%d", i)},
 			"BlobSize":    &types.AttributeValueMemberN{Value: "123"},
@@ -317,7 +317,24 @@ func TestQueryIndexCount(t *testing.T) {
 			"RequestedAt": &types.AttributeValueMemberN{Value: strconv.FormatInt(time.Now().Unix(), 10)},
 		}
 	}
-	unprocessed, err := dynamoClient.PutItems(ctx, tableName, items)
+
+	numItemsConfirmed := 20
+	items2 := make([]commondynamodb.Item, numItemsConfirmed)
+	for i := 0; i < numItemsConfirmed; i += 1 {
+		items2[i] = commondynamodb.Item{
+			"MetadataKey": &types.AttributeValueMemberS{Value: fmt.Sprintf("key%d", i+numItemsProcessing)},
+			"BlobKey":     &types.AttributeValueMemberS{Value: fmt.Sprintf("blob%d", i+numItemsProcessing)},
+			"BlobSize":    &types.AttributeValueMemberN{Value: "123"},
+			"BlobStatus":  &types.AttributeValueMemberN{Value: "1"},
+			"RequestedAt": &types.AttributeValueMemberN{Value: strconv.FormatInt(time.Now().Unix(), 10)},
+		}
+	}
+
+	unprocessed, err := dynamoClient.PutItems(ctx, tableName, items1)
+	assert.NoError(t, err)
+	assert.Len(t, unprocessed, 0)
+
+	unprocessed, err = dynamoClient.PutItems(ctx, tableName, items2)
 	assert.NoError(t, err)
 	assert.Len(t, unprocessed, 0)
 
@@ -326,7 +343,14 @@ func TestQueryIndexCount(t *testing.T) {
 			Value: "0",
 		}})
 	assert.NoError(t, err)
-	assert.Equal(t, int(count), 30)
+	assert.Equal(t, int(count), 10)
+
+	count, err = dynamoClient.QueryIndexCount(ctx, tableName, indexName, "BlobStatus = :status", commondynamodb.ExpresseionValues{
+		":status": &types.AttributeValueMemberN{
+			Value: "1",
+		}})
+	assert.NoError(t, err)
+	assert.Equal(t, int(count), 20)
 }
 
 func TestQueryIndexPaginationSingleItem(t *testing.T) {
