0375fd0b1634854e7f4695619f7781ada15de4d9
Jian Xiao <iamjianxiao@gmail.com>
2024-01-24 19:21:38 +0000

    Validate the time intervals and fix the mix uses of nanosecond and second

--- disperser/dataapi/subgraph_client.go
+++ disperser/dataapi/subgraph_client.go
@@ -2,6 +2,7 @@ package dataapi
 
 import (
 	"context"
+	"fmt"
 	"sort"
 	"strconv"
 	"sync"
@@ -148,7 +149,7 @@ func (sc *subgraphClient) QueryNumBatchesByOperatorsInThePastBlockTimestamp(ctx 
 		return nil, err
 	}
 
-	intervalEvents, err := getOperatorsWithRegisteredDeregisteredIntervalEvents(ctx, registeredOperators, deregisteredOperators, blockTimestamp, nonSigners)
+	intervalEvents, err := sc.getOperatorsWithRegisteredDeregisteredIntervalEvents(ctx, registeredOperators, deregisteredOperators, blockTimestamp, nonSigners)
 	if err != nil {
 		return nil, err
 	}
@@ -159,7 +160,7 @@ func (sc *subgraphClient) QueryNumBatchesByOperatorsInThePastBlockTimestamp(ctx 
 		intervalEventsPool   = workerpool.New(maxWorkerPoolSize)
 		currentTs            = uint64(time.Now().Unix())
 	)
-	for ie := range intervalEvents {
+	for _, ie := range intervalEvents {
 		interval := ie
 		intervalEventsPool.Submit(func() {
 			end := interval.Events[1]
@@ -220,13 +221,13 @@ func (sc *subgraphClient) QueryIndexedDeregisteredOperatorsInTheLast14Days(ctx c
 	}, nil
 }
 
-func getOperatorsWithRegisteredDeregisteredIntervalEvents(
+func (sc *subgraphClient) getOperatorsWithRegisteredDeregisteredIntervalEvents(
 	ctx context.Context,
 	registeredOperators []*subgraph.Operator,
 	deregisteredOperators []*subgraph.Operator,
 	blockTimestamp uint64,
 	nonSigners map[string]int,
-) (chan OperatorEvents, error) {
+) ([]OperatorEvents, error) {
 	sort.SliceStable(registeredOperators, func(i, j int) bool {
 		return registeredOperators[i].BlockTimestamp < registeredOperators[j].BlockTimestamp
 	})
@@ -270,81 +271,99 @@ func getOperatorsWithRegisteredDeregisteredIntervalEvents(
 	}
 
 	currentTs := uint64(time.Now().Unix())
-	operatorEventsChan := make(chan OperatorEvents, 1)
-	go func() {
-		defer close(operatorEventsChan)
+	events := make([]OperatorEvents, 0)
 
-		// For the time window [blockTimestamp, now], compute the sub intervals during
-		// which the operator is live in EigenDA network for validating batches.
-		for operatorId := range nonSigners {
-			reg := operators[operatorId][0]
-			dereg := operators[operatorId][1]
-			if len(reg) == 0 && len(dereg) == 0 {
-				// The operator has no registration/deregistration events: it's live
-				// for the entire time window.
-				operatorEventsChan <- OperatorEvents{
-					OperatorId: operatorId,
-					Events:     []uint64{blockTimestamp, currentTs},
-				}
-			} else if len(reg) == 0 {
-				// The operator has only deregistration event: it's live from the beginning
-				// of the time window until the deregistration.
-				operatorEventsChan <- OperatorEvents{
-					OperatorId: operatorId,
-					Events:     []uint64{blockTimestamp, dereg[0]},
-				}
-			} else if len(dereg) == 0 {
-				// The operator has only registration event: it's live from registration to
-				// the end of the time window.
-				operatorEventsChan <- OperatorEvents{
-					OperatorId: operatorId,
-					Events:     []uint64{reg[0], currentTs},
+	// For the time window [blockTimestamp, now], compute the sub intervals during
+	// which the operator is live in EigenDA network for validating batches.
+	for operatorId := range nonSigners {
+		reg := operators[operatorId][0]
+		dereg := operators[operatorId][1]
+
+		// In EigenDA, the registration and deregistration events on timeline for an
+		// operator will be like reg-dereg-reg-dereg...
+		//
+		// The reason is that registering an operator that's already registered will fail
+		// and deregistering an operator that's already deregistered will also fail. So
+		// the registeration and deregistration will alternate.
+		if len(reg)-len(dereg) > 1 || len(dereg)-len(reg) > 1 {
+			return nil, fmt.Errorf("The number of registration and deregistration events cannot differ by more than one, num registration events: %d, num deregistration events: %d, operatorId: %s", len(reg), len(dereg), operatorId)
+		}
+
+		if len(reg) == 0 && len(dereg) == 0 {
+			// The operator has no registration/deregistration events: it's live
+			// for the entire time window.
+			events = append(events, OperatorEvents{
+				OperatorId: operatorId,
+				Events:     []uint64{blockTimestamp, currentTs},
+			})
+		} else if len(reg) == 0 {
+			// The operator has only deregistration event: it's live from the beginning
+			// of the time window until the deregistration.
+			events = append(events, OperatorEvents{
+				OperatorId: operatorId,
+				Events:     []uint64{blockTimestamp, dereg[0]},
+			})
+		} else if len(dereg) == 0 {
+			// The operator has only registration event: it's live from registration to
+			// the end of the time window.
+			events = append(events, OperatorEvents{
+				OperatorId: operatorId,
+				Events:     []uint64{reg[0], currentTs},
+			})
+		} else {
+			// The operator has both registration and deregistration events in the time
+			// window.
+			if reg[0] < dereg[0] {
+				// The first event in the time window is registration. This means at
+				// the beginning (i.e. blockTimestamp) it's not live.
+				for i := 0; i < len(reg); i++ {
+					if i < len(dereg) {
+						events = append(events, OperatorEvents{
+							OperatorId: operatorId,
+							Events:     []uint64{reg[i], dereg[i]},
+						})
+					} else {
+						events = append(events, OperatorEvents{
+							OperatorId: operatorId,
+							Events:     []uint64{reg[i], currentTs},
+						})
+					}
 				}
 			} else {
-				// The operator has both registration and deregistration events in the time
-				// window.
-				if reg[0] < dereg[0] {
-					// The first event in the time window is registration. This means at
-					// the beginning (i.e. blockTimestamp) it's not live.
-					for i := 0; i < len(reg); i++ {
-						if i < len(dereg) {
-							operatorEventsChan <- OperatorEvents{
-								OperatorId: operatorId,
-								Events:     []uint64{reg[i], dereg[i]},
-							}
-						} else {
-							operatorEventsChan <- OperatorEvents{
-								OperatorId: operatorId,
-								Events:     []uint64{reg[i], currentTs},
-							}
-						}
-					}
-				} else {
-					// The first event in the time window is deregistration. This means at
-					// the beginning (i.e. blockTimestamp) it's live already.
-					operatorEventsChan <- OperatorEvents{
-						OperatorId: operatorId,
-						Events:     []uint64{blockTimestamp, dereg[0]},
-					}
-					for i := 0; i < len(reg); i++ {
-						if i+1 < len(dereg) {
-							operatorEventsChan <- OperatorEvents{
-								OperatorId: operatorId,
-								Events:     []uint64{reg[i], dereg[i+1]},
-							}
-						} else {
-							operatorEventsChan <- OperatorEvents{
-								OperatorId: operatorId,
-								Events:     []uint64{reg[i], currentTs},
-							}
-						}
+				// The first event in the time window is deregistration. This means at
+				// the beginning (i.e. blockTimestamp) it's live already.
+				events = append(events, OperatorEvents{
+					OperatorId: operatorId,
+					Events:     []uint64{blockTimestamp, dereg[0]},
+				})
+				for i := 0; i < len(reg); i++ {
+					if i+1 < len(dereg) {
+						events = append(events, OperatorEvents{
+							OperatorId: operatorId,
+							Events:     []uint64{reg[i], dereg[i+1]},
+						})
+					} else {
+						events = append(events, OperatorEvents{
+							OperatorId: operatorId,
+							Events:     []uint64{reg[i], currentTs},
+						})
 					}
 				}
 			}
 		}
-	}()
+	}
 
-	return operatorEventsChan, nil
+	// Validate the registration and deregistration events are in timeline order.
+	for i := 0; i < len(events); i++ {
+		if events[i].Events[0] > events[i].Events[1] {
+			return nil, fmt.Errorf("Registration timestamp should not be greater than deregistration or current timestamp for operatorId %s, registration timestamp: %d, deregistration or current timestamp: %d", events[i].OperatorId, events[i].Events[0], events[i].Events[1])
+		}
+		if i > 0 && events[i-1].OperatorId == events[i].OperatorId && events[i-1].Events[1] > events[i].Events[0] {
+			return nil, fmt.Errorf("Registration should not happen when the operator is already registered, but found two consecutive registrations at timestamp %d and %d for operatorId %s", events[i-1].Events[0], events[i].Events[0], events[i].OperatorId)
+		}
+	}
+
+	return events, nil
 }
 
 func convertBatches(subgraphBatches []*subgraph.Batches) ([]*Batch, error) {
@@ -371,7 +390,7 @@ func convertBatches(subgraphBatches []*subgraph.Batches) ([]*Batch, error) {
 			Id:              []byte(batch.Id),
 			BatchId:         batchId,
 			BatchHeaderHash: []byte(batch.BatchHeaderHash),
-			BlockTimestamp:  ConvertNanosecondToSecond(timestamp),
+			BlockTimestamp:  timestamp,
 			BlockNumber:     blockNum,
 			TxHash:          []byte(batch.TxHash),
 			GasFees:         gasFees,
@@ -414,7 +433,7 @@ func convertOperator(operator *subgraph.Operator) (*Operator, error) {
 		Id:              []byte(operator.Id),
 		OperatorId:      []byte(operator.OperatorId),
 		Operator:        []byte(operator.Operator),
-		BlockTimestamp:  ConvertNanosecondToSecond(timestamp),
+		BlockTimestamp:  timestamp,
 		BlockNumber:     blockNum,
 		TransactionHash: []byte(operator.TransactionHash),
 	}, nil
