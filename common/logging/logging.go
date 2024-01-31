8517344981c7af2173142d9d0b4e71fc6c8c2d12
Daniel Mancia <21249320+dmanc@users.noreply.github.com>
2024-01-30 18:35:32 -0800

    Update config so that it defaults to previous behavior

--- common/logging/logging.go
+++ common/logging/logging.go
@@ -20,6 +20,32 @@ func (l *Logger) SetHandler(h log.Handler) {
 	l.Logger.SetHandler(h)
 }
 
+func getStreamHandlerFromFormat(format string) (log.Handler, error) {
+	switch format {
+	case "terminal":
+		return log.StreamHandler(os.Stdout, log.TerminalFormat(false)), nil
+	case "json":
+		return log.StreamHandler(os.Stdout, log.JSONFormat()), nil
+	case "logfmt":
+		return log.StreamHandler(os.Stdout, log.LogfmtFormat()), nil
+	default:
+		return nil, fmt.Errorf("invalid log format: %s", format)
+	}
+}
+
+func getFileHandlerFromFormat(path string, format string) (log.Handler, error) {
+	switch format {
+	case "terminal":
+		return log.FileHandler(path, log.TerminalFormat(false))
+	case "json":
+		return log.FileHandler(path, log.JSONFormat())
+	case "logfmt":
+		return log.FileHandler(path, log.LogfmtFormat())
+	default:
+		return nil, fmt.Errorf("invalid log format: %s", format)
+	}
+}
+
 // GetLogger returns a logger with the specified configuration.
 func GetLogger(cfg Config) (common.Logger, error) {
 	fileLevel, err := log.LvlFromString(cfg.FileLevel)
@@ -32,10 +58,19 @@ func GetLogger(cfg Config) (common.Logger, error) {
 	}
 
 	logger := &Logger{Logger: log.New()}
-	stdh := log.StreamHandler(os.Stdout, log.LogfmtFormat())
+	// This is required to print locations of log calls
+	// This was recently added in this PR: https://github.com/ethereum/go-ethereum/pull/28069/files
+	// where the default behavior was changed to not print origins
+	// This was due to it being very expensive to compute origins
+	// We should evaluate enabling/disabling this based on the flag
+	log.PrintOrigins(true)
+	stdh, err := getStreamHandlerFromFormat(cfg.StdFormat)
+	if err != nil {
+		return nil, err
+	}
 	stdHandler := log.CallerFileHandler(log.LvlFilterHandler(stdLevel, stdh))
 	if cfg.Path != "" {
-		fh, err := log.FileHandler(cfg.Path, log.LogfmtFormat())
+		fh, err := getFileHandlerFromFormat(cfg.Path, cfg.FileFormat)
 		if err != nil {
 			return nil, err
 		}
