64eae7f1feffb50912e4aff225cfd1e7b9e29cd2
Teddy Knox <teddyknox@gmail.com>
2024-02-13 15:18:29 -0500

    Add MIT licenses to code used by Rollkit

--- common/logging.go
+++ common/logging.go
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2024 Layr Labs, Inc.
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
 package common
 
 import "github.com/ethereum/go-ethereum/log"
