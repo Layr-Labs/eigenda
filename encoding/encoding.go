c7c2ce802ac9b35cd0c6250d6fe54d899af3b01d
Robert Raynor <35671663+mooselumph@users.noreply.github.com>
2024-02-20 22:56:17 +0000

    Remove duplicate KzgConfig definition

--- encoding/encoding.go
+++ encoding/encoding.go
@@ -7,19 +7,6 @@ import (
 	"github.com/Layr-Labs/eigenda/pkg/kzg/bn254"
 )
 
-type KzgConfig struct {
-	G1Path          string
-	G2Path          string
-	G1PowerOf2Path  string
-	G2PowerOf2Path  string
-	CacheDir        string
-	NumWorker       uint64
-	SRSOrder        uint64 // Order is the total size of SRS
-	SRSNumberToLoad uint64 // Number of points to be loaded from the begining
-	Verbose         bool
-	PreloadEncoder  bool
-}
-
 // Proof is the multireveal proof
 // Coeffs is identical to input data converted into Fr element
 type Frame struct {
