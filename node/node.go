8d45404d7f715135b487c52e69ef45d55b2f87ca
Siddharth More <Siddhi More>
2024-03-22 12:18:49 -0700

    add comma

--- node/node.go
+++ node/node.go
@@ -37,7 +37,7 @@ const (
 var (
 	// eigenDAUIMap is a mapping for ChainID to the EigenDA UI url.
 	eigenDAUIMap = map[string]string{
-		"17000": "https://holesky.eigenlayer.xyz/avs/eigenda"
+		"17000": "https://holesky.eigenlayer.xyz/avs/eigenda",
 	}
 )
 
