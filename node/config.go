08d8781a2165c159ac9bb502dd61ed6ed340601c
Ian Shim <100327837+ian-shim@users.noreply.github.com>
2024-04-08 12:42:40 -0700

    Bump v0.6.1 (#458)

--- node/config.go
+++ node/config.go
@@ -23,7 +23,7 @@ const (
 	// Min number of seconds for the ExpirationPollIntervalSecFlag.
 	minExpirationPollIntervalSec = 3
 	AppName                      = "da-node"
-	SemVer                       = "0.6.0"
+	SemVer                       = "0.6.1"
 	GitCommit                    = ""
 	GitDate                      = ""
 )
