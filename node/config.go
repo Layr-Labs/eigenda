f61d0b87b1e49c36adf39e357eeb902e574d9a38
Ubuntu <ubuntu@ip-172-31-91-211.ec2.internal>
2024-04-16 22:59:54 +0000

    bump version

--- node/config.go
+++ node/config.go
@@ -23,7 +23,7 @@ const (
 	// Min number of seconds for the ExpirationPollIntervalSecFlag.
 	minExpirationPollIntervalSec = 3
 	AppName                      = "da-node"
-	SemVer                       = "0.6.1"
+	SemVer                       = "0.6.2"
 	GitCommit                    = ""
 	GitDate                      = ""
 )
