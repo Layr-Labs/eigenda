c633e4a5f55369fae10dff65f654cfe907a27d42
Bowen Xue <93296844+bxue-l2@users.noreply.github.com>
2024-04-22 13:37:14 -0700

    Release/0.6.2 (#513)
    Co-authored-by: Ian Shim <100327837+ian-shim@users.noreply.github.com>
    Co-authored-by: Teddy Knox <teddyknox@gmail.com>
    Co-authored-by: Jian Xiao <99709935+jianoaix@users.noreply.github.com>
    Co-authored-by: Robert Raynor <35671663+mooselumph@users.noreply.github.com>
    Co-authored-by: siddimore <siddimore@gmail.com>
    Co-authored-by: Romain Guichard <romain@particule.io>
    Co-authored-by: Bowen Xue <ubuntu@ip-172-31-91-211.ec2.internal>
    Co-authored-by: anupsv <anupsv@users.noreply.github.com>

--- node/config.go
+++ node/config.go
@@ -23,7 +23,7 @@ const (
 	// Min number of seconds for the ExpirationPollIntervalSecFlag.
 	minExpirationPollIntervalSec = 3
 	AppName                      = "da-node"
-	SemVer                       = "0.6.2-rc.0"
+	SemVer                       = "0.6.2"
 	GitCommit                    = ""
 	GitDate                      = ""
 )
