7b9003a3782a249139c1b4223b1332ae85ee74d1
gopherorg <gopherworld@icloud.com>
2024-05-13 18:21:41 +0800

    chore: fix some typos in comments
    Signed-off-by: gopherorg <gopherworld@icloud.com>

--- disperser/dataapi/server_test.go
+++ disperser/dataapi/server_test.go
@@ -1231,7 +1231,7 @@ func TestFetchDeregisteredOperatorOnline(t *testing.T) {
 }
 
 func TestFetchDeregisteredOperatorsMultipleOfflineOnline(t *testing.T) {
-	// Skipping this test as repported being flaky but could not reproduce it locally
+	// Skipping this test as reported being flaky but could not reproduce it locally
 	t.Skip("Skipping testing in CI environment")
 
 	defer goleak.VerifyNone(t)
