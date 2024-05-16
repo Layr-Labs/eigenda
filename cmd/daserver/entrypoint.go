820f78dccbb019009a406ab65189c931d570ac74
Ethen Pociask <ethenpo@gmail.com>
2024-05-15 17:48:51 -0700

    feat(op-plasma-eigenda): Metrics server

--- cmd/daserver/entrypoint.go
+++ cmd/daserver/entrypoint.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
 
+	"github.com/Layr-Labs/op-plasma-eigenda/metrics"
 	"github.com/urfave/cli/v2"
 
 	plasma "github.com/Layr-Labs/op-plasma-eigenda"
@@ -13,15 +16,23 @@ import (
 	"github.com/ethereum-optimism/optimism/op-service/opio"
 )
 
+type App struct {
+	DAServer   *http.Server
+	MetricsSvr *http.Server
+}
+
 func StartDAServer(cliCtx *cli.Context) error {
+	println("CHECKING")
 	if err := CheckRequired(cliCtx); err != nil {
 		return err
 	}
-
+	println("Reading CLI CFG")
 	cfg := ReadCLIConfig(cliCtx)
 	if err := cfg.Check(); err != nil {
 		return err
 	}
+	println("HERERERER")
+	m := metrics.NewMetrics("default")
 
 	log := oplog.NewLogger(oplog.AppOut(cliCtx), oplog.ReadCLIConfig(cliCtx)).New("role", "eigenda_plasma_server")
 	oplog.SetGlobalLogHandler(log.Handler())
@@ -61,8 +72,7 @@ func StartDAServer(cliCtx *cli.Context) error {
 		}
 		store = eigenda
 	}
-
-	server := plasma.NewDAServer(cliCtx.String(ListenAddrFlagName), cliCtx.Int(PortFlagName), store, log)
+	server := plasma.NewDAServer(cliCtx.String(ListenAddrFlagName), cliCtx.Int(PortFlagName), store, log, m)
 
 	if err := server.Start(); err != nil {
 		return fmt.Errorf("failed to start the DA server")
@@ -75,6 +85,21 @@ func StartDAServer(cliCtx *cli.Context) error {
 			log.Error("failed to stop DA server", "err", err)
 		}
 	}()
+
+	if cfg.MetricsCfg.Enabled {
+		log.Debug("starting metrics server", "addr", cfg.MetricsCfg.ListenAddr, "port", cfg.MetricsCfg.ListenPort)
+		metricsSrv, err := m.StartServer(cfg.MetricsCfg.ListenAddr, cfg.MetricsCfg.ListenPort)
+		if err != nil {
+			return fmt.Errorf("failed to start metrics server: %w", err)
+		}
+		defer func() {
+			if err := metricsSrv.Stop(context.Background()); err != nil {
+				log.Error("failed to stop metrics server", "err", err)
+			}
+		}()
+		log.Info("started metrics server", "addr", metricsSrv.Addr())
+		m.RecordUp()
+	}
 
 	opio.BlockOnInterrupts()
 
