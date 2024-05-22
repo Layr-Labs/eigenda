244c88d6bc08ff78ba2d57ccd02a7a692d0f0889
Teddy Knox <teddyknox@gmail.com>
2024-05-22 18:24:54 -0400

    Remove default quorums validation logic

--- api/clients/config.go
+++ api/clients/config.go
@@ -1,7 +1,6 @@
 package clients
 
 import (
-	"fmt"
 	"time"
 )
 
@@ -18,7 +17,7 @@ type EigenDAClientConfig struct {
 	// The total amount of time that the client will waiting for a response from the EigenDA disperser
 	ResponseTimeout time.Duration
 
-	// The quorum IDs to write blobs to using this client. Should not include quorums 0 or 1.
+	// The quorum IDs to write blobs to using this client. Should not include default quorums 0 or 1.
 	CustomQuorumIDs []uint
 
 	// Signer private key in hex encoded format. This key should not be associated with an Ethereum address holding any funds.
@@ -31,14 +30,7 @@ type EigenDAClientConfig struct {
 	PutBlobEncodingVersion BlobEncodingVersion
 }
 
-var DefaultQuorums = map[uint]bool{0: true, 1: true}
-
 func (c *EigenDAClientConfig) Check() error {
-	for _, e := range c.CustomQuorumIDs {
-		if DefaultQuorums[e] {
-			return fmt.Errorf("EigenDA client config failed validation because CustomQuorumIDs includes a default quorum ID %d. Because it is included by default this quorum ID can be removed from the client configuration", e)
-		}
-	}
 	if c.StatusQueryRetryInterval == 0 {
 		c.StatusQueryRetryInterval = 5 * time.Second
 	}
