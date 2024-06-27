gelatodigital/eigenda-proxy

Add commitment mode parameter and commitment type system

client/client.go
@@ -7,9 +7,7 @@ import (
 	"io"
 	"net/http"
 
-	"github.com/Layr-Labs/eigenda-proxy/common"
-	"github.com/Layr-Labs/eigenda-proxy/eigenda"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/Layr-Labs/eigenda-proxy/server"
 )
 
 const (
@@ -25,8 +23,8 @@ type Config struct {
 // ProxyClient is an interface for communicating with the EigenDA proxy server
 type ProxyClient interface {
 	Health() error
-	GetData(ctx context.Context, cert *common.Certificate, domain common.DomainType) ([]byte, error)
-	SetData(ctx context.Context, b []byte) (*common.Certificate, error)
+	GetData(ctx context.Context, cert []byte, domain server.DomainType, commitmentType server.CommitmentType) ([]byte, error)
+	SetData(ctx context.Context, b []byte, commitmentType server.CommitmentType) ([]byte, error)
 }
 
 // client is the implementation of ProxyClient
@@ -64,16 +62,8 @@ func (c *client) Health() error {
 }
 
 // GetData fetches blob data associated with a DA certificate
-func (c *client) GetData(ctx context.Context, cert *common.Certificate, domain common.DomainType) ([]byte, error) {
-	b, err := rlp.EncodeToBytes(cert)
-	if err != nil {
-		return nil, err
-	}
-
-	// encode prefix bytes
-	b = eigenda.GenericPrefix(eigenda.Commitment(b).Encode())
-
-	url := fmt.Sprintf("%s/get/0x%x?domain=%s", c.cfg.URL, b, domain.String())
+func (c *client) GetData(ctx context.Context, comm []byte, domain server.DomainType, commitmentType server.CommitmentType) ([]byte, error) {
+	url := fmt.Sprintf("%s/get/0x%x?domain=%s&commitment_mode=%s", c.cfg.URL, comm, domain.String(), commitmentType)
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
@@ -96,8 +86,8 @@ func (c *client) GetData(ctx context.Context, cert *common.Certificate, domain c
 }
 
 // SetData writes raw byte data to DA and returns the respective certificate
-func (c *client) SetData(ctx context.Context, b []byte) (*common.Certificate, error) {
-	url := fmt.Sprintf("%s/put/", c.cfg.URL)
+func (c *client) SetData(ctx context.Context, b []byte, commitmentType server.CommitmentType) ([]byte, error) {
+	url := fmt.Sprintf("%s/put/?commitment_mode=%s", c.cfg.URL, commitmentType)
 	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, bytes.NewReader(b))
 	if err != nil {
 		return nil, fmt.Errorf("failed to create HTTP request: %w", err)
@@ -117,14 +107,9 @@ func (c *client) SetData(ctx context.Context, b []byte) (*common.Certificate, er
 		return nil, err
 	}
 
-	if len(b) < decodingOffset {
-		return nil, fmt.Errorf("read certificate is of invalid length: %d", len(b))
+	if len(b) == 0 {
+		return nil, fmt.Errorf("read certificate is empty")
 	}
 
-	var cert *common.Certificate
-	if err = rlp.DecodeBytes(b[decodingOffset:], &cert); err != nil {
-		return nil, err
-	}
-
-	return cert, err
+	return b, err
 }

e2e/server_test.go
@@ -5,8 +5,8 @@ import (
 	"time"
 
 	"github.com/Layr-Labs/eigenda-proxy/client"
+	"github.com/Layr-Labs/eigenda-proxy/server"
 
-	"github.com/Layr-Labs/eigenda-proxy/common"
 	"github.com/Layr-Labs/eigenda-proxy/e2e"
 	"github.com/Layr-Labs/eigenda/api/clients/codecs"
 	"github.com/ethereum-optimism/optimism/op-e2e/e2eutils/wait"
@@ -87,17 +87,17 @@ func TestProxyClient(t *testing.T) {
 	var testPreimage = []byte("inter-subjective and not objective!")
 
 	t.Log("Setting input data on proxy server...")
-	blobInfo, err := daClient.SetData(ts.Ctx, testPreimage)
+	blobInfo, err := daClient.SetData(ts.Ctx, testPreimage, server.DefaultCommitmentMode)
 	require.NoError(t, err)
 
 	// 2 - fetch data from EigenDA for generated commitment key
 	t.Log("Getting input data from proxy server...")
-	preimage, err := daClient.GetData(ts.Ctx, blobInfo, common.BinaryDomain)
+	preimage, err := daClient.GetData(ts.Ctx, blobInfo, server.BinaryDomain, server.DefaultCommitmentMode)
 	require.NoError(t, err)
 	require.Equal(t, testPreimage, preimage)
 
 	// 3 - fetch iFFT representation of preimage
-	iFFTPreimage, err := daClient.GetData(ts.Ctx, blobInfo, common.PolyDomain)
+	iFFTPreimage, err := daClient.GetData(ts.Ctx, blobInfo, server.PolyDomain, server.DefaultCommitmentMode)
 	require.NoError(t, err)
 	require.NotEqual(t, preimage, iFFTPreimage)
 

Layr-Labs/eigenda

client/client.go
@@ -7,14 +7,7 @@ import (
 	"io"
 	"net/http"
 
-	"github.com/Layr-Labs/eigenda-proxy/common"
-	"github.com/Layr-Labs/eigenda-proxy/eigenda"
-	"github.com/ethereum/go-ethereum/rlp"
-)
-
-const (
-	// NOTE: this will need to be updated as plasma's implementation changes
-	decodingOffset = 3
+	"github.com/Layr-Labs/eigenda-proxy/server"
 )
 
 // TODO: Add support for custom http client option
@@ -25,8 +18,8 @@ type Config struct {
 // ProxyClient is an interface for communicating with the EigenDA proxy server
 type ProxyClient interface {
 	Health() error
-	GetData(ctx context.Context, cert *common.Certificate, domain common.DomainType) ([]byte, error)
-	SetData(ctx context.Context, b []byte) (*common.Certificate, error)
+	GetData(ctx context.Context, cert []byte, domain server.DomainType) ([]byte, error)
+	SetData(ctx context.Context, b []byte) ([]byte, error)
 }
 
 // client is the implementation of ProxyClient
@@ -34,6 +27,8 @@ type client struct {
 	cfg        *Config
 	httpClient *http.Client
 }
+
+var _ ProxyClient = (*client)(nil)
 
 func New(cfg *Config) ProxyClient {
 	return &client{
@@ -64,16 +59,8 @@ func (c *client) Health() error {
 }
 
 // GetData fetches blob data associated with a DA certificate
-func (c *client) GetData(ctx context.Context, cert *common.Certificate, domain common.DomainType) ([]byte, error) {
-	b, err := rlp.EncodeToBytes(cert)
-	if err != nil {
-		return nil, err
-	}
-
-	// encode prefix bytes
-	b = eigenda.Commitment(b).Encode()
-
-	url := fmt.Sprintf("%s/get/0x%x?domain=%s", c.cfg.URL, b, domain.String())
+func (c *client) GetData(ctx context.Context, comm []byte, domain server.DomainType) ([]byte, error) {
+	url := fmt.Sprintf("%s/get/0x%x?domain=%s&commitment_mode=simple", c.cfg.URL, comm, domain.String())
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
@@ -96,8 +83,8 @@ func (c *client) GetData(ctx context.Context, cert *common.Certificate, domain c
 }
 
 // SetData writes raw byte data to DA and returns the respective certificate
-func (c *client) SetData(ctx context.Context, b []byte) (*common.Certificate, error) {
-	url := fmt.Sprintf("%s/put/", c.cfg.URL)
+func (c *client) SetData(ctx context.Context, b []byte) ([]byte, error) {
+	url := fmt.Sprintf("%s/put/?commitment_mode=simple", c.cfg.URL)
 	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, bytes.NewReader(b))
 	if err != nil {
 		return nil, fmt.Errorf("failed to create HTTP request: %w", err)
@@ -117,14 +104,9 @@ func (c *client) SetData(ctx context.Context, b []byte) (*common.Certificate, er
 		return nil, err
 	}
 
-	if len(b) < decodingOffset {
-		return nil, fmt.Errorf("read certificate is of invalid length: %d", len(b))
+	if len(b) == 0 {
+		return nil, fmt.Errorf("read certificate is empty")
 	}
 
-	var cert *common.Certificate
-	if err = rlp.DecodeBytes(b[decodingOffset:], &cert); err != nil {
-		return nil, err
-	}
-
-	return cert, err
+	return b, err
 }

e2e/server_test.go
@@ -5,8 +5,8 @@ import (
 	"time"
 
 	"github.com/Layr-Labs/eigenda-proxy/client"
+	"github.com/Layr-Labs/eigenda-proxy/server"
 
-	"github.com/Layr-Labs/eigenda-proxy/common"
 	"github.com/Layr-Labs/eigenda-proxy/e2e"
 	"github.com/Layr-Labs/eigenda/api/clients/codecs"
 	"github.com/ethereum-optimism/optimism/op-e2e/e2eutils/wait"
@@ -65,7 +65,7 @@ func TestProxyClient(t *testing.T) {
 	daClient := client.New(cfg)
 	t.Log("Waiting for client to establish connection with plasma server...")
 	// wait for server to come online after starting
-	wait.For(ts.Ctx, time.Second*1, func() (bool, error) {
+	err := wait.For(ts.Ctx, time.Second*1, func() (bool, error) {
 		err := daClient.Health()
 		if err != nil {
 			return false, nil
@@ -73,6 +73,7 @@ func TestProxyClient(t *testing.T) {
 
 		return true, nil
 	})
+	require.NoError(t, err)
 
 	// 1 - write arbitrary data to EigenDA
 
@@ -84,12 +85,12 @@ func TestProxyClient(t *testing.T) {
 
 	// 2 - fetch data from EigenDA for generated commitment key
 	t.Log("Getting input data from proxy server...")
-	preimage, err := daClient.GetData(ts.Ctx, blobInfo, common.BinaryDomain)
+	preimage, err := daClient.GetData(ts.Ctx, blobInfo, server.BinaryDomain)
 	require.NoError(t, err)
 	require.Equal(t, testPreimage, preimage)
 
 	// 3 - fetch iFFT representation of preimage
-	iFFTPreimage, err := daClient.GetData(ts.Ctx, blobInfo, common.PolyDomain)
+	iFFTPreimage, err := daClient.GetData(ts.Ctx, blobInfo, server.PolyDomain)
 	require.NoError(t, err)
 	require.NotEqual(t, preimage, iFFTPreimage)
 