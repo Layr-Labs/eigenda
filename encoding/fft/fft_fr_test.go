ac86110c2b418f6f73a698b09d6a719dca6819ff
Cody Littley <cody@eigenlabs.org>
2024-07-15 08:36:44 -0500

    Fix fft_fr.IsPowerOfTwo(). (#83)
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

--- encoding/fft/fft_fr_test.go
+++ encoding/fft/fft_fr_test.go
@@ -24,6 +24,7 @@
 package fft
 
 import (
+	"math"
 	"testing"
 
 	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
@@ -98,5 +99,25 @@ func TestInvFFT(t *testing.T) {
 
 	for i := range res {
 		assert.True(t, res[i].Equal(&expected[i]))
+	}
+}
+
+func TestIsPowerOfTwo(t *testing.T) {
+	var i uint64
+	for i = 0; i <= 1024; i++ {
+		result := IsPowerOfTwo(i)
+
+		var expectedResult bool
+		if i == 0 {
+			// Special case: math.Log2() is undefined for 0
+			expectedResult = false
+		} else {
+			// If a number is not a power of two then the log base 2 of that number will not be a whole integer.
+			logBase2 := math.Log2(float64(i))
+			truncatedLogBase2 := float64(uint64(logBase2))
+			expectedResult = logBase2 == truncatedLogBase2
+		}
+
+		assert.Equal(t, expectedResult, result, "IsPowerOfTwo(%d) returned unexpected result '%t'.", i, result)
 	}
 }
