2566c2135b5174c23560fcbe425b1aec01b8df73
Ian Shim <100327837+ian-shim@users.noreply.github.com>
2024-08-02 11:27:03 -0700

    Downgrade encoder error logs (#682)

--- disperser/batcher/encoding_streamer.go
+++ disperser/batcher/encoding_streamer.go
@@ -142,6 +142,10 @@ func (e *EncodingStreamer) Start(ctx context.Context) error {
 					}
 					if strings.Contains(err.Error(), "too many requests") {
 						e.logger.Warn("encoding request ratelimited", "err", err)
+					} else if strings.Contains(err.Error(), "connection reset by peer") {
+						e.logger.Warn("encoder connection reset by peer", "err", err)
+					} else if strings.Contains(err.Error(), "error reading from server: EOF") {
+						e.logger.Warn("encoder request dropped", "err", err)
 					} else {
 						e.logger.Error("error processing encoded blobs", "err", err)
 					}
