679c10d256f29db4f4a9f3b470d83065c945b6b1
Ethen Pociask <ethenpo@gmail.com>
2024-08-05 02:18:42 -0400

    fix: Oversized blob test - use smaller blob

--- e2e/server_test.go
+++ e2e/server_test.go
@@ -134,8 +134,8 @@ func TestProxyClientWithOversizedBlob(t *testing.T) {
 		URL: ts.Address(),
 	}
 	daClient := client.New(cfg)
-	//  2MB blob
-	testPreimage := []byte(e2e.RandString(200000000))
+	//  5MB blob
+	testPreimage := []byte(e2e.RandString(5_000_0000))
 
 	t.Log("Setting input data on proxy server...")
 	blobInfo, err := daClient.SetData(ts.Ctx, testPreimage)
