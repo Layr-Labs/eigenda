635f59844d25c12a09486346c5bea5e15d461b89
Cody Littley <cody@eigenlabs.org>
2024-08-15 10:11:51 -0500

    Settings tweak.
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

--- lightnode/kvstore/badger_store.go
+++ lightnode/kvstore/badger_store.go
@@ -32,6 +32,7 @@ func NewBadgerStore(logger logging.Logger, path string) (KVStore, error) {
 
 	options := badger.DefaultOptions(path)
 	options.Compression = boptions.None
+	options.BlockCacheSize = 0
 
 	db, err := badger.Open(options)
 	if err != nil {
