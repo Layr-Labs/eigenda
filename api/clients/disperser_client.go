d0f397aaea6251fe7a14d64066da662fce97e11c
Ubuntu <ubuntu@ip-172-31-91-211.ec2.internal>
2024-08-19 22:54:47 +0000

    use insecure credential

--- api/clients/disperser_client.go
+++ api/clients/disperser_client.go
@@ -228,7 +228,7 @@ func (c *disperserClient) GetBlobStatus(ctx context.Context, requestID []byte) (
 func (c *disperserClient) RetrieveBlob(ctx context.Context, batchHeaderHash []byte, blobIndex uint32) ([]byte, error) {
 	addr := fmt.Sprintf("%v:%v", c.config.Hostname, c.config.Port)
 
-	options := make([]grpc.DialOption, 0)
+	options := c.getDialOptions()
 	options = append(options, grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(100*1024*1024))) // 100MiB receive buffer
 
 	conn, err := grpc.Dial(addr, options...)
