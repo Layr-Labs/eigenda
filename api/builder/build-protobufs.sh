17435e1cdb2b4d4e6d2c520ae2f89934df500817
Cody Littley <cody@eigenlabs.org>
2024-08-30 09:25:08 -0500

    Use more specific initial docker image.
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

--- api/builder/build-protobufs.sh
+++ api/builder/build-protobufs.sh
@@ -7,4 +7,4 @@ ROOT="${SCRIPT_DIR}/../.."
 docker container run \
   --rm \
   --mount "type=bind,source=${ROOT},target=/home/user/eigenda" \
-  pbuf-compiler bash -c 'source .bashrc && cd eigenda && make protoc'
+  pbuf-compiler bash -c 'source ~/.bashrc && cd eigenda && make protoc'
