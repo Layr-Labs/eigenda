5b6eef32ed29074d8e314f918536f69e454b859c
Cody Littley <cody@eigenlabs.org>
2024-09-04 14:24:28 -0500

    Made suggested changes.
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

--- lightnode/docker/build.sh
+++ lightnode/docker/build.sh
@@ -3,38 +3,11 @@
 # The location where this script can be found.
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "${SCRIPT_DIR}"
-
-source default-args.sh
-# Create a file called args.sh to override the default values of GIT_URL and BRANCH_OR_COMMIT.
-source args.sh
-
-echo "git url: ${GIT_URL}"
-echo "branch or commit: ${BRANCH_OR_COMMIT}"
-
-# Create a file with information about this build. This file will be copied into the docker image.
-rm build-info.txt 2> /dev/null || true
-touch build-info.txt
-echo "git URL: ${GIT_URL}" >> build-info.txt
-
-# This will return an empty string if a git sha is provided. Will return sha and branch name if a branch is provided.
-COMMIT_SHA=$(git ls-remote $GIT_URL $BRANCH_OR_COMMIT)
-if [ -z "$COMMIT_SHA" ]; then
-  echo "target: ${BRANCH_OR_COMMIT}" >> build-info.txt
-else
-  echo "target: ${COMMIT_SHA}" >> build-info.txt
-fi
-
-echo "docker build commit: $(git rev-parse HEAD)" >> build-info.txt
-
-# Add the --no-cache flag to force a rebuild.
-# Add the --progress=plain flag to show verbose output during the build.
+REPO_ROOT=$(git rev-parse --show-toplevel)
+cd "${REPO_ROOT}"
 
 docker build \
-  --build-arg="GIT_URL=${GIT_URL}" \
-  --build-arg="BRANCH_OR_COMMIT=${BRANCH_OR_COMMIT}" \
-  -f Dockerfile \
+  --build-arg="REPO_ROOT=${REPO_ROOT}" \
+  -f "lightnode/docker/Dockerfile" \
   --tag lnode:latest \
   .
-
-# Don't leave trash on the filesystem.
-rm build-info.txt 2> /dev/null || true
--- lightnode/docker/default-args.sh
+++ lightnode/docker/default-args.sh
@@ -1,11 +1,5 @@
 # Default arguments for building the docker image.
 # To override these values locally, create a file named `args.sh` in the same directory. 'args.sh' is ignored by git.
 
-# The location of the code to clone.
-export GIT_URL=https://github.com/Layr-Labs/eigenda.git
-
-# The name of the branch or the commit sha to clone.
-export BRANCH_OR_COMMIT=master
-
 # The location on the host file system where light node data will be stored.
 export DATA_PATH=~/.lnode-data
