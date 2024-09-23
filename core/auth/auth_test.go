4a22a7aefe3d58cd5bbf8ec8d65641889677a926
Bowen Xue <93296844+bxue-l2@users.noreply.github.com>
2024-09-22 20:15:10 -0700

    rm sig check (#770)
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-91-211.ec2.internal>

--- core/auth/auth_test.go
+++ core/auth/auth_test.go
@@ -19,9 +19,12 @@ func TestAuthentication(t *testing.T) {
 	privateKeyHex := "0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
 	signer := auth.NewLocalBlobRequestSigner(privateKeyHex)
 
+	accountId, err := signer.GetAccountID()
+	assert.NoError(t, err)
+
 	testHeader := core.BlobAuthHeader{
 		BlobCommitments:    encoding.BlobCommitments{},
-		AccountID:          signer.GetAccountID(),
+		AccountID:          accountId,
 		Nonce:              rand.Uint32(),
 		AuthenticationData: []byte{},
 	}
@@ -46,9 +49,12 @@ func TestAuthenticationFail(t *testing.T) {
 	privateKeyHex := "0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
 	signer := auth.NewLocalBlobRequestSigner(privateKeyHex)
 
+	accountId, err := signer.GetAccountID()
+	assert.NoError(t, err)
+
 	testHeader := core.BlobAuthHeader{
 		BlobCommitments:    encoding.BlobCommitments{},
-		AccountID:          signer.GetAccountID(),
+		AccountID:          accountId,
 		Nonce:              rand.Uint32(),
 		AuthenticationData: []byte{},
 	}
@@ -65,4 +71,19 @@ func TestAuthenticationFail(t *testing.T) {
 	err = authenticator.AuthenticateBlobRequest(testHeader)
 	assert.Error(t, err)
 
+}
+
+func TestNoopSignerFail(t *testing.T) {
+	signer := auth.NewLocalNoopSigner()
+	accountId, err := signer.GetAccountID()
+	assert.EqualError(t, err, "noop signer cannot get accountID")
+
+	testHeader := core.BlobAuthHeader{
+		BlobCommitments:    encoding.BlobCommitments{},
+		AccountID:          accountId,
+		Nonce:              rand.Uint32(),
+		AuthenticationData: []byte{},
+	}
+	_, err = signer.SignBlobRequest(testHeader)
+	assert.EqualError(t, err, "noop signer cannot sign blob request")
 }
