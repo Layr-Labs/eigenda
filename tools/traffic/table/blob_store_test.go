aac8c64bc9587c1e7b5a54ac57621c18c0cb98b3
Cody Littley <cody@eigenlabs.org>
2024-10-03 15:03:04 -0500

    Cleanup.
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

--- tools/traffic/table/blob_store_test.go
+++ tools/traffic/table/blob_store_test.go
@@ -10,12 +10,10 @@ import (
 // randomMetadata generates a random BlobMetadata instance.
 func randomMetadata(t *testing.T, permits int) *BlobMetadata {
 	key := make([]byte, 32)
-	batchHeaderHash := [32]byte{}
 	checksum := [16]byte{}
 	_, _ = rand.Read(key)
 	_, _ = rand.Read(checksum[:])
-	_, _ = rand.Read(batchHeaderHash[:])
-
+	batchHeaderHash := [32]byte{}
 	metadata, err := NewBlobMetadata(key, checksum, 1024, 0, batchHeaderHash, permits)
 	assert.Nil(t, err)
 
