1ad837e31681512ed7c886581ae2b642b78247cb
Ian Shim <shim@fastmail.com>
2024-10-02 22:28:09 -0700

    remove minibatcher

--- disperser/cmd/batcher/main.go
+++ disperser/cmd/batcher/main.go
@@ -239,18 +239,13 @@ func RunBatcher(ctx *cli.Context) error {
 		logger.Info("Enabled metrics for Batcher", "socket", httpSocket)
 	}
 
-	if config.EnableMinibatch {
-		// TODO: implement and run batchConfirmer for minibatch
-		return errors.New("minibatch is not supported")
-	} else {
-		batcher, err := batcher.NewBatcher(config.BatcherConfig, config.TimeoutConfig, queue, dispatcher, ics, asgn, encoderClient, agg, client, finalizer, tx, txnManager, logger, metrics, handleBatchLivenessChan)
-		if err != nil {
-			return err
-		}
-		err = batcher.Start(context.Background())
-		if err != nil {
-			return err
-		}
+	batcher, err := batcher.NewBatcher(config.BatcherConfig, config.TimeoutConfig, queue, dispatcher, ics, asgn, encoderClient, agg, client, finalizer, tx, txnManager, logger, metrics, handleBatchLivenessChan)
+	if err != nil {
+		return err
+	}
+	err = batcher.Start(context.Background())
+	if err != nil {
+		return err
 	}
 
 	// Signal readiness
