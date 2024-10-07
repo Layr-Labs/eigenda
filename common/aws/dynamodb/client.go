27fa23ced98cfafda5c2830038d17795f1821505
hopeyen <hopey1118@gmail.com>
2024-10-07 15:49:03 -0700

    feat: numeric incremental updates, query with ordering and lim

--- common/aws/dynamodb/client.go
+++ common/aws/dynamodb/client.go
@@ -128,6 +128,11 @@ func (c *Client) PutItems(ctx context.Context, tableName string, items []Item) (
 }
 
 func (c *Client) UpdateItem(ctx context.Context, tableName string, key Key, item Item) (Item, error) {
+	err := ensureKeyAttributes(key, item)
+	if err != nil {
+		return nil, err
+	}
+
 	update := expression.UpdateBuilder{}
 	for itemKey, itemValue := range item {
 		// Ignore primary key updates
