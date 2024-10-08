daebf59efaf506e0feeed01e0e9a8e7826621743
Cody Littley <cody@eigenlabs.org>
2024-10-08 08:38:56 -0500

    Find .proto files with cross-shell compatible strategy.
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

--- api/builder/protoc.sh
+++ api/builder/protoc.sh
@@ -12,12 +12,14 @@ PROTO_DIR="${API_DIR}/proto"
 GRPC_DIR="${API_DIR}/grpc"
 mkdir -p "${GRPC_DIR}"
 
+PROTO_FILES=( $(find "${PROTO_DIR}" -name '*.proto') )
+
 protoc -I "${PROTO_DIR}" \
 	--go_out="${GRPC_DIR}" \
 	--go_opt=paths=source_relative \
 	--go-grpc_out="${GRPC_DIR}" \
 	--go-grpc_opt=paths=source_relative \
-	"${PROTO_DIR}"/**/*.proto
+	${PROTO_FILES[@]}
 
 # Build protobufs in the disperser/api/proto directory.
 
@@ -26,9 +28,11 @@ DISPERSER_PROTO_DIR="$DISPERSER_DIR/api/proto"
 DISPERSER_GRPC_DIR="$DISPERSER_DIR/api/grpc"
 mkdir -p "${DISPERSER_GRPC_DIR}"
 
+DISPERSER_PROTO_FILES=( $(find "${DISPERSER_PROTO_DIR}" -name '*.proto') )
+
 protoc -I "${DISPERSER_PROTO_DIR}" -I "${PROTO_DIR}" \
 	--go_out="${DISPERSER_GRPC_DIR}" \
 	--go_opt=paths=source_relative \
 	--go-grpc_out="${DISPERSER_GRPC_DIR}" \
 	--go-grpc_opt=paths=source_relative \
-	"${DISPERSER_PROTO_DIR}"/**/*.proto
+	${DISPERSER_PROTO_FILES[@]}
