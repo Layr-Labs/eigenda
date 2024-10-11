413dde7dee99e272c7152e7f6f01061cde7d24b1
Cody Littley <56973212+cody-littley@users.noreply.github.com>
2024-10-11 09:01:19 -0500

    Validate protos workflow (#799)
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

--- api/builder/protoc.sh
+++ api/builder/protoc.sh
@@ -12,6 +12,10 @@ PROTO_DIR="${API_DIR}/proto"
 GRPC_DIR="${API_DIR}/grpc"
 mkdir -p "${GRPC_DIR}"
 
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
 PROTO_FILES=( $(find "${PROTO_DIR}" -name '*.proto') )
 
 protoc -I "${PROTO_DIR}" \
@@ -21,12 +25,20 @@ protoc -I "${PROTO_DIR}" \
 	--go-grpc_opt=paths=source_relative \
 	${PROTO_FILES[@]}
 
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
 # Build protobufs in the disperser/api/proto directory.
 
 DISPERSER_DIR="$SCRIPT_DIR/../../disperser"
 DISPERSER_PROTO_DIR="$DISPERSER_DIR/api/proto"
 DISPERSER_GRPC_DIR="$DISPERSER_DIR/api/grpc"
 mkdir -p "${DISPERSER_GRPC_DIR}"
+
+if [ $? -ne 0 ]; then
+  exit 1
+fi
 
 DISPERSER_PROTO_FILES=( $(find "${DISPERSER_PROTO_DIR}" -name '*.proto') )
 
@@ -36,3 +48,7 @@ protoc -I "${DISPERSER_PROTO_DIR}" -I "${PROTO_DIR}" \
 	--go-grpc_out="${DISPERSER_GRPC_DIR}" \
 	--go-grpc_opt=paths=source_relative \
 	${DISPERSER_PROTO_FILES[@]}
+
+if [ $? -ne 0 ]; then
+  exit 1
+fi
