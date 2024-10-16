2392cb204b1f8b188883b1a085aff7a3eb0dc55d
Cody Littley <cody@eigenlabs.org>
2024-10-16 08:44:06 -0500

    Remove TODO
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

--- common/kvstore/ttl_store.go
+++ common/kvstore/ttl_store.go
@@ -16,14 +16,14 @@ type TTLStore interface {
 	// Key is eventually deleted after the TTL elapses.
 	//
 	// Warning: updating the value of a key with a ttl/expiration has undefined behavior. Support for this pattern
-	// may be implemented in the future if a use case is identified. TODO
+	// may be implemented in the future if a use case is identified.
 	PutWithTTL(key []byte, value []byte, ttl time.Duration) error
 
 	// PutWithExpiration adds a key-value pair to the store that expires at a specified time.
 	// Key is eventually deleted after the expiry time.
 	//
 	// Warning: updating the value of a key with a ttl/expiration has undefined behavior. Support for this pattern
-	// may be implemented in the future if a use case is identified. TODO
+	// may be implemented in the future if a use case is identified.
 	PutWithExpiration(key []byte, value []byte, expiryTime time.Time) error
 
 	// NewTTLBatch creates a new TTLBatch that can be used to perform multiple operations atomically.
