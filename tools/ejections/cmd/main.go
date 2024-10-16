c330b048d88dffb364dacef887f53aa731ed919f
Patrick Schork <354473+pschork@users.noreply.github.com>
2024-10-16 00:08:36 -0700

    wip

---
+++ tools/ejections/cmd/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Layr-Labs/eigenda/common"
+	"github.com/Layr-Labs/eigenda/disperser/dataapi"
+	"github.com/Layr-Labs/eigenda/disperser/dataapi/subgraph"
+	"github.com/Layr-Labs/eigenda/tools/ejections"
+	"github.com/Layr-Labs/eigenda/tools/ejections/flags"
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/urfave/cli"
+)
+
+var (
+	version   = ""
+	gitCommit = ""
+	gitDate   = ""
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Version = fmt.Sprintf("%s,%s,%s", version, gitCommit, gitDate)
+	app.Name = "ejections"
+	app.Description = "operator ejections scanner"
+	app.Usage = ""
+	app.Flags = flags.Flags
+	app.Action = RunScan
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func RunScan(ctx *cli.Context) error {
+	config, err := ejections.NewConfig(ctx)
+	if err != nil {
+		return err
+	}
+
+	logger, err := common.NewLogger(config.LoggerConfig)
+	if err != nil {
+		return err
+	}
+	logger.Info("starting ejections scanner", "subgraph", config.SubgraphEndpoint)
+	subgraphApi := subgraph.NewApi(config.SubgraphEndpoint, config.SubgraphEndpoint)
+	subgraphClient := dataapi.NewSubgraphClient(subgraphApi, logger)
+
+	ejections, err := subgraphClient.QueryOperatorEjectionsForTimeWindow(context.Background(), 90, config.OperatorId)
+	if err != nil {
+		logger.Warn("failed to fetch operator ejections", "operatorId", config.OperatorId, "error", err)
+		return errors.New("operator ejections not found")
+	}
+	for _, ejection := range ejections {
+		logger.Info("operator ejection", "operatorId", ejection.OperatorId, "ejectionTime", ejection.BlockTimestamp, "ejectionTxnHash", ejection.TransactionHash)
+	}
+
+	return nil
+}
+
+func displayResults(results map[string]int) {
+	tw := table.NewWriter()
+
+	rowHeader := table.Row{"semver", "count"}
+	tw.AppendHeader(rowHeader)
+
+	total := 0
+	for semver, count := range results {
+		tw.AppendRow(table.Row{semver, count})
+		total += count
+	}
+	tw.AppendFooter(table.Row{"total", total})
+
+	fmt.Println(tw.Render())
+}
---
+++ tools/ejections/flags/flags.go
@@ -0,0 +1,63 @@
+package flags
+
+import (
+	"time"
+
+	"github.com/Layr-Labs/eigenda/common"
+	"github.com/urfave/cli"
+)
+
+const (
+	FlagPrefix = ""
+	envPrefix  = ""
+)
+
+var (
+	/* Required Flags*/
+	SubgraphEndpointFlag = cli.StringFlag{
+		Name:     common.PrefixFlag(FlagPrefix, "subgraph-endpoint"),
+		Usage:    "Subgraph endpoint to query operator state",
+		Required: true,
+		EnvVar:   common.PrefixEnvVar(envPrefix, "SUBGRAPH_ENDPOINT"),
+	}
+	/* Optional Flags*/
+	TimeoutFlag = cli.DurationFlag{
+		Name:     common.PrefixFlag(FlagPrefix, "timeout"),
+		Usage:    "seconds to wait for GPRC response",
+		Required: false,
+		EnvVar:   common.PrefixEnvVar(envPrefix, "TIMEOUT"),
+		Value:    3 * time.Second,
+	}
+	MaxConnectionsFlag = cli.IntFlag{
+		Name:     common.PrefixFlag(FlagPrefix, "max-connections"),
+		Usage:    "maximum number of connections to DA nodes",
+		Required: false,
+		EnvVar:   common.PrefixEnvVar(envPrefix, "MAX_CONNECTIONS"),
+		Value:    30,
+	}
+	OperatorIdFlag = cli.StringFlag{
+		Name:     common.PrefixFlag(FlagPrefix, "operator-id"),
+		Usage:    "operator id to scan",
+		Required: false,
+		EnvVar:   common.PrefixEnvVar(envPrefix, "OPERATOR_ID"),
+		Value:    "",
+	}
+)
+
+var requiredFlags = []cli.Flag{
+	SubgraphEndpointFlag,
+}
+
+var optionalFlags = []cli.Flag{
+	TimeoutFlag,
+	MaxConnectionsFlag,
+	OperatorIdFlag,
+}
+
+// Flags contains the list of configuration options available to the binary.
+var Flags []cli.Flag
+
+func init() {
+	Flags = append(requiredFlags, optionalFlags...)
+	Flags = append(Flags, common.LoggerCLIFlags(envPrefix, FlagPrefix)...)
+}
