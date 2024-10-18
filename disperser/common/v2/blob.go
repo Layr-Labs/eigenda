75c03abe55500de929526f96c0fa2058d1a04719
Ian Shim <shim@fastmail.com>
2024-10-17 23:04:07 -0700

    v2 blob metadata store

--- disperser/common/v2/blob.go
+++ disperser/common/v2/blob.go
@@ -1,11 +1,6 @@
 package v2
 
-import (
-	"encoding/hex"
-
-	"github.com/Layr-Labs/eigenda/core"
-	"github.com/Layr-Labs/eigenda/encoding"
-)
+import "github.com/Layr-Labs/eigenda/core"
 
 type BlobStatus uint
 
@@ -16,36 +11,14 @@ const (
 	Failed
 )
 
-type BlobVersion uint32
-
-type BlobKey [32]byte
-
-func (b BlobKey) Hex() string {
-	return hex.EncodeToString(b[:])
-}
-
-func HexToBlobKey(h string) (BlobKey, error) {
-	b, err := hex.DecodeString(h)
-	if err != nil {
-		return BlobKey{}, err
-	}
-	return BlobKey(b), nil
-}
-
-type BlobHeader struct {
-	BlobVersion     BlobVersion              `json:"version"`
-	BlobQuorumInfos []*core.BlobQuorumInfo   `json:"blob_quorum_infos"`
-	BlobCommitment  encoding.BlobCommitments `json:"commitments"`
-
-	core.PaymentMetadata `json:"payment_metadata"`
-}
-
 type BlobMetadata struct {
-	BlobHeader `json:"blob_header"`
+	core.BlobHeaderV2 `json:"blob_header"`
 
-	BlobStatus  BlobStatus `json:"blob_status"`
-	Expiry      uint64     `json:"expiry"`
-	NumRetries  uint       `json:"num_retries"`
-	BlobSize    uint64     `json:"blob_size"`
-	RequestedAt uint64     `json:"requested_at"`
+	BlobKey    core.BlobKey `json:"blob_key"`
+	BlobStatus BlobStatus   `json:"blob_status"`
+	// Expiry is Unix timestamp of the blob expiry in seconds from epoch
+	Expiry      uint64 `json:"expiry"`
+	NumRetries  uint   `json:"num_retries"`
+	BlobSize    uint64 `json:"blob_size"`
+	RequestedAt uint64 `json:"requested_at"`
 }
