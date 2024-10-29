9104ab1515f00a37dc858bed996f4749d37cb7e8
Cody Littley <cody@eigenlabs.org>
2024-10-29 11:57:03 -0500

    Fix unit test.
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

--- common/aws/test/client_test.go
+++ common/aws/test/client_test.go
@@ -77,7 +77,7 @@ var clientBuilders = []*clientBuilder{
 			}
 
 			err = client.CreateBucket(context.Background(), bucket)
-			if err != nil && err.Error() != "container already exists" {
+			if err != nil {
 				return nil, err
 			}
 
@@ -93,7 +93,7 @@ var clientBuilders = []*clientBuilder{
 func setupLocalstack() error {
 	var err error
 	dockertestPool, dockertestResource, err = deploy.StartDockertestWithLocalstackContainer(localstackPort)
-	if err != nil {
+	if err != nil && err.Error() != "container already exists" {
 		teardownLocalstack()
 		return err
 	}
