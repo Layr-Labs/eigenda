f4015d632a88305e62bcfc35189b95da519dfa70
pschork <354473+pschork@users.noreply.github.com>
2024-10-31 14:44:27 -0700

    Log semver scan operator id as hex (#802)

--- tools/semverscan/cmd/main.go
+++ tools/semverscan/cmd/main.go
@@ -59,38 +59,73 @@ func RunScan(ctx *cli.Context) error {
 	if err != nil {
 		log.Fatalln("could not start tcp listener", err)
 	}
-	cs := eth.NewChainState(tx, gethClient)
+	chainState := eth.NewChainState(tx, gethClient)
 
 	logger.Info("Connecting to subgraph", "url", config.ChainStateConfig.Endpoint)
-	ics := thegraph.MakeIndexedChainState(config.ChainStateConfig, cs, logger)
+	ics := thegraph.MakeIndexedChainState(config.ChainStateConfig, chainState, logger)
 
 	currentBlock, err := ics.GetCurrentBlockNumber()
 	if err != nil {
 		return fmt.Errorf("failed to fetch current block number - %s", err)
 	}
-	operatorState, err := ics.GetIndexedOperatorState(context.Background(), currentBlock, []core.QuorumID{0, 1, 2})
+	operatorState, err := chainState.GetOperatorState(context.Background(), currentBlock, []core.QuorumID{0, 1, 2})
 	if err != nil {
-		return fmt.Errorf("failed to fetch indexed operator state - %s", err)
+		return fmt.Errorf("failed to fetch operator state - %s", err)
 	}
-	logger.Info("Queried operator state", "count", len(operatorState.IndexedOperators))
+	operators, err := ics.GetIndexedOperators(context.Background(), currentBlock)
+	if err != nil {
+		return fmt.Errorf("failed to fetch indexed operators info - %s", err)
+	}
+	if config.OperatorId != "" {
+		operatorId, err := core.OperatorIDFromHex(config.OperatorId)
+		if err != nil {
+			return fmt.Errorf("failed to parse operator id %s - %v", config.OperatorId, err)
+		}
+		for operator := range operators {
+			if operator.Hex() != operatorId.Hex() {
+				delete(operators, operator)
+			}
+		}
+	}
+	logger.Info("Queried operator state", "count", len(operators))
 
-	semvers := semver.ScanOperators(operatorState.IndexedOperators, config.Workers, config.Timeout, logger)
+	semvers := semver.ScanOperators(operators, operatorState, config.UseRetrievalClient, config.Workers, config.Timeout, logger)
+	for semver, metrics := range semvers {
+		logger.Info("Semver Report", "semver", semver, "operators", metrics.Operators, "stake", metrics.QuorumStakePercentage)
+	}
 	displayResults(semvers)
 	return nil
 }
 
-func displayResults(results map[string]int) {
+func displayResults(results map[string]*semver.SemverMetrics) {
 	tw := table.NewWriter()
+	rowAutoMerge := table.RowConfig{AutoMerge: true}
+	tw.AppendHeader(table.Row{"semver", "install %", "operators", "quorum 0 stake %", "quorum 1 stake %", "quorum 2 stake %"}, rowAutoMerge)
+	//tw.AppendHeader(table.Row{"", "", "quorum 0", "quorum 1", "quorum 2"})
 
-	rowHeader := table.Row{"semver", "count"}
-	tw.AppendHeader(rowHeader)
-
-	total := 0
-	for semver, count := range results {
-		tw.AppendRow(table.Row{semver, count})
-		total += count
+	total_operators := 0
+	total_semver_pct := 0.0
+	total_stake_q0 := 0.0
+	total_stake_q1 := 0.0
+	total_stake_q2 := 0.0
+	for _, metrics := range results {
+		total_operators += int(metrics.Operators)
+		total_stake_q0 += metrics.QuorumStakePercentage[0]
+		total_stake_q1 += metrics.QuorumStakePercentage[1]
+		total_stake_q2 += metrics.QuorumStakePercentage[2]
 	}
-	tw.AppendFooter(table.Row{"total", total})
+	for semver, metrics := range results {
+		semver_pct := 100 * (float64(metrics.Operators) / float64(total_operators))
+		total_semver_pct += semver_pct
+		tw.AppendRow(table.Row{semver, semver_pct, metrics.Operators, metrics.QuorumStakePercentage[0], metrics.QuorumStakePercentage[1], metrics.QuorumStakePercentage[2]})
+	}
+	tw.AppendFooter(table.Row{"totals", total_semver_pct, total_operators, total_stake_q0, total_stake_q1, total_stake_q2})
+	tw.SetColumnConfigs([]table.ColumnConfig{
+		{Number: 1, AutoMerge: true},
+		{Number: 3, AlignHeader: 2},
+		{Number: 4, AlignHeader: 2},
+		{Number: 5, AlignHeader: 2},
+	})
 
 	fmt.Println(tw.Render())
 }
