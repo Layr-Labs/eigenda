7d41cf7d1621588a39aa13ec9fc35f80899655e8
Samuel Laferriere <samlaf92@gmail.com>
2024-11-01 19:39:40 +0000

    test(api): add more tests for FailoverError

--- api/errors_test.go
+++ api/errors_test.go
@@ -7,17 +7,32 @@ import (
 )
 
 func TestErrorFailoverErrorsIs(t *testing.T) {
-	baseErr := fmt.Errorf("some error")
+	baseErr := fmt.Errorf("base error")
 	failoverErr := NewErrorFailover(baseErr)
+	otherFailoverErr := NewErrorFailover(fmt.Errorf("some other error"))
+	wrappedFailoverErr := fmt.Errorf("wrapped: %w", failoverErr)
 
-	// error comparison only checks the type of the error
+	if !errors.Is(failoverErr, failoverErr) {
+		t.Error("should match itself")
+	}
+
+	if !errors.Is(failoverErr, baseErr) {
+		t.Error("should match base error")
+	}
+
+	if errors.Is(failoverErr, fmt.Errorf("some other error")) {
+		t.Error("should not match other errors")
+	}
+
+	if !errors.Is(failoverErr, otherFailoverErr) {
+		t.Error("should match other failover error")
+	}
+
 	if !errors.Is(failoverErr, &ErrorFailover{}) {
 		t.Error("should match ErrorFailover type")
 	}
 
-	// can also compare if failover error is wrapped
-	wrapped := fmt.Errorf("wrapped: %w", failoverErr)
-	if !errors.Is(wrapped, &ErrorFailover{}) {
+	if !errors.Is(wrappedFailoverErr, &ErrorFailover{}) {
 		t.Error("should match ErrorFailover type even when wrapped")
 	}
 
