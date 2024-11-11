39981e2a4084a4b665a7c552083b0532283600f8
Cody Littley <cody@eigenlabs.org>
2024-11-11 09:45:31 -0600

    Added another test. For some reason a lot of tests are failing.
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

--- relay/server_test.go
+++ relay/server_test.go
@@ -521,3 +521,406 @@ func TestBatchedReadWriteChunks(t *testing.T) {
 		}
 	}
 }
+
+func TestReadWriteChunksWithSharding(t *testing.T) {
+	tu.InitializeRandom()
+
+	logger, err := common.NewLogger(common.DefaultLoggerConfig())
+	require.NoError(t, err)
+
+	setup(t)
+	defer teardown()
+
+	// These are used to write data to S3/dynamoDB
+	metadataStore := buildMetadataStore(t)
+	chunkReader, chunkWriter := buildChunkStore(t, logger)
+
+	shardCount := rand.Intn(10) + 10
+	shardList := make([]v2.RelayKey, 0)
+	shardSet := make(map[v2.RelayKey]struct{})
+	for i := 0; i < shardCount; i++ {
+		if i%2 == 0 {
+			shardList = append(shardList, v2.RelayKey(i))
+			shardSet[v2.RelayKey(i)] = struct{}{}
+		}
+	}
+	shardMap := make(map[v2.BlobKey][]v2.RelayKey)
+
+	// This is the server used to read it back
+	config := DefaultConfig()
+	config.Shards = shardList
+	server, err := NewServer(
+		context.Background(),
+		logger,
+		config,
+		metadataStore,
+		nil, /* not used in this test*/
+		chunkReader)
+	require.NoError(t, err)
+
+	expectedData := make(map[v2.BlobKey][]*encoding.Frame)
+	fragmentInfoMap := make(map[v2.BlobKey]*encoding.FragmentInfo)
+
+	blobCount := 10
+	for i := 0; i < blobCount; i++ {
+		header, _, chunks := randomBlobChunks(t)
+
+		blobKey, err := header.BlobKey()
+		require.NoError(t, err)
+		expectedData[blobKey] = chunks
+
+		// Assign two shards to each blob.
+		shard1 := v2.RelayKey(rand.Intn(shardCount))
+		shard2 := v2.RelayKey(rand.Intn(shardCount))
+		shards := []v2.RelayKey{shard1, shard2}
+		shardMap[blobKey] = shards
+
+		coeffs, chunkProofs := disassembleFrames(chunks)
+		err = chunkWriter.PutChunkProofs(context.Background(), blobKey, chunkProofs)
+		require.NoError(t, err)
+		fragmentInfo, err := chunkWriter.PutChunkCoefficients(context.Background(), blobKey, coeffs)
+		require.NoError(t, err)
+		fragmentInfoMap[blobKey] = fragmentInfo
+
+		err = metadataStore.PutBlobCertificate(
+			context.Background(),
+			&v2.BlobCertificate{
+				BlobHeader: header,
+				RelayKeys:  shards,
+			},
+			&encoding.FragmentInfo{
+				TotalChunkSizeBytes: fragmentInfo.TotalChunkSizeBytes,
+				FragmentSizeBytes:   fragmentInfo.FragmentSizeBytes,
+			})
+		require.NoError(t, err)
+	}
+
+	// Request the entire blob by range. 25% of the blobs will be assigned to shards we don't have.
+	for key, data := range expectedData {
+		requestedChunks := make([]*pb.ChunkRequest, 0)
+		requestedChunks = append(requestedChunks, &pb.ChunkRequest{
+			Request: &pb.ChunkRequest_ByRange{
+				ByRange: &pb.ChunkRequestByRange{
+					BlobKey:    key[:],
+					StartIndex: 0,
+					EndIndex:   uint32(len(data)),
+				},
+			},
+		})
+		request := &pb.GetChunksRequest{
+			ChunkRequests: requestedChunks,
+		}
+
+		isBlobInCorrectShard := false
+		blobShards := shardMap[key]
+		for _, shard := range blobShards {
+			if _, ok := shardSet[shard]; ok {
+				isBlobInCorrectShard = true
+				break
+			}
+		}
+
+		response, err := server.GetChunks(context.Background(), request)
+
+		if isBlobInCorrectShard {
+			require.NoError(t, err)
+
+			require.Equal(t, 1, len(response.Data))
+
+			for i, frame := range response.Data[0].Data {
+				convertedFrame := encoding.FrameFromProtobuf(frame)
+				require.Equal(t, data[i], convertedFrame)
+			}
+		} else {
+			require.Error(t, err)
+			require.Nil(t, response)
+		}
+	}
+
+	// Request the entire blob by index
+	for key, data := range expectedData {
+		requestedChunks := make([]*pb.ChunkRequest, 0)
+
+		indices := make([]uint32, len(data))
+		for i := range data {
+			indices[i] = uint32(i)
+		}
+
+		requestedChunks = append(requestedChunks, &pb.ChunkRequest{
+			Request: &pb.ChunkRequest_ByIndex{
+				ByIndex: &pb.ChunkRequestByIndex{
+					BlobKey:      key[:],
+					ChunkIndices: indices,
+				},
+			},
+		})
+		request := &pb.GetChunksRequest{
+			ChunkRequests: requestedChunks,
+		}
+
+		isBlobInCorrectShard := false
+		blobShards := shardMap[key]
+		for _, shard := range blobShards {
+			if _, ok := shardSet[shard]; ok {
+				isBlobInCorrectShard = true
+				break
+			}
+		}
+
+		if isBlobInCorrectShard {
+			response, err := server.GetChunks(context.Background(), request)
+			require.NoError(t, err)
+
+			require.Equal(t, 1, len(response.Data))
+
+			for i, frame := range response.Data[0].Data {
+				convertedFrame := encoding.FrameFromProtobuf(frame)
+				require.Equal(t, data[i], convertedFrame)
+			}
+		} else {
+			response, err := server.GetChunks(context.Background(), request)
+			require.Error(t, err)
+			require.Nil(t, response)
+		}
+	}
+
+	// Request part of the blob back by range
+	for key, data := range expectedData {
+		requestedChunks := make([]*pb.ChunkRequest, 0)
+
+		startIndex := rand.Intn(len(data))
+		var endIndex int
+		if startIndex == len(data)-1 {
+			endIndex = len(data)
+		} else {
+			endIndex = startIndex + rand.Intn(len(data)-startIndex)
+		}
+
+		requestedChunks = append(requestedChunks, &pb.ChunkRequest{
+			Request: &pb.ChunkRequest_ByRange{
+				ByRange: &pb.ChunkRequestByRange{
+					BlobKey:    key[:],
+					StartIndex: uint32(startIndex),
+					EndIndex:   uint32(endIndex),
+				},
+			},
+		})
+		request := &pb.GetChunksRequest{
+			ChunkRequests: requestedChunks,
+		}
+
+		isBlobInCorrectShard := false
+		blobShards := shardMap[key]
+		for _, shard := range blobShards {
+			if _, ok := shardSet[shard]; ok {
+				isBlobInCorrectShard = true
+				break
+			}
+		}
+
+		if isBlobInCorrectShard {
+			response, err := server.GetChunks(context.Background(), request)
+			require.NoError(t, err)
+
+			require.Equal(t, 1, len(response.Data))
+
+			for i := startIndex; i < endIndex; i++ {
+				convertedFrame := encoding.FrameFromProtobuf(response.Data[0].Data[i-startIndex])
+				require.Equal(t, data[i], convertedFrame)
+			}
+		}
+	}
+
+	// Request part of the blob back by index
+	for key, data := range expectedData {
+		requestedChunks := make([]*pb.ChunkRequest, 0)
+
+		indices := make([]uint32, 0)
+		for i := range data {
+			if i%2 == 0 {
+				indices = append(indices, uint32(i))
+			}
+		}
+
+		requestedChunks = append(requestedChunks, &pb.ChunkRequest{
+			Request: &pb.ChunkRequest_ByIndex{
+				ByIndex: &pb.ChunkRequestByIndex{
+					BlobKey:      key[:],
+					ChunkIndices: indices,
+				},
+			},
+		})
+		request := &pb.GetChunksRequest{
+			ChunkRequests: requestedChunks,
+		}
+
+		isBlobInCorrectShard := false
+		blobShards := shardMap[key]
+		for _, shard := range blobShards {
+			if _, ok := shardSet[shard]; ok {
+				isBlobInCorrectShard = true
+				break
+			}
+		}
+
+		if isBlobInCorrectShard {
+			response, err := server.GetChunks(context.Background(), request)
+			require.NoError(t, err)
+
+			require.Equal(t, 1, len(response.Data))
+
+			for i := 0; i < len(indices); i++ {
+				if i%2 == 0 {
+					convertedFrame := encoding.FrameFromProtobuf(response.Data[0].Data[i/2])
+					require.Equal(t, data[indices[i]], convertedFrame)
+				}
+			}
+		} else {
+			response, err := server.GetChunks(context.Background(), request)
+			require.Error(t, err)
+			require.Nil(t, response)
+		}
+	}
+}
+
+func TestBatchedReadWriteChunksWithSharding(t *testing.T) {
+	tu.InitializeRandom()
+
+	logger, err := common.NewLogger(common.DefaultLoggerConfig())
+	require.NoError(t, err)
+
+	setup(t)
+	defer teardown()
+
+	// These are used to write data to S3/dynamoDB
+	metadataStore := buildMetadataStore(t)
+	chunkReader, chunkWriter := buildChunkStore(t, logger)
+
+	shardCount := rand.Intn(10) + 10
+	shardList := make([]v2.RelayKey, 0)
+	shardSet := make(map[v2.RelayKey]struct{})
+	for i := 0; i < shardCount; i++ {
+		if i%2 == 0 {
+			shardList = append(shardList, v2.RelayKey(i))
+			shardSet[v2.RelayKey(i)] = struct{}{}
+		}
+	}
+	shardMap := make(map[v2.BlobKey][]v2.RelayKey)
+
+	// This is the server used to read it back
+	config := DefaultConfig()
+	config.Shards = shardList
+	server, err := NewServer(
+		context.Background(),
+		logger,
+		config,
+		metadataStore,
+		nil, /* not used in this test */
+		chunkReader)
+	require.NoError(t, err)
+
+	expectedData := make(map[v2.BlobKey][]*encoding.Frame)
+	fragmentInfoMap := make(map[v2.BlobKey]*encoding.FragmentInfo)
+
+	blobCount := 10
+	for i := 0; i < blobCount; i++ {
+		header, _, chunks := randomBlobChunks(t)
+
+		blobKey, err := header.BlobKey()
+		require.NoError(t, err)
+		expectedData[blobKey] = chunks
+
+		coeffs, chunkProofs := disassembleFrames(chunks)
+		err = chunkWriter.PutChunkProofs(context.Background(), blobKey, chunkProofs)
+		require.NoError(t, err)
+		fragmentInfo, err := chunkWriter.PutChunkCoefficients(context.Background(), blobKey, coeffs)
+		require.NoError(t, err)
+		fragmentInfoMap[blobKey] = fragmentInfo
+
+		// Assign two shards to each blob.
+		shard1 := v2.RelayKey(rand.Intn(shardCount))
+		shard2 := v2.RelayKey(rand.Intn(shardCount))
+		shards := []v2.RelayKey{shard1, shard2}
+		shardMap[blobKey] = shards
+
+		err = metadataStore.PutBlobCertificate(
+			context.Background(),
+			&v2.BlobCertificate{
+				BlobHeader: header,
+				RelayKeys:  shards,
+			},
+			&encoding.FragmentInfo{
+				TotalChunkSizeBytes: fragmentInfo.TotalChunkSizeBytes,
+				FragmentSizeBytes:   fragmentInfo.FragmentSizeBytes,
+			})
+		require.NoError(t, err)
+	}
+
+	keyCount := 2
+
+	// Read the blobs back. On average, we expect 25% of the blobs to be assigned to shards we don't have.
+	for i := 0; i < 10; i++ {
+		keys := make([]v2.BlobKey, 0, keyCount)
+		for key := range expectedData {
+			keys = append(keys, key)
+			if len(keys) == keyCount {
+				break
+			}
+		}
+
+		requestedChunks := make([]*pb.ChunkRequest, 0)
+		for _, key := range keys {
+
+			boundKey := key
+			request := &pb.ChunkRequest{
+				Request: &pb.ChunkRequest_ByRange{
+					ByRange: &pb.ChunkRequestByRange{
+						BlobKey:    boundKey[:],
+						StartIndex: 0,
+						EndIndex:   uint32(len(expectedData[key])),
+					},
+				},
+			}
+
+			requestedChunks = append(requestedChunks, request)
+		}
+		request := &pb.GetChunksRequest{
+			ChunkRequests: requestedChunks,
+		}
+
+		allInCorrectShard := true
+		for _, key := range keys {
+			isBlobInCorrectShard := false
+			blobShards := shardMap[key]
+			for _, shard := range blobShards {
+				if _, ok := shardSet[shard]; ok {
+					isBlobInCorrectShard = true
+					break
+				}
+			}
+			if !isBlobInCorrectShard {
+				allInCorrectShard = false
+				break
+			}
+		}
+
+		response, err := server.GetChunks(context.Background(), request)
+
+		if allInCorrectShard {
+			require.NoError(t, err)
+
+			require.Equal(t, keyCount, len(response.Data))
+
+			for keyIndex, key := range keys {
+				data := expectedData[key]
+				for frameIndex, frame := range response.Data[keyIndex].Data {
+					convertedFrame := encoding.FrameFromProtobuf(frame)
+					require.Equal(t, data[frameIndex], convertedFrame)
+				}
+			}
+		} else {
+			require.Error(t, err)
+			require.Nil(t, response)
+		}
+	}
+}
