e39eaf9f20339237fee6b642c73816b22825f3d5
Cody Littley <cody@eigenlabs.org>
2024-11-15 13:34:20 -0600

    Incremental progress.
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

--- relay/limiter/blob_rate_limiter_test.go
+++ relay/limiter/blob_rate_limiter_test.go
@@ -1,1 +1,24 @@
 package limiter
+
+import (
+	"testing"
+)
+
+func TestConcurrentBlobOperations(t *testing.T) {
+
+	//concurrencyLimit := 10
+	//
+	//config := DefaultConfig()
+	//config.MaxConcurrentGetBlobOps = concurrencyLimit
+	//
+	//limiter := NewBlobRateLimiter(config)
+	//
+	//// time starts at current time, but advances manually afterward
+	//now := time.Now()
+	//
+	//// We should be able to start 10 operations concurrently
+	//for i := 0; i < concurrencyLimit; i++ {
+	//
+	//}
+
+}
