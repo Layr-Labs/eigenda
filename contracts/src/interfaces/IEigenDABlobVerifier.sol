8937de02af852c633fbd303bf4d64e9335ae08b7
QUAQ <archk.latham@gmail.com>
2024-11-20 16:40:04 -0600

    rm binding update

--- contracts/src/interfaces/IEigenDABlobVerifier.sol
+++ contracts/src/interfaces/IEigenDABlobVerifier.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import {IEigenDAServiceManager} from "./IEigenDAServiceManager.sol";
-import {EigenDABlobVerificationUtils} from "../libraries/EigenDABlobVerificationUtils.sol";
 import {IEigenDAThresholdRegistry} from "./IEigenDAThresholdRegistry.sol";
 import "./IEigenDAStructs.sol";
 
