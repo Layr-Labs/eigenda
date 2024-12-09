fe93aa126ca1e3ad20172916c601e410d8927630
Cody Littley <cody@eigenlabs.org>
2024-12-09 14:51:59 -0600

    Removed metrics wrapper from the relay.
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

--- relay/metrics/metrics.go
+++ relay/metrics/metrics.go
@@ -1,15 +1,22 @@
 package metrics
 
 import (
+	"fmt"
 	"github.com/Layr-Labs/eigenda/common/metrics"
 	"github.com/Layr-Labs/eigenda/relay/cache"
 	"github.com/Layr-Labs/eigensdk-go/logging"
 	grpcprom "github.com/grpc-ecosystem/go-grpc-middleware/providers/prometheus"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"google.golang.org/grpc"
+	"net/http"
 )
 
+const namespace := "eigenda_relay"
+
 type RelayMetrics struct {
-	metricsServer    metrics.Metrics
 	grpcServerOption grpc.ServerOption
 
 	// Cache metrics
@@ -18,43 +25,47 @@ type RelayMetrics struct {
 	BlobCacheMetrics     *cache.CacheAccessorMetrics
 
 	// GetChunks metrics
-	GetChunksLatency               metrics.LatencyMetric
-	GetChunksAuthenticationLatency metrics.LatencyMetric
-	GetChunksMetadataLatency       metrics.LatencyMetric
-	GetChunksDataLatency           metrics.LatencyMetric
-	GetChunksAuthFailures          metrics.CountMetric
-	GetChunksRateLimited           metrics.CountMetric
-	GetChunksKeyCount              metrics.GaugeMetric
-	GetChunksDataSize              metrics.GaugeMetric
+	GetChunksLatency               *prometheus.SummaryVec
+	GetChunksAuthenticationLatency *prometheus.SummaryVec
+	GetChunksMetadataLatency       *prometheus.SummaryVec
+	GetChunksDataLatency           *prometheus.SummaryVec
+	GetChunksAuthFailures          *prometheus.CounterVec
+	GetChunksRateLimited           *prometheus.CounterVec
+	GetChunksKeyCount              *prometheus.GaugeVec
+	GetChunksDataSize              *prometheus.GaugeVec
 
 	// GetBlob metrics
-	GetBlobLatency         metrics.LatencyMetric
-	GetBlobMetadataLatency metrics.LatencyMetric
-	GetBlobDataLatency     metrics.LatencyMetric
-	GetBlobRateLimited     metrics.CountMetric
-	GetBlobDataSize        metrics.GaugeMetric
-}
-
-type RateLimitLabel struct {
-	Reason string
+	GetBlobLatency         *prometheus.SummaryVec
+	GetBlobMetadataLatency *prometheus.SummaryVec
+	GetBlobDataLatency     *prometheus.SummaryVec
+	GetBlobRateLimited     *prometheus.CounterVec
+	GetBlobDataSize        *prometheus.GaugeVec
 }
 
 // NewRelayMetrics creates a new RelayMetrics instance, which encapsulates all metrics related to the relay.
 func NewRelayMetrics(logger logging.Logger, port int) (*RelayMetrics, error) {
 
-	server := metrics.NewMetrics(logger, "relay", port)
+	reg := prometheus.NewRegistry()
+	reg.MustRegister(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}))
+	reg.MustRegister(collectors.NewGoCollector())
+
+	logger.Infof("Starting metrics server at port %d", port)
+	addr := fmt.Sprintf(":%d", port)
+	mux := http.NewServeMux()
+	mux.Handle("/metrics", promhttp.HandlerFor(
+		reg,
+		promhttp.HandlerOpts{},
+	))
+	server := &http.Server{
+		Addr:    addr,
+		Handler: mux,
+	}
 
 	grpcMetrics := grpcprom.NewServerMetrics()
-	server.RegisterExternalMetrics(grpcMetrics)
+	reg.MustRegister(grpcMetrics)
 	grpcServerOption := grpc.UnaryInterceptor(
 		grpcMetrics.UnaryServerInterceptor(),
 	)
-
-	standardQuantiles := []*metrics.Quantile{
-		metrics.NewQuantile(0.5),
-		metrics.NewQuantile(0.9),
-		metrics.NewQuantile(0.99),
-	}
 
 	metadataCacheMetrics, err := cache.NewCacheAccessorMetrics(server, "metadata")
 	if err != nil {
@@ -71,137 +82,148 @@ func NewRelayMetrics(logger logging.Logger, port int) (*RelayMetrics, error) {
 		return nil, err
 	}
 
-	getChunksLatencyMetric, err := server.NewLatencyMetric(
-		"get_chunks_latency",
-		"Latency of the GetChunks RPC",
-		nil,
-		standardQuantiles...)
-	if err != nil {
-		return nil, err
-	}
+	objectives := map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001}
 
-	getChunksAuthenticationLatencyMetric, err := server.NewLatencyMetric(
-		"get_chunks_authentication_latency",
-		"Latency of the GetChunks RPC client authentication",
-		nil,
-		standardQuantiles...)
-	if err != nil {
-		return nil, err
-	}
+	getChunksLatency := promauto.With(reg).NewSummaryVec(
+		prometheus.SummaryOpts{
+			Namespace:  namespace,
+			Name:       "get_chunks_latency_ms",
+			Help:       "Latency of the GetChunks RPC",
+			Objectives: objectives,
+		},
+		[]string{},
+	)
 
-	getChunksMetadataLatencyMetric, err := server.NewLatencyMetric(
-		"get_chunks_metadata_latency",
-		"Latency of the GetChunks RPC metadata retrieval",
-		nil,
-		standardQuantiles...)
-	if err != nil {
-		return nil, err
-	}
+	getChunksAuthenticationLatency := promauto.With(reg).NewSummaryVec(
+		prometheus.SummaryOpts{
+			Namespace:  namespace,
+			Name:       "get_chunks_authentication_latency_ms",
+			Help:       "Latency of the GetChunks RPC client authentication",
+			Objectives: objectives,
+		},
+		[]string{},
+	)
 
-	getChunksDataLatencyMetric, err := server.NewLatencyMetric(
-		"get_chunks_data_latency",
-		"Latency of the GetChunks RPC data retrieval",
-		nil,
-		standardQuantiles...)
-	if err != nil {
-		return nil, err
-	}
+	getChunksMetadataLatency := promauto.With(reg).NewSummaryVec(
+		prometheus.SummaryOpts{
+			Namespace:  namespace,
+			Name:       "get_chunks_metadata_latency_ms",
+			Help:       "Latency of the GetChunks RPC metadata retrieval",
+			Objectives: objectives,
+		},
+		[]string{},
+	)
 
-	getChunksAuthFailures, err := server.NewCountMetric(
-		"get_chunks_auth_failure",
-		"Number of GetChunks RPC authentication failures",
-		nil)
-	if err != nil {
-		return nil, err
-	}
+	getChunksDataLatency := promauto.With(reg).NewSummaryVec(
+		prometheus.SummaryOpts{
+			Namespace:  namespace,
+			Name:       "get_chunks_data_latency_ms",
+			Help:       "Latency of the GetChunks RPC data retrieval",
+			Objectives: objectives,
+		},
+		[]string{},
+	)
 
-	getChunksRateLimited, err := server.NewCountMetric(
-		"get_chunks_rate_limited",
-		"Number of GetChunks RPC rate limited",
-		RateLimitLabel{})
-	if err != nil {
-		return nil, err
-	}
+	getChunksAuthFailures := promauto.With(reg).NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: namespace,
+			Name:      "get_chunks_auth_failure_count",
+			Help:      "Number of GetChunks RPC authentication failures",
+		},
+		[]string{},
+	)
 
-	getChunksKeyCount, err := server.NewGaugeMetric(
-		"get_chunks_key",
-		"count",
-		"Number of keys in a GetChunks request.",
-		nil)
-	if err != nil {
-		return nil, err
-	}
+	getChunksRateLimited := promauto.With(reg).NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: namespace,
+			Name:      "get_chunks_rate_limited_count",
+			Help:      "Number of GetChunks RPC rate limited",
+		},
+		[]string{"reason"},
+	)
 
-	getChunksDataSize, err := server.NewGaugeMetric(
-		"get_chunks_data_size",
-		"bytes",
-		"Data size in a GetChunks request.",
-		nil)
-	if err != nil {
-		return nil, err
-	}
+	getChunksKeyCount := promauto.With(reg).NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      "get_chunks_key_count",
+			Help:      "Number of keys in a GetChunks request.",
+		},
+		[]string{},
+	)
 
-	getBlobLatencyMetric, err := server.NewLatencyMetric(
-		"get_blob_latency",
-		"Latency of the GetBlob RPC",
-		nil,
-		standardQuantiles...)
-	if err != nil {
-		return nil, err
-	}
+	getChunksDataSize := promauto.With(reg).NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      "get_chunks_data_size_bytes",
+			Help:      "Data size in a GetChunks request.",
+		},
+		[]string{},
+	)
 
-	getBlobMetadataLatencyMetric, err := server.NewLatencyMetric(
-		"get_blob_metadata_latency",
-		"Latency of the GetBlob RPC metadata retrieval",
-		nil,
-		standardQuantiles...)
-	if err != nil {
-		return nil, err
-	}
+	getBlobLatency := promauto.With(reg).NewSummaryVec(
+		prometheus.SummaryOpts{
+			Namespace:  namespace,
+			Name:       "get_blob_latency_ms",
+			Help:       "Latency of the GetBlob RPC",
+			Objectives: objectives,
+		},
+		[]string{},
+	)
 
-	getBlobDataLatencyMetric, err := server.NewLatencyMetric(
-		"get_blob_data_latency",
-		"Latency of the GetBlob RPC data retrieval",
-		nil,
-		standardQuantiles...)
-	if err != nil {
-		return nil, err
-	}
+	getBlobMetadataLatency := promauto.With(reg).NewSummaryVec(
+		prometheus.SummaryOpts{
+			Namespace:  namespace,
+			Name:       "get_blob_metadata_latency_ms",
+			Help:       "Latency of the GetBlob RPC metadata retrieval",
+			Objectives: objectives,
+		},
+		[]string{},
+	)
 
-	getBlobRateLimited, err := server.NewCountMetric(
-		"get_blob_rate_limited",
-		"Number of GetBlob RPC rate limited",
-		RateLimitLabel{})
-	if err != nil {
-		return nil, err
-	}
+	getBlobDataLatency := promauto.With(reg).NewSummaryVec(
+		prometheus.SummaryOpts{
+			Namespace:  namespace,
+			Name:       "get_blob_data_latency_ms",
+			Help:       "Latency of the GetBlob RPC data retrieval",
+			Objectives: objectives,
+		},
+		[]string{},
+	)
 
-	getBlobDataSize, err := server.NewGaugeMetric(
-		"get_blob_data_size",
-		"bytes",
-		"Data size of requested blobs.",
-		nil)
-	if err != nil {
-		return nil, err
-	}
+	getBlobRateLimited := promauto.With(reg).NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: namespace,
+			Name:      "get_blob_rate_limited_count",
+			Help:      "Number of GetBlob RPC rate limited",
+		},
+		[]string{"reason"},
+	)
+
+	getBlobDataSize := promauto.With(reg).NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      "get_blob_data_size_bytes",
+			Help:      "Data size of requested blobs.",
+		},
+		[]string{},
+	)
 
 	return &RelayMetrics{
-		metricsServer:                  server,
-		MetadataCacheMetrics:           metadataCacheMetrics,
-		ChunkCacheMetrics:              chunkCacheMetrics,
-		BlobCacheMetrics:               blobCacheMetrics,
-		grpcServerOption:               grpcServerOption,
-		GetChunksLatency:               getChunksLatencyMetric,
-		GetChunksAuthenticationLatency: getChunksAuthenticationLatencyMetric,
-		GetChunksMetadataLatency:       getChunksMetadataLatencyMetric,
-		GetChunksDataLatency:           getChunksDataLatencyMetric,
+		grpcServerOption: grpcServerOption,
+		MetadataCacheMetrics: metadataCacheMetrics,
+		ChunkCacheMetrics:    chunkCacheMetrics,
+		BlobCacheMetrics:     blobCacheMetrics,
+		GetChunksLatency:               getChunksLatency,
+		GetChunksAuthenticationLatency: getChunksAuthenticationLatency,
+		GetChunksMetadataLatency:       getChunksMetadataLatency,
+		GetChunksDataLatency:           getChunksDataLatency,
 		GetChunksAuthFailures:          getChunksAuthFailures,
 		GetChunksRateLimited:           getChunksRateLimited,
 		GetChunksKeyCount:              getChunksKeyCount,
 		GetChunksDataSize:              getChunksDataSize,
-		GetBlobLatency:                 getBlobLatencyMetric,
-		GetBlobMetadataLatency:         getBlobMetadataLatencyMetric,
-		GetBlobDataLatency:             getBlobDataLatencyMetric,
+		GetBlobLatency:                 getBlobLatency,
+		GetBlobMetadataLatency:         getBlobMetadataLatency,
+		GetBlobDataLatency:             getBlobDataLatency,
 		GetBlobRateLimited:             getBlobRateLimited,
 		GetBlobDataSize:                getBlobDataSize,
 	}, nil
