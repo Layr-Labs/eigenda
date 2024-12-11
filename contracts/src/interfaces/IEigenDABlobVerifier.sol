3ed9ef6ed3eb72c46ce3050eb84af28f0afdfae2
quaq <56312047+0x0aa0@users.noreply.github.com>
2024-12-11 11:50:02 -0600

    V2 Blob Verification (#781)

--- contracts/src/interfaces/IEigenDABlobVerifier.sol
+++ contracts/src/interfaces/IEigenDABlobVerifier.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import {IEigenDAThresholdRegistry} from "./IEigenDAThresholdRegistry.sol";
 import "./IEigenDAStructs.sol";
-import "./IEigenDAThresholdRegistry.sol";
 
 interface IEigenDABlobVerifier is IEigenDAThresholdRegistry {
 
@@ -16,17 +16,6 @@ interface IEigenDABlobVerifier is IEigenDAThresholdRegistry {
         BlobVerificationProof calldata blobVerificationProof
     ) external view;
 
-    /**
-     * @notice Verifies that a blob is valid for the required quorums and additional quorums
-     * @param blobHeader The blob header to verify
-     * @param blobVerificationProof The blob verification proof to verify the blob against
-     * @param additionalQuorumNumbersRequired The additional required quorum numbers 
-     */
-    function verifyBlobV1(
-        BlobHeader calldata blobHeader,
-        BlobVerificationProof calldata blobVerificationProof,
-        bytes calldata additionalQuorumNumbersRequired
-    ) external view;
 
     /**
      * @notice Verifies a batch of blobs for the required quorums
@@ -37,19 +26,6 @@ interface IEigenDABlobVerifier is IEigenDAThresholdRegistry {
         BlobHeader[] calldata blobHeaders,
         BlobVerificationProof[] calldata blobVerificationProofs
     ) external view;
-
-    /**
-     * @notice Verifies a batch of blobs for the required quorums and additional quorums
-     * @param blobHeaders The blob headers to verify
-     * @param blobVerificationProofs The blob verification proofs to verify the blobs against
-     * @param additionalQuorumNumbersRequired The additional required quorum numbers
-     */
-    function verifyBlobsV1(
-        BlobHeader[] calldata blobHeaders,
-        BlobVerificationProof[] calldata blobVerificationProofs,
-        bytes calldata additionalQuorumNumbersRequired
-    ) external view;
-
 
     /**
      * @notice Verifies a blob for the required quorums and the default security thresholds
@@ -64,59 +40,21 @@ interface IEigenDABlobVerifier is IEigenDAThresholdRegistry {
     ) external view;
 
     /**
-     * @notice Verifies a blob for the required quorums and additional quorums
-     * @param batchHeader The batch header of the blob
+     * @notice Verifies a blob for the base required quorums and the default security thresholds
+     * @param signedBatch The signed batch to verify the blob against
      * @param blobVerificationProof The blob verification proof for the blob
-     * @param nonSignerStakesAndSignature The nonSignerStakesAndSignature to verify the blob against
-     * @param additionalQuorumNumbersRequired The additional required quorum numbers
      */
-    function verifyBlobV2(
-        BatchHeaderV2 calldata batchHeader,
-        BlobVerificationProofV2 calldata blobVerificationProof,
-        NonSignerStakesAndSignature calldata nonSignerStakesAndSignature,
-        bytes calldata additionalQuorumNumbersRequired
-    ) external view;
-
-    /**
-     * @notice Verifies a blob for the required quorums and additional quorums and a custom security threshold
-     * @param batchHeader The batch header of the blob
-     * @param blobVerificationProof The blob verification proof for the blob
-     * @param nonSignerStakesAndSignature The nonSignerStakesAndSignature to verify the blob against
-     * @param securityThreshold The custom security threshold to verify the blob against
-     * @param additionalQuorumNumbersRequired The additional required quorum numbers
-     */
-    function verifyBlobV2(
-        BatchHeaderV2 calldata batchHeader,
-        BlobVerificationProofV2 calldata blobVerificationProof,
-        NonSignerStakesAndSignature calldata nonSignerStakesAndSignature,
-        SecurityThresholds memory securityThreshold,
-        bytes calldata additionalQuorumNumbersRequired
-    ) external view;
-
-    /**
-     * @notice Verifies a batch of blobs for the required quorums and additional quorums and a set of custom security threshold
-     * @param batchHeader The batch headers of the blobs
-     * @param blobVerificationProof The blob verification proofs for the blobs
-     * @param nonSignerStakesAndSignature The nonSignerStakesAndSignatures to verify the blobs against
-     * @param securityThresholds The set of custom security thresholds to verify the blobs against
-     * @param additionalQuorumNumbersRequired The additional required quorum numbers
-     */
-    function verifyBlobV2(
-        BatchHeaderV2 calldata batchHeader,
-        BlobVerificationProofV2 calldata blobVerificationProof,
-        NonSignerStakesAndSignature calldata nonSignerStakesAndSignature,
-        SecurityThresholds[] memory securityThresholds,
-        bytes calldata additionalQuorumNumbersRequired
+    function verifyBlobV2FromSignedBatch(
+        SignedBatch calldata signedBatch,
+        BlobVerificationProofV2 calldata blobVerificationProof
     ) external view;
 
     /**
      * @notice Returns the nonSignerStakesAndSignature for a given blob and signed batch
      * @param signedBatch The signed batch to get the nonSignerStakesAndSignature for
-     * @param blobHeader The blob header of the blob in the signed batch
      */
     function getNonSignerStakesAndSignature(
-        SignedBatch calldata signedBatch,
-        BlobHeaderV2 calldata blobHeader
+        SignedBatch calldata signedBatch
     ) external view returns (NonSignerStakesAndSignature memory);
 
     /**
