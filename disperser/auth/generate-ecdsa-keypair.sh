29e7e7d16bb88ed9b993e946200fdb2bd5dd9880
Cody Littley <cody@eigenlabs.org>
2024-12-12 11:16:30 -0600

    Delete stale code.
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

--- disperser/auth/generate-ecdsa-keypair.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# This script generates a new ECDSA keypair for the disperser service.
-
-openssl ecparam -name prime256v1 -genkey -noout -out eigenda-disperser-private.pem
-openssl ec -in eigenda-disperser-private.pem -pubout -out eigenda-disperser-public.pem
