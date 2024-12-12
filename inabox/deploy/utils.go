168ded5d45ec96b943eac0126612ccb137ff358d
Cody Littley <cody@eigenlabs.org>
2024-12-12 08:40:08 -0600

    Debug
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

--- inabox/deploy/utils.go
+++ inabox/deploy/utils.go
@@ -306,8 +306,8 @@ func execCmd(name string, args []string, envVars []string) error {
 	var out bytes.Buffer
 	var stderr bytes.Buffer
 	// TODO: When these are uncommented, the deployer sometimes fails to start anvil
-	cmd.Stdout = &out
-	cmd.Stderr = &stderr
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
 	err := cmd.Run()
 	if err != nil {
