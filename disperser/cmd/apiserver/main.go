8938f25698dc0a3c42e13407059576008bf32546
hopeyen <hopey1118@gmail.com>
2024-12-13 15:52:20 -0800

    temp: adding logs for debugging

--- disperser/cmd/apiserver/main.go
+++ disperser/cmd/apiserver/main.go
@@ -104,11 +104,6 @@ func RunDisperserServer(ctx *cli.Context) error {
 		}
 
 		logger.Info("Creating onchain payment state")
-		blockNumber, err := transactor.GetCurrentBlockNumber(context.Background())
-		if err != nil {
-			return fmt.Errorf("failed to get current block number: %w", err)
-		}
-		logger.Info("current block number", "block", blockNumber)
 		paymentChainState, err := mt.NewOnchainPaymentState(context.Background(), transactor)
 		if err != nil {
 			return fmt.Errorf("failed to create onchain payment state: %w", err)
