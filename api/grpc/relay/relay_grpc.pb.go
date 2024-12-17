722070bc85f67fa52205620a3a81952ded389dd3
Ian Shim <100327837+ian-shim@users.noreply.github.com>
2024-12-17 13:46:19 -0800

    Assign relay proto its own package (#1025)

--- api/grpc/relay/relay_grpc.pb.go
+++ api/grpc/relay/relay_grpc.pb.go
@@ -19,8 +19,8 @@ import (
 const _ = grpc.SupportPackageIsVersion7
 
 const (
-	Relay_GetBlob_FullMethodName   = "/node.Relay/GetBlob"
-	Relay_GetChunks_FullMethodName = "/node.Relay/GetChunks"
+	Relay_GetBlob_FullMethodName   = "/relay.Relay/GetBlob"
+	Relay_GetChunks_FullMethodName = "/relay.Relay/GetChunks"
 )
 
 // RelayClient is the client API for Relay service.
@@ -133,7 +133,7 @@ func _Relay_GetChunks_Handler(srv interface{}, ctx context.Context, dec func(int
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
 var Relay_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "node.Relay",
+	ServiceName: "relay.Relay",
 	HandlerType: (*RelayServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
