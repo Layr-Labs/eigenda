2a2350d42c13b318de54377c75a3b895d5c91f3d
QUAQ <archk.latham@gmail.com>
2024-12-18 20:14:40 -0600

    disperser test

---
+++ contracts/test/unit/EigenDADisperserRegistryUnit.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.12;
+
+import "../MockEigenDADeployer.sol";
+
+contract EigenDADisperserRegistryUnit is MockEigenDADeployer {
+
+    event DisperserAdded(uint32 indexed key, address indexed disperser);
+
+    function setUp() virtual public {
+        _deployDA();
+    }
+
+    function test_initalize() public {
+        require(eigenDADisperserRegistry.owner() == registryCoordinatorOwner, "EigenDADisperserRegistry: owner is not set");
+        vm.expectRevert("Initializable: contract is already initialized");
+        eigenDADisperserRegistry.initialize(address(this));
+    }
+
+    function test_setDisperserInfo() public {
+        uint32 disperserKey = 1;
+        address disperserAddress = address(uint160(uint256(keccak256(abi.encodePacked("disperser")))));
+        DisperserInfo memory disperserInfo = DisperserInfo({
+            disperserAddress: disperserAddress
+        });
+
+        vm.expectEmit(address(eigenDADisperserRegistry));
+        emit DisperserAdded(disperserKey, disperserAddress);
+        vm.prank(registryCoordinatorOwner);
+        eigenDADisperserRegistry.setDisperserInfo(disperserKey, disperserInfo);
+
+        assertEq(eigenDADisperserRegistry.disperserKeyToAddress(disperserKey), disperserAddress);
+    }
+
+    function test_setDisperserInfo_revert_notOwner() public {
+        uint32 disperserKey = 1;
+        address disperserAddress = address(uint160(uint256(keccak256(abi.encodePacked("disperser")))));
+        DisperserInfo memory disperserInfo = DisperserInfo({
+            disperserAddress: disperserAddress
+            });
+
+        vm.expectRevert("Ownable: caller is not the owner");
+        eigenDADisperserRegistry.setDisperserInfo(disperserKey, disperserInfo);
+    }
+}
