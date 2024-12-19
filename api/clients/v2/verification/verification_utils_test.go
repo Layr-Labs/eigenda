8881a19bdc24541744cd3f2619d11e81df2d9c98
litt3 <102969658+litt3@users.noreply.github.com>
2024-12-19 16:11:48 -0500

    Fix conflicts with upstream
    Signed-off-by: litt3 <102969658+litt3@users.noreply.github.com>

--- api/clients/v2/verification/verification_utils_test.go
+++ api/clients/v2/verification/verification_utils_test.go
@@ -17,8 +17,6 @@ var (
 		[]byte("Fourscore and seven years ago our fathers brought forth, on this continent, a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal. Now we are engaged in a great civil war, testing whether that nation, or any nation so conceived, and so dedicated, can long endure. We are met on a great battle-field of that war. We have come to dedicate a portion of that field, as a final resting-place for those who here gave their lives, that that nation might live. It is altogether fitting and proper that we should do this. But, in a larger sense, we cannot dedicate, we cannot consecrate—we cannot hallow—this ground. The brave men, living and dead, who struggled here, have consecrated it far above our poor power to add or detract. The world will little note, nor long remember what we say here, but it can never forget what they did here. It is for us the living, rather, to be dedicated here to the unfinished work which they who fought here have thus far so nobly advanced. It is rather for us to be here dedicated to the great task remaining before us—that from these honored dead we take increased devotion to that cause for which they here gave the last full measure of devotion—that we here highly resolve that these dead shall not have died in vain—that this nation, under God, shall have a new birth of freedom, and that government of the people, by the people, for the people, shall not perish from the earth."))
 
 	kzgConfig       *kzg.KzgConfig
-	kzgVerifier     *verifier.Verifier
-	testRandom      *random.TestRandom
 	srsNumberToLoad uint64
 )
 
@@ -28,21 +26,19 @@ func setup() {
 	srsNumberToLoad = 2900
 
 	kzgConfig = &kzg.KzgConfig{
-		G1Path:          "../../../inabox/resources/kzg/g1.point",
-		G2Path:          "../../../inabox/resources/kzg/g2.point",
-		G2PowerOf2Path:  "../../../inabox/resources/kzg/g2.point.powerOf2",
-		CacheDir:        "../../../inabox/resources/kzg/SRSTables",
+		G1Path:          "../../../../inabox/resources/kzg/g1.point",
+		G2Path:          "../../../../inabox/resources/kzg/g2.point",
+		G2PowerOf2Path:  "../../../../inabox/resources/kzg/g2.point.powerOf2",
+		CacheDir:        "../../../../inabox/resources/kzg/SRSTables",
 		SRSOrder:        3000,
 		SRSNumberToLoad: srsNumberToLoad,
 		NumWorker:       uint64(runtime.GOMAXPROCS(0)),
 		LoadG2Points:    false,
 	}
-
-	testRandom = random.NewTestRandom()
 }
 
 // randomlyModifyBytes picks a random byte from the input array, and increments it
-func randomlyModifyBytes(inputBytes []byte) {
+func randomlyModifyBytes(testRandom *random.TestRandom, inputBytes []byte) {
 	indexToModify := testRandom.Intn(len(inputBytes))
 	inputBytes[indexToModify] = inputBytes[indexToModify] + 1
 }
@@ -85,7 +81,8 @@ func TestComputeAndCompareKzgCommitmentFailure(t *testing.T) {
 	assert.Nil(t, err)
 
 	// randomly modify the bytes, and make sure the commitment verification fails
-	randomlyModifyBytes(gettysburgAddressBytes)
+	testRandom := random.NewTestRandom(t)
+	randomlyModifyBytes(testRandom, gettysburgAddressBytes)
 	err = GenerateAndCompareBlobCommitment(
 		kzgVerifier,
 		commitment,
@@ -110,7 +107,8 @@ func TestGenerateBlobCommitmentEquality(t *testing.T) {
 	assert.Equal(t, commitment1, commitment2)
 
 	// randomly modify a byte
-	randomlyModifyBytes(gettysburgAddressBytes)
+	testRandom := random.NewTestRandom(t)
+	randomlyModifyBytes(testRandom, gettysburgAddressBytes)
 	commitmentA, err := GenerateBlobCommitment(kzgVerifier, gettysburgAddressBytes)
 	assert.NotNil(t, commitmentA)
 	assert.Nil(t, err)
