6059c6a068298d11c41e50f5bcd208d0da44906a
hopeyen <60078528+hopeyen@users.noreply.github.com>
2024-12-20 10:37:04 +0700

    [v2] payments inabox  (#991)

--- disperser/apiserver/disperse_blob_v2.go
+++ disperser/apiserver/disperse_blob_v2.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"math/big"
 	"time"
 
 	"github.com/Layr-Labs/eigenda/api"
 	pb "github.com/Layr-Labs/eigenda/api/grpc/disperser/v2"
+	"github.com/Layr-Labs/eigenda/core"
 	corev2 "github.com/Layr-Labs/eigenda/core/v2"
 	"github.com/Layr-Labs/eigenda/disperser/common"
 	dispv2 "github.com/Layr-Labs/eigenda/disperser/common/v2"
@@ -107,6 +109,19 @@ func (s *DispersalServerV2) validateDispersalRequest(ctx context.Context, req *p
 		return api.NewErrorInvalidArg("blob header must contain commitments")
 	}
 
+	blobHeader, err := corev2.BlobHeaderFromProtobuf(blobHeaderProto)
+	if err != nil {
+		return api.NewErrorInvalidArg(fmt.Sprintf("invalid blob header: %s", err.Error()))
+	}
+
+	if blobHeader.PaymentMetadata == (core.PaymentMetadata{}) {
+		return api.NewErrorInvalidArg("payment metadata is required")
+	}
+
+	if len(blobHeader.PaymentMetadata.AccountID) == 0 || blobHeader.PaymentMetadata.ReservationPeriod == 0 || blobHeader.PaymentMetadata.CumulativePayment == nil {
+		return api.NewErrorInvalidArg("invalid payment metadata")
+	}
+
 	if len(blobHeaderProto.GetQuorumNumbers()) == 0 {
 		return api.NewErrorInvalidArg("blob header must contain at least one quorum number")
 	}
@@ -122,7 +137,7 @@ func (s *DispersalServerV2) validateDispersalRequest(ctx context.Context, req *p
 	}
 
 	// validate every 32 bytes is a valid field element
-	_, err := rs.ToFrArray(data)
+	_, err = rs.ToFrArray(data)
 	if err != nil {
 		s.logger.Error("failed to convert a 32bytes as a field element", "err", err)
 		return api.NewErrorInvalidArg("encountered an error to convert a 32-bytes into a valid field element, please use the correct format where every 32bytes(big-endian) is less than 21888242871839275222246405745257275088548364400416034343698204186575808495617")
@@ -132,38 +147,25 @@ func (s *DispersalServerV2) validateDispersalRequest(ctx context.Context, req *p
 		return api.NewErrorInvalidArg(fmt.Sprintf("invalid blob version %d; valid blob versions are: %v", blobHeaderProto.GetVersion(), onchainState.BlobVersionParameters.Keys()))
 	}
 
-	blobHeader, err := corev2.BlobHeaderFromProtobuf(blobHeaderProto)
-	if err != nil {
-		return api.NewErrorInvalidArg(fmt.Sprintf("invalid blob header: %s", err.Error()))
+	if err = s.authenticator.AuthenticateBlobRequest(blobHeader); err != nil {
+		return api.NewErrorInvalidArg(fmt.Sprintf("authentication failed: %s", err.Error()))
 	}
-	// TODO(ian-shim): enable this check for authentication
-	// if blobHeader.PaymentMetadata == nil {
-	// 	return api.NewErrorInvalidArg("payment metadata is required")
-	// }
-	// if err = s.authenticator.AuthenticateBlobRequest(blobHeader); err != nil {
-	// 	return api.NewErrorInvalidArg(fmt.Sprintf("authentication failed: %s", err.Error()))
-	// }
-
-	// TODO(ian-shim): enable this check when we have payment metadata + authentication in disperser client
-	// if len(blobHeader.PaymentMetadata.AccountID) == 0 || (blobHeader.PaymentMetadata.ReservationPeriod == 0 && blobHeader.PaymentMetadata.CumulativePayment == nil) {
-	// 	return api.NewErrorInvalidArg("invalid payment metadata")
-	// }
 
 	// handle payments and check rate limits
-	// reservationPeriod := blobHeaderProto.GetPaymentHeader().GetReservationPeriod()
-	// cumulativePayment := new(big.Int).SetBytes(blobHeaderProto.GetPaymentHeader().GetCumulativePayment())
-	// accountID := blobHeaderProto.GetPaymentHeader().GetAccountId()
+	reservationPeriod := blobHeaderProto.GetPaymentHeader().GetReservationPeriod()
+	cumulativePayment := new(big.Int).SetBytes(blobHeaderProto.GetPaymentHeader().GetCumulativePayment())
+	accountID := blobHeaderProto.GetPaymentHeader().GetAccountId()
 
-	// paymentHeader := core.PaymentMetadata{
-	// 	AccountID:         accountID,
-	// 	ReservationPeriod:          reservationPeriod,
-	// 	CumulativePayment: cumulativePayment,
-	// }
+	paymentHeader := core.PaymentMetadata{
+		AccountID:         accountID,
+		ReservationPeriod: reservationPeriod,
+		CumulativePayment: cumulativePayment,
+	}
 
-	// err := s.meterer.MeterRequest(ctx, paymentHeader, blobLength, blobHeader.QuorumNumbers)
-	// if err != nil {
-	// 	return api.NewErrorResourceExhausted(err.Error())
-	// }
+	err = s.meterer.MeterRequest(ctx, paymentHeader, blobLength, blobHeader.QuorumNumbers)
+	if err != nil {
+		return api.NewErrorResourceExhausted(err.Error())
+	}
 
 	commitments, err := s.prover.GetCommitmentsForPaddedLength(data)
 	if err != nil {
