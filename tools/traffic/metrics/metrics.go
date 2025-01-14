2c7a414e61d36296799b18dc16ac63644e5bab1e
Daniel Mancia <21249320+dmanc@users.noreply.github.com>
2025-01-14 10:32:20 -0800

    Traffic generator V2 (#1055)

--- tools/traffic/metrics/metrics.go
+++ tools/traffic/metrics/metrics.go
@@ -1,19 +1,23 @@
 package metrics
 
 import (
+	"context"
 	"fmt"
+	"net/http"
+	"time"
+
 	"github.com/Layr-Labs/eigensdk-go/logging"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"net/http"
-	"strings"
 )
 
 // Metrics allows the creation of metrics for the traffic generator.
 type Metrics interface {
 	// Start starts the metrics server.
-	Start()
+	Start() error
+	// Shutdown shuts down the metrics server.
+	Shutdown() error
 	// NewLatencyMetric creates a new LatencyMetric instance. Useful for reporting the latency of an operation.
 	NewLatencyMetric(description string) LatencyMetric
 	// NewCountMetric creates a new CountMetric instance. Useful for tracking the count of a type of event.
@@ -33,55 +37,73 @@ type metrics struct {
 	httpPort string
 	logger   logging.Logger
 
-	metricsBlacklist      []string
-	metricsFuzzyBlacklist []string
+	shutdown func() error
 }
 
 // NewMetrics creates a new Metrics instance.
 func NewMetrics(
 	httpPort string,
 	logger logging.Logger,
-	metricsBlacklist []string,
-	metricsFuzzyBlacklist []string) Metrics {
+) Metrics {
 
 	namespace := "eigenda_generator"
 	reg := prometheus.NewRegistry()
 	reg.MustRegister(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}))
 	reg.MustRegister(collectors.NewGoCollector())
 
-	if metricsBlacklist == nil {
-		metricsBlacklist = []string{}
-	}
-	if metricsFuzzyBlacklist == nil {
-		metricsFuzzyBlacklist = []string{}
-	}
-
 	metrics := &metrics{
-		count:                 buildCounterCollector(namespace, reg),
-		latency:               buildLatencyCollector(namespace, reg),
-		gauge:                 buildGaugeCollector(namespace, reg),
-		registry:              reg,
-		httpPort:              httpPort,
-		logger:                logger.With("component", "GeneratorMetrics"),
-		metricsBlacklist:      metricsBlacklist,
-		metricsFuzzyBlacklist: metricsFuzzyBlacklist,
+		count:    buildCounterCollector(namespace, reg),
+		latency:  buildLatencyCollector(namespace, reg),
+		gauge:    buildGaugeCollector(namespace, reg),
+		registry: reg,
+		httpPort: httpPort,
+		logger:   logger.With("component", "GeneratorMetrics"),
 	}
 	return metrics
 }
 
-// Start starts the metrics server.
-func (metrics *metrics) Start() {
-	metrics.logger.Info("Starting metrics server at ", "port", metrics.httpPort)
+func (metrics *metrics) Start() error {
+	metrics.logger.Info("Starting metrics server", "port", metrics.httpPort)
 	addr := fmt.Sprintf(":%s", metrics.httpPort)
+	// Create mux and add /metrics handler
+	mux := http.NewServeMux()
+	mux.Handle("/metrics", promhttp.HandlerFor(
+		metrics.registry,
+		promhttp.HandlerOpts{},
+	))
+
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: mux,
+	}
+
 	go func() {
-		mux := http.NewServeMux()
-		mux.Handle("/metrics", promhttp.HandlerFor(
-			metrics.registry,
-			promhttp.HandlerOpts{},
-		))
-		err := http.ListenAndServe(addr, mux)
-		panic(fmt.Sprintf("Prometheus server failed: %s", err))
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			metrics.logger.Error("Prometheus server failed", "err", err)
+		}
 	}()
+
+	// Store shutdown function
+	metrics.shutdown = func() error {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		metrics.logger.Info("Shutting down metrics server")
+		if err := srv.Shutdown(ctx); err != nil {
+			metrics.logger.Error("Metrics server shutdown failed", "err", err)
+			return err
+		}
+		return nil
+	}
+
+	return nil
+}
+
+func (metrics *metrics) Shutdown() error {
+	if metrics.shutdown != nil {
+		return metrics.shutdown()
+	}
+	return nil
 }
 
 // NewLatencyMetric creates a new LatencyMetric instance.
@@ -89,7 +111,6 @@ func (metrics *metrics) NewLatencyMetric(description string) LatencyMetric {
 	return &latencyMetric{
 		metrics:     metrics,
 		description: description,
-		disabled:    metrics.isBlacklisted(description),
 	}
 }
 
@@ -98,7 +119,6 @@ func (metrics *metrics) NewCountMetric(description string) CountMetric {
 	return &countMetric{
 		metrics:     metrics,
 		description: description,
-		disabled:    metrics.isBlacklisted(description),
 	}
 }
 
@@ -107,21 +127,5 @@ func (metrics *metrics) NewGaugeMetric(description string) GaugeMetric {
 	return &gaugeMetric{
 		metrics:     metrics,
 		description: description,
-		disabled:    metrics.isBlacklisted(description),
 	}
-}
-
-// isBlacklisted returns true if the metric name is blacklisted.
-func (metrics *metrics) isBlacklisted(metricName string) bool {
-	for _, blacklisted := range metrics.metricsBlacklist {
-		if metricName == blacklisted {
-			return true
-		}
-	}
-	for _, blacklisted := range metrics.metricsFuzzyBlacklist {
-		if strings.Contains(metricName, blacklisted) {
-			return true
-		}
-	}
-	return false
 }
