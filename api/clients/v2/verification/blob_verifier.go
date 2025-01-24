180bc960910b7b2b445bb4cacd580d050c1d1e57
litt <102969658+litt3@users.noreply.github.com>
2025-01-24 10:54:22 -0500

    Implement v2 client GET functionality (#972)
    Signed-off-by: litt3 <102969658+litt3@users.noreply.github.com>

--- api/clients/v2/verification/blob_verifier.go
+++ api/clients/v2/verification/blob_verifier.go
@@ -4,14 +4,23 @@ import (
 	"context"
 	"fmt"
 
-	commonv2 "github.com/Layr-Labs/eigenda/api/grpc/common/v2"
-	"github.com/Layr-Labs/eigenda/common"
+	"github.com/Layr-Labs/eigenda/common/geth"
 
 	disperser "github.com/Layr-Labs/eigenda/api/grpc/disperser/v2"
 	verifierBindings "github.com/Layr-Labs/eigenda/contracts/bindings/EigenDABlobVerifier"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	gethcommon "github.com/ethereum/go-ethereum/common"
 )
+
+// IBlobVerifier is the interface representing a BlobVerifier
+//
+// This interface exists in order to allow verification mocking in unit tests.
+type IBlobVerifier interface {
+	VerifyBlobV2(
+		ctx context.Context,
+		eigenDACert *EigenDACert,
+	) error
+}
 
 // BlobVerifier is responsible for making eth calls against the BlobVerifier contract to ensure cryptographic and
 // structural integrity of V2 certificates
@@ -24,7 +33,7 @@ type BlobVerifier struct {
 
 // NewBlobVerifier constructs a BlobVerifier
 func NewBlobVerifier(
-	ethClient common.EthClient, // the eth client, which should already be set up
+	ethClient geth.EthClient, // the eth client, which should already be set up
 	blobVerifierAddress string, // the hex address of the EigenDABlobVerifier contract
 ) (*BlobVerifier, error) {
 
@@ -52,12 +61,12 @@ func (v *BlobVerifier) VerifyBlobV2FromSignedBatch(
 	// Contains all necessary information about the blob, so that it can be verified.
 	blobVerificationProof *disperser.BlobInclusionInfo,
 ) error {
-	convertedSignedBatch, err := verifierBindings.ConvertSignedBatch(signedBatch)
+	convertedSignedBatch, err := SignedBatchProtoToBinding(signedBatch)
 	if err != nil {
 		return fmt.Errorf("convert signed batch: %s", err)
 	}
 
-	convertedBlobVerificationProof, err := verifierBindings.ConvertVerificationProof(blobVerificationProof)
+	convertedBlobVerificationProof, err := VerificationProofProtoToBinding(blobVerificationProof)
 	if err != nil {
 		return fmt.Errorf("convert blob verification proof: %s", err)
 	}
@@ -79,28 +88,13 @@ func (v *BlobVerifier) VerifyBlobV2FromSignedBatch(
 // This method returns nil if the blob is successfully verified. Otherwise, it returns an error.
 func (v *BlobVerifier) VerifyBlobV2(
 	ctx context.Context,
-	// The header of the batch that the blob is contained in
-	batchHeader *commonv2.BatchHeader,
-	// Contains data pertaining to the blob's inclusion in the batch
-	blobVerificationProof *disperser.BlobInclusionInfo,
-	// Contains data that can be used to verify that the blob actually exists in the claimed batch
-	nonSignerStakesAndSignature verifierBindings.NonSignerStakesAndSignature,
+	eigenDACert *EigenDACert,
 ) error {
-	convertedBatchHeader, err := verifierBindings.ConvertBatchHeader(batchHeader)
-	if err != nil {
-		return fmt.Errorf("convert batch header: %s", err)
-	}
-
-	convertedBlobVerificationProof, err := verifierBindings.ConvertVerificationProof(blobVerificationProof)
-	if err != nil {
-		return fmt.Errorf("convert blob verification proof: %s", err)
-	}
-
-	err = v.blobVerifierCaller.VerifyBlobV2(
+	err := v.blobVerifierCaller.VerifyBlobV2(
 		&bind.CallOpts{Context: ctx},
-		*convertedBatchHeader,
-		*convertedBlobVerificationProof,
-		nonSignerStakesAndSignature)
+		eigenDACert.BatchHeader,
+		eigenDACert.BlobVerificationProof,
+		eigenDACert.NonSignerStakesAndSignature)
 
 	if err != nil {
 		return fmt.Errorf("verify blob v2: %s", err)
