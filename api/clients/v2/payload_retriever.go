95b2a3ee7c432b4de22eb618aa54a0459ed2ae3a
litt3 <102969658+litt3@users.noreply.github.com>
2025-01-30 09:55:20 -0500

    Expand PayloadRetriever doc
    Signed-off-by: litt3 <102969658+litt3@users.noreply.github.com>

--- api/clients/v2/payload_retriever.go
+++ api/clients/v2/payload_retriever.go
@@ -7,6 +7,9 @@ import (
 )
 
 // PayloadRetriever represents something that knows how to retrieve a payload from some backend using a verification.EigenDACert
+//
+// This interface may be implemented to provide alternate retrieval methods, for example payload retrieval from an S3
+// bucket instead of from EigenDA relays or nodes.
 type PayloadRetriever interface {
 	// GetPayload retrieves a payload from some backend, using the provided certificate
 	GetPayload(ctx context.Context, eigenDACert *verification.EigenDACert) ([]byte, error)
