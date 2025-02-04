7af4107239c8133c5db65f842d30cc4d38a2c26c
pschork <354473+pschork@users.noreply.github.com>
2025-02-03 22:37:52 -0800

    Adds isolated v2 reachability check to operator node (#1152)

--- node/grpc/run.go
+++ node/grpc/run.go
@@ -17,15 +17,22 @@ import (
 const localhost = "0.0.0.0"
 
 func RunServers(serverV1 *Server, serverV2 *ServerV2, config *node.Config, logger logging.Logger) error {
-	if serverV1 == nil {
-		return errors.New("node V1 server is not configured")
+	if config.EnableV1 && serverV1 == nil {
+		return errors.New("node v1 server is not configured")
 	}
 	if config.EnableV2 && serverV2 == nil {
-		return errors.New("node V2 server is not configured")
+		return errors.New("node v2 server is not configured")
+	}
+	if !config.EnableV1 && !config.EnableV2 {
+		return errors.New("node is not configured to run any servers")
 	}
 
+	// V1 dispersal service
 	go func() {
-		// V1 dispersal service
+		if !config.EnableV1 {
+			logger.Warn("v1 is not enabled, skipping v1 dispersal server startup")
+			return
+		}
 		for {
 			addr := fmt.Sprintf("%s:%s", localhost, config.InternalDispersalPort)
 			listener, err := net.Listen("tcp", addr)
@@ -44,7 +51,7 @@ func RunServers(serverV1 *Server, serverV2 *ServerV2, config *node.Config, logge
 
 			healthcheck.RegisterHealthServer("node.Dispersal", gs)
 
-			logger.Info("port", config.InternalDispersalPort, "address", listener.Addr().String(), "GRPC Listening")
+			logger.Info("v1 dispersal enabled on port", config.InternalDispersalPort, "address", listener.Addr().String(), "GRPC Listening")
 			if err := gs.Serve(listener); err != nil {
 				logger.Error("dispersal server failed; restarting.", "err", err)
 			}
@@ -54,7 +61,7 @@ func RunServers(serverV1 *Server, serverV2 *ServerV2, config *node.Config, logge
 	// V2 dispersal service
 	go func() {
 		if !config.EnableV2 {
-			logger.Warn("V2 is not enabled, skipping V2 dispersal server startup")
+			logger.Warn("v2 is not enabled, skipping v2 dispersal server startup")
 			return
 		}
 		for {
@@ -75,15 +82,19 @@ func RunServers(serverV1 *Server, serverV2 *ServerV2, config *node.Config, logge
 
 			healthcheck.RegisterHealthServer("node.v2.Dispersal", gs)
 
-			logger.Info("port", config.V2DispersalPort, "address", listener.Addr().String(), "GRPC Listening")
+			logger.Info("v2 dispersal enabled on port", config.V2DispersalPort, "address", listener.Addr().String(), "GRPC Listening")
 			if err := gs.Serve(listener); err != nil {
 				logger.Error("dispersal v2 server failed; restarting.", "err", err)
 			}
 		}
 	}()
 
-	// Retrieval service
+	// v1 Retrieval service
 	go func() {
+		if !config.EnableV1 {
+			logger.Warn("v1 is not enabled, skipping v1 retrieval server startup")
+			return
+		}
 		for {
 			addr := fmt.Sprintf("%s:%s", localhost, config.InternalRetrievalPort)
 			listener, err := net.Listen("tcp", addr)
@@ -101,16 +112,17 @@ func RunServers(serverV1 *Server, serverV2 *ServerV2, config *node.Config, logge
 			pb.RegisterRetrievalServer(gs, serverV1)
 			healthcheck.RegisterHealthServer("node.Retrieval", gs)
 
-			logger.Info("port", config.InternalRetrievalPort, "address", listener.Addr().String(), "GRPC Listening")
+			logger.Info("v1 retrieval enabled on port", config.InternalRetrievalPort, "address", listener.Addr().String(), "GRPC Listening")
 			if err := gs.Serve(listener); err != nil {
 				logger.Error("retrieval server failed; restarting.", "err", err)
 			}
 		}
 	}()
 
+	// v2 Retrieval service
 	go func() {
 		if !config.EnableV2 {
-			logger.Warn("V2 is not enabled, skipping V2 retrieval server startup")
+			logger.Warn("v2 is not enabled, skipping v2 retrieval server startup")
 			return
 		}
 		for {
@@ -130,7 +142,7 @@ func RunServers(serverV1 *Server, serverV2 *ServerV2, config *node.Config, logge
 
 			healthcheck.RegisterHealthServer("node.v2.Retrieval", gs)
 
-			logger.Info("port", config.V2RetrievalPort, "address", listener.Addr().String(), "GRPC Listening")
+			logger.Info("v2 retrieval enabled on port", config.V2RetrievalPort, "address", listener.Addr().String(), "GRPC Listening")
 			if err := gs.Serve(listener); err != nil {
 				logger.Error("retrieval v2 server failed; restarting.", "err", err)
 			}
