a66068efa5e1eb8e9899e56759b383790ef84253
litt <102969658+litt3@users.noreply.github.com>
2025-02-04 16:10:32 -0500

    Wait for internal eth client to reach block num (#1204)
    Signed-off-by: litt3 <102969658+litt3@users.noreply.github.com>

--- api/clients/v2/verification/cert_verifier.go
+++ api/clients/v2/verification/cert_verifier.go
@@ -3,9 +3,13 @@ package verification
 import (
 	"context"
 	"fmt"
+	"sync/atomic"
+	"time"
+
 	disperser "github.com/Layr-Labs/eigenda/api/grpc/disperser/v2"
-	"github.com/Layr-Labs/eigenda/common/geth"
+	"github.com/Layr-Labs/eigenda/common"
 	verifierBindings "github.com/Layr-Labs/eigenda/contracts/bindings/EigenDACertVerifier"
+	"github.com/Layr-Labs/eigensdk-go/logging"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	gethcommon "github.com/ethereum/go-ethereum/common"
 )
@@ -30,18 +34,35 @@ type ICertVerifier interface {
 //
 // The cert verifier contract is located at https://github.com/Layr-Labs/eigenda/blob/master/contracts/src/core/EigenDACertVerifier.sol
 type CertVerifier struct {
+	logger logging.Logger
 	// go binding around the EigenDACertVerifier ethereum contract
 	certVerifierCaller *verifierBindings.ContractEigenDACertVerifierCaller
+	ethClient          common.EthClient
+	pollInterval       time.Duration
+	// storage shared between goroutines, containing the most recent block number observed by calling ethClient.BlockNumber()
+	latestBlockNumber atomic.Uint64
+	// atomic bool, so that only a single goroutine is polling the internal client with BlockNumber() calls at any given time
+	pollingActive atomic.Bool
 }
 
 var _ ICertVerifier = &CertVerifier{}
 
 // NewCertVerifier constructs a CertVerifier
 func NewCertVerifier(
-	ethClient geth.EthClient, // the eth client, which should already be set up
-	certVerifierAddress string, // the hex address of the EigenDACertVerifier contract
+	logger logging.Logger,
+	// the eth client, which should already be set up
+	ethClient common.EthClient,
+	// the hex address of the EigenDACertVerifier contract
+	certVerifierAddress string,
+	// pollInterval is how frequently to check latest block number when waiting for the internal eth client to advance
+	// to a certain block. This is needed because the RBN in a cert might be further in the future than the internal
+	// eth client. In such a case, we must wait for the internal client to catch up to the block number
+	// contained in the cert: otherwise, calls will fail.
+	//
+	// If the configured pollInterval duration is <= 0, then the block number check will be skipped, and calls that
+	// rely on the client having reached a certain block number will fail if the internal client is behind.
+	pollInterval time.Duration,
 ) (*CertVerifier, error) {
-
 	verifierCaller, err := verifierBindings.NewContractEigenDACertVerifierCaller(
 		gethcommon.HexToAddress(certVerifierAddress),
 		ethClient)
@@ -50,12 +71,28 @@ func NewCertVerifier(
 		return nil, fmt.Errorf("bind to verifier contract at %s: %w", certVerifierAddress, err)
 	}
 
+	if pollInterval <= time.Duration(0) {
+		logger.Warn(
+			`CertVerifier poll interval is <= 0. Therefore, any method calls made with this object that 
+					rely on the internal client having reached a certain block number will fail if
+					the internal client is too far behind.`,
+			"pollInterval", pollInterval)
+	}
+
 	return &CertVerifier{
+		logger:             logger,
 		certVerifierCaller: verifierCaller,
+		ethClient:          ethClient,
+		pollInterval:       pollInterval,
 	}, nil
 }
 
-// VerifyCertV2FromSignedBatch calls the verifyDACertV2FromSignedBatch view function on the EigenDACertVerifier contract
+// VerifyCertV2FromSignedBatch calls the verifyDACertV2FromSignedBatch view function on the EigenDACertVerifier contract.
+//
+// Before verifying the cert, this method will wait for the internal client to advance to a sufficient block height.
+// This wait will time out if the duration exceeds the timeout configured for the input ctx parameter. If
+// CertVerifier.pollInterval is configured to be <= 0, then this method will *not* wait for the internal client to
+// advance, and will instead simply fail verification if the internal client is behind.
 //
 // This method returns nil if the cert is successfully verified. Otherwise, it returns an error.
 func (cv *CertVerifier) VerifyCertV2FromSignedBatch(
@@ -76,6 +113,11 @@ func (cv *CertVerifier) VerifyCertV2FromSignedBatch(
 		return fmt.Errorf("convert blob inclusion info: %w", err)
 	}
 
+	err = cv.MaybeWaitForBlockNumber(ctx, signedBatch.GetHeader().GetReferenceBlockNumber())
+	if err != nil {
+		return fmt.Errorf("wait for block number: %w", err)
+	}
+
 	err = cv.certVerifierCaller.VerifyDACertV2FromSignedBatch(
 		&bind.CallOpts{Context: ctx},
 		*convertedSignedBatch,
@@ -88,14 +130,24 @@ func (cv *CertVerifier) VerifyCertV2FromSignedBatch(
 	return nil
 }
 
-// VerifyCertV2 calls the VerifyCertV2 view function on the EigenDACertVerifier contract
+// VerifyCertV2 calls the VerifyCertV2 view function on the EigenDACertVerifier contract.
+//
+// Before verifying the cert, this method will wait for the internal client to advance to a sufficient block height.
+// This wait will time out if the duration exceeds the timeout configured for the input ctx parameter. If
+// CertVerifier.pollInterval is configured to be <= 0, then this method will *not* wait for the internal client to
+// advance, and will instead simply fail verification if the internal client is behind.
 //
 // This method returns nil if the cert is successfully verified. Otherwise, it returns an error.
 func (cv *CertVerifier) VerifyCertV2(
 	ctx context.Context,
 	eigenDACert *EigenDACert,
 ) error {
-	err := cv.certVerifierCaller.VerifyDACertV2(
+	err := cv.MaybeWaitForBlockNumber(ctx, uint64(eigenDACert.BatchHeader.ReferenceBlockNumber))
+	if err != nil {
+		return fmt.Errorf("wait for block number: %w", err)
+	}
+
+	err = cv.certVerifierCaller.VerifyDACertV2(
 		&bind.CallOpts{Context: ctx},
 		eigenDACert.BatchHeader,
 		eigenDACert.BlobInclusionInfo,
@@ -110,6 +162,12 @@ func (cv *CertVerifier) VerifyCertV2(
 
 // GetNonSignerStakesAndSignature calls the getNonSignerStakesAndSignature view function on the EigenDACertVerifier
 // contract, and returns the resulting NonSignerStakesAndSignature object.
+//
+// Before getting the NonSignerStakesAndSignature, this method will wait for the internal client to advance to a
+// sufficient block height. This wait will time out if the duration exceeds the timeout configured for the input ctx
+// parameter. If CertVerifier.pollInterval is configured to be <= 0, then this method will *not* wait for the internal
+// client to advance, and will instead simply fail to get the NonSignerStakesAndSignature if the internal client is
+// behind.
 func (cv *CertVerifier) GetNonSignerStakesAndSignature(
 	ctx context.Context,
 	signedBatch *disperser.SignedBatch,
@@ -118,6 +176,11 @@ func (cv *CertVerifier) GetNonSignerStakesAndSignature(
 	signedBatchBinding, err := SignedBatchProtoToBinding(signedBatch)
 	if err != nil {
 		return nil, fmt.Errorf("convert signed batch: %w", err)
+	}
+
+	err = cv.MaybeWaitForBlockNumber(ctx, signedBatch.GetHeader().GetReferenceBlockNumber())
+	if err != nil {
+		return nil, fmt.Errorf("wait for block number: %w", err)
 	}
 
 	nonSignerStakesAndSignature, err := cv.certVerifierCaller.GetNonSignerStakesAndSignature(
@@ -129,4 +192,79 @@ func (cv *CertVerifier) GetNonSignerStakesAndSignature(
 	}
 
 	return &nonSignerStakesAndSignature, nil
+}
+
+// MaybeWaitForBlockNumber waits until the internal eth client has advanced to a certain targetBlockNumber, unless
+// configured pollInterval is <= 0, in which case this method will NOT wait for the internal client to advance.
+//
+// This method will check the current block number of the internal client every CertVerifier.pollInterval duration.
+// It will return nil if the internal client advances to (or past) the targetBlockNumber. It will return an error
+// if the input context times out, or if any error occurs when checking the block number of the internal client.
+//
+// This method is synchronized in a way that, if called by multiple goroutines, only a single goroutine will actually
+// poll the internal eth client for most recent block number. The goroutine responsible for polling at a given time
+// updates an atomic integer, so that all goroutines may check the most recent block without duplicating work.
+func (cv *CertVerifier) MaybeWaitForBlockNumber(ctx context.Context, targetBlockNumber uint64) error {
+	if cv.pollInterval <= 0 {
+		// don't wait for the internal client to advance
+		return nil
+	}
+
+	if cv.latestBlockNumber.Load() >= targetBlockNumber {
+		// immediately return if the local client isn't behind the target block number
+		return nil
+	}
+
+	ticker := time.NewTicker(cv.pollInterval)
+	defer ticker.Stop()
+
+	polling := false
+	if cv.pollingActive.CompareAndSwap(false, true) {
+		// no other goroutine is currently polling, so assume responsibility
+		polling = true
+		defer cv.pollingActive.Store(false)
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			return fmt.Errorf(
+				"timed out waiting for block number %d (latest block number observed was %d): %w",
+				targetBlockNumber, cv.latestBlockNumber.Load(), ctx.Err())
+		case <-ticker.C:
+			if cv.latestBlockNumber.Load() >= targetBlockNumber {
+				return nil
+			}
+
+			if cv.pollingActive.CompareAndSwap(false, true) {
+				// no other goroutine is currently polling, so assume responsibility
+				polling = true
+				defer cv.pollingActive.Store(false)
+			}
+
+			if polling {
+				actualBlockNumber, err := cv.ethClient.BlockNumber(ctx)
+				if err != nil {
+					cv.logger.Debug(
+						"ethClient.BlockNumber returned an error",
+						"targetBlockNumber", targetBlockNumber,
+						"latestBlockNumber", cv.latestBlockNumber.Load(),
+						"error", err)
+
+					// tolerate some failures here. if failure continues for too long, it will be caught by the timeout
+					continue
+				}
+
+				cv.latestBlockNumber.Store(actualBlockNumber)
+				if actualBlockNumber >= targetBlockNumber {
+					return nil
+				}
+			}
+
+			cv.logger.Debug(
+				"local client is behind the reference block number",
+				"targetBlockNumber", targetBlockNumber,
+				"actualBlockNumber", cv.latestBlockNumber.Load())
+		}
+	}
 }
