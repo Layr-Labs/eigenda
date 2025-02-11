4b108859aafd4d9bea47adb90895ed3fe6ebb0ac
Cody Littley <cody@eigenlabs.org>
2025-02-11 16:36:27 -0600

    cleanup
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

--- litt/disk/address_map.go
+++ litt/disk/address_map.go
@@ -2,24 +2,23 @@ package disk
 
 import (
 	"fmt"
-	"github.com/Layr-Labs/eigenda/litt"
 )
 
 // addressMap manages a mapping between keys and the location of their data on disk (i.e. "addresses").
 type addressMap struct {
 	// addresses maps keys to their locations on disk.
-	addresses map[string]litt.address
+	addresses map[string]address
 }
 
 // TODO thread safety
 
 // setAddress sets the address of a key in a table.
-func (k *addressMap) setAddress(key []byte, addr litt.address) {
+func (k *addressMap) setAddress(key []byte, addr address) {
 	k.addresses[string(key)] = addr
 }
 
 // getAddress gets the address of a key in a table.
-func (k *addressMap) getAddress(key []byte) (litt.address, error) {
+func (k *addressMap) getAddress(key []byte) (address, error) {
 	a, ok := k.addresses[string(key)]
 	if !ok {
 		return 0, fmt.Errorf("key not found in address map: %s", key)
