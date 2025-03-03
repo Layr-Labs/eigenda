957f89633b2678fdaa1c867a17de820701f5cf45
Cody Littley <cody@eigenlabs.org>
2025-03-03 13:30:22 -0600

    fix docs
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

--- api/grpc/relay/relay.pb.go
+++ api/grpc/relay/relay.pb.go
@@ -146,14 +146,14 @@ type GetChunksRequest struct {
 	//     a. if the chunk request is a request by index:
 	//     i.   a one byte ASCII representation of the character "i" (aka Ox69)
 	//     ii.  the length blob key in bytes
-	//     ii.  the blob key
-	//     iii. the start index
-	//     iv.  the end index
+	//     iii. the blob key
+	//     iv.  the start index
+	//     v.   the end index
 	//     b. if the chunk request is a request by range:
 	//     i.   a one byte ASCII representation of the character "r" (aka Ox72)
 	//     ii.  the length of the blob key in bytes
-	//     ii.  the blob key
-	//     iii. each requested chunk index, in order
+	//     iii. the blob key
+	//     iv.  each requested chunk index, in order
 	OperatorSignature []byte `protobuf:"bytes,3,opt,name=operator_signature,json=operatorSignature,proto3" json:"operator_signature,omitempty"`
 }
 
