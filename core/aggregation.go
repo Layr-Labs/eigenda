4adc1113810217fab447da3167bf9692a68a2f57
Ian Shim <100327837+ian-shim@users.noreply.github.com>
2025-03-04 10:36:21 -0800

    Make sure no operator can reply to same batch multiple times (#1356)

--- core/aggregation.go
+++ core/aggregation.go
@@ -133,6 +133,11 @@ func (a *StdSignatureAggregator) ReceiveSignatures(ctx context.Context, state *I
 	for numReply := 0; numReply < numOperators; numReply++ {
 		var err error
 		r := <-messageChan
+		if seen := signerMap[r.Operator]; seen {
+			a.Logger.Warn("duplicate signature received", "operatorID", r.Operator.Hex())
+			continue
+		}
+
 		operatorIDHex := r.Operator.Hex()
 		operatorAddr, ok := a.OperatorAddresses.Get(r.Operator)
 		if !ok && a.Transactor != nil {
