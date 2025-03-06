fb5e9ace222f8af6555e2b5595760aeee9f282e2
Cody Littley <cody@eigenlabs.org>
2025-03-06 15:22:40 -0600

    add keymap migration test
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

---
+++ litt/test/keymap_migration_test.go
@@ -0,0 +1,170 @@
+package test
+
+import (
+	"context"
+	"os"
+	"path"
+	"testing"
+	"time"
+
+	"github.com/Layr-Labs/eigenda/common/testutils/random"
+	"github.com/Layr-Labs/eigenda/litt/disktable/keymap"
+	"github.com/Layr-Labs/eigenda/litt/littbuilder"
+	"github.com/Layr-Labs/eigenda/litt/types"
+	"github.com/stretchr/testify/require"
+)
+
+// Tests migration from one type of KeyMap to another. This is not defined in the disktable package because this
+// migration requires a littbuilder.LittDBConfig, which is not available in the disktable package.
+func TestKeyMapMigration(t *testing.T) {
+	rand := random.NewTestRandom()
+	directory := t.TempDir()
+
+	directoryCount := 8
+	shardDirectories := make([]string, 0, directoryCount)
+	for i := 0; i < directoryCount; i++ {
+		shardDirectories = append(shardDirectories, path.Join(directory, rand.String(32)))
+	}
+
+	// Build the table using LevelDBKeyMap.
+	config, err := littbuilder.DefaultConfig(shardDirectories...)
+	require.NoError(t, err)
+	config.ShardingFactor = uint32(directoryCount)
+	config.KeyMapType = keymap.LevelDBKeyMapType
+
+	db, err := config.Build(context.Background())
+	require.NoError(t, err)
+	err = db.Start()
+	require.NoError(t, err)
+	table, err := db.GetTable("test")
+	require.NoError(t, err)
+
+	// Fill the table with some data.
+	expectedValues := make(map[string][]byte)
+
+	iterations := 1000
+	for i := 0; i < iterations; i++ {
+
+		// Write some data.
+		batchSize := rand.Int32Range(1, 10)
+
+		if batchSize == 1 {
+			key := rand.PrintableVariableBytes(32, 64)
+			value := rand.PrintableVariableBytes(1, 128)
+			err = table.Put(key, value)
+			require.NoError(t, err)
+			expectedValues[string(key)] = value
+		} else {
+			batch := make([]*types.KVPair, 0, batchSize)
+			for j := int32(0); j < batchSize; j++ {
+				key := rand.PrintableVariableBytes(32, 64)
+				value := rand.PrintableVariableBytes(1, 128)
+				batch = append(batch, &types.KVPair{Key: key, Value: value})
+				expectedValues[string(key)] = value
+			}
+			err = table.PutBatch(batch)
+			require.NoError(t, err)
+		}
+
+		// Once in a while, flush the table.
+		if rand.BoolWithProbability(0.1) {
+			err = table.Flush()
+			require.NoError(t, err)
+		}
+
+		// Once in a while, sleep for a short time. For tables that do garbage collection, the garbage
+		// collection interval has been configured to be 1ms. Sleeping 5ms should be enough to give
+		// the garbage collector a chance to run.
+		if rand.BoolWithProbability(0.01) {
+			time.Sleep(5 * time.Millisecond)
+		}
+
+		// Once in a while, scan the table and verify that all expected values are present.
+		// Don't do this every time for the sake of test runtime.
+		if rand.BoolWithProbability(0.01) || i == iterations-1 /* always check on the last iteration */ {
+			for expectedKey, expectedValue := range expectedValues {
+				value, ok, err := table.Get([]byte(expectedKey))
+				require.NoError(t, err)
+				require.True(t, ok)
+				require.Equal(t, expectedValue, value)
+			}
+
+			// Try fetching a value that isn't in the table.
+			_, ok, err := table.Get(rand.PrintableVariableBytes(32, 64))
+			require.NoError(t, err)
+			require.False(t, ok)
+		}
+	}
+
+	// Shut down the table and move the keymap directory. There shouldn't be any problems caused by this.
+	err = db.Stop()
+	require.NoError(t, err)
+
+	// By default, the keymap will store its data inside directory 0
+	keymapPath := path.Join(shardDirectories[0], "test", "ldb-keymap")
+	newKeymapPath := path.Join(shardDirectories[int(rand.Int64Range(1, int64(directoryCount)))],
+		"test", "ldb-keymap")
+
+	err = os.Rename(keymapPath, newKeymapPath)
+	require.NoError(t, err)
+
+	// Reload the table and check the data
+	db, err = config.Build(context.Background())
+	require.NoError(t, err)
+	err = db.Start()
+	require.NoError(t, err)
+	table, err = db.GetTable("test")
+	require.NoError(t, err)
+
+	for expectedKey, expectedValue := range expectedValues {
+		value, ok, err := table.Get([]byte(expectedKey))
+		require.NoError(t, err)
+		require.True(t, ok)
+		require.Equal(t, expectedValue, value)
+	}
+
+	// Close the table and reopen it using a MemKeyMap
+	err = db.Stop()
+	require.NoError(t, err)
+	config.KeyMapType = keymap.MemKeyMapType
+
+	db, err = config.Build(context.Background())
+	require.NoError(t, err)
+	err = db.Start()
+	require.NoError(t, err)
+	table, err = db.GetTable("test")
+	require.NoError(t, err)
+
+	for expectedKey, expectedValue := range expectedValues {
+		value, ok, err := table.Get([]byte(expectedKey))
+		require.NoError(t, err)
+		require.True(t, ok)
+		require.Equal(t, expectedValue, value)
+	}
+
+	// We shouldn't see the keymap directory anymore
+	_, err = os.Stat(newKeymapPath)
+	require.True(t, os.IsNotExist(err))
+
+	// Close the table and reopen it using a LevelDBKeyMap
+	err = db.Stop()
+	require.NoError(t, err)
+	config.KeyMapType = keymap.LevelDBKeyMapType
+
+	db, err = config.Build(context.Background())
+	require.NoError(t, err)
+	err = db.Start()
+	require.NoError(t, err)
+	table, err = db.GetTable("test")
+	require.NoError(t, err)
+
+	for expectedKey, expectedValue := range expectedValues {
+		value, ok, err := table.Get([]byte(expectedKey))
+		require.NoError(t, err)
+		require.True(t, ok)
+		require.Equal(t, expectedValue, value)
+	}
+
+	err = db.Destroy()
+	require.NoError(t, err)
+}
