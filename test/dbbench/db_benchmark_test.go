566b4d2887575a60d1d41fdcd496c87ab3411c29
Cody Littley <cody@eigenlabs.org>
2025-03-13 12:56:35 -0500

    capture keys with secondary flush pathwayi
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

--- test/dbbench/db_benchmark_test.go
+++ test/dbbench/db_benchmark_test.go
@@ -557,7 +557,6 @@ func TestBadgerDB(t *testing.T) {
 
 	unflushedKeys := make([][]byte, 0, 100)
 	writeFunction := func(key []byte, value []byte) error {
-		fmt.Printf("objects in batch: %d\n", objectsInBatch) // TODO
 		if captureKeys {
 			unflushedKeys = append(unflushedKeys, key)
 		}
@@ -571,6 +570,24 @@ func TestBadgerDB(t *testing.T) {
 				if err != nil {
 					return err
 				}
+
+				if captureKeys {
+					// If the DB is ACID compliant, all of these keys should now be durable
+
+					for _, unflushedKey := range unflushedKeys {
+						_, err = keyCaptureWriter.WriteString(fmt.Sprintf("%s", unflushedKey))
+						if err != nil {
+							panic(err)
+						}
+					}
+					err = keyCaptureWriter.Flush()
+					fmt.Printf("\nwrote %d keys\n", len(unflushedKeys))
+					if err != nil {
+						panic(err)
+					}
+					unflushedKeys = make([][]byte, 0, 100)
+				}
+
 				transaction = db.NewTransaction(true)
 				objectsInBatch = 0
 				err = transaction.SetEntry(entry)
@@ -582,7 +599,6 @@ func TestBadgerDB(t *testing.T) {
 		objectsInBatch++
 
 		if objectsInBatch >= batchSize {
-			fmt.Printf("\nflushing batch\n")
 
 			//writeLimiter <- struct{}{}
 			transactionToCommit := transaction
@@ -594,7 +610,6 @@ func TestBadgerDB(t *testing.T) {
 			}
 
 			if captureKeys {
-
 				// If the DB is ACID compliant, all of these keys should now be durable
 
 				for _, unflushedKey := range unflushedKeys {
@@ -609,7 +624,6 @@ func TestBadgerDB(t *testing.T) {
 					panic(err)
 				}
 				unflushedKeys = make([][]byte, 0, 100)
-
 			}
 
 			//<-writeLimiter
