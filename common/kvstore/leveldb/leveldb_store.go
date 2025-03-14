1ecbc78a10f481b0424d732effb00c796147d962
Cody Littley <cody@eigenlabs.org>
2025-03-14 12:38:23 -0500

    sync write everyhwere
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

--- common/kvstore/leveldb/leveldb_store.go
+++ common/kvstore/leveldb/leveldb_store.go
@@ -67,7 +67,7 @@ func (store *levelDBStore) Put(key []byte, value []byte) error {
 	if value == nil {
 		value = []byte{}
 	}
-	return store.db.Put(key, value, store.writeOptions)
+	return store.db.Put(key, value, &opt.WriteOptions{Sync: true}) // TODO don't merge with this enabled by default!!!
 }
 
 // Get retrieves data from the store. Returns kvstore.ErrNotFound if the data is not found.
@@ -107,7 +107,7 @@ func (store *levelDBStore) WriteBatch(keys [][]byte, values [][]byte) error {
 	for i, key := range keys {
 		batch.Put(key, values[i])
 	}
-	return store.db.Write(batch, store.writeOptions)
+	return store.db.Write(batch, &opt.WriteOptions{Sync: true}) // TODO don't merge with this enabled by default!!!
 }
 
 // NewBatch creates a new batch for the store.
@@ -137,7 +137,7 @@ func (m *levelDBBatch) Delete(key []byte) {
 }
 
 func (m *levelDBBatch) Apply() error {
-	return m.store.db.Write(m.batch, m.writeOptions)
+	return m.store.db.Write(m.batch, &opt.WriteOptions{Sync: true}) // TODO don't merge with this enabled by default!!!
 }
 
 // Size returns the number of operations in the batch.
