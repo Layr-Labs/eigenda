d8361b8369b1a27157faddef05e1afec0f92458d
Cody Littley <cody@eigenlabs.org>
2025-03-14 12:33:47 -0500

    enable sync levelDB writes
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

--- common/kvstore/leveldb/leveldb_store.go
+++ common/kvstore/leveldb/leveldb_store.go
@@ -91,7 +91,7 @@ func (store *levelDBStore) DeleteBatch(keys [][]byte) error {
 	for _, key := range keys {
 		batch.Delete(key)
 	}
-	return store.db.Write(batch, nil)
+	return store.db.Write(batch, &opt.WriteOptions{Sync: true}) // TODO don't merge with this enabled by default!!!
 }
 
 // WriteBatch adds multiple key-value pairs to the store.
