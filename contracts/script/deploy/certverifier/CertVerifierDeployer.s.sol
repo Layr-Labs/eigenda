9b4ccb14186debcffc05082a0cc1b2a982cea982
Ethen Pociask <ethen@eigenlabs.org>
2025-03-23 04:00:48 +0900

    chore: Cert verifier only using V1 docs

--- contracts/script/deploy/certverifier/CertVerifierDeployer.s.sol
+++ contracts/script/deploy/certverifier/CertVerifierDeployer.s.sol
@@ -21,19 +21,31 @@ import "src/interfaces/IEigenDAStructs.sol";
 //forge script script/deploy/certverifier/CertVerifierDeployer.s.sol:CertVerifierDeployer --sig "run(string, string)" <config.json> <output.json> --rpc-url $RPC --private-key $PRIVATE_KEY -vvvv --etherscan-api-key $ETHERSCAN_API_KEY --verify --broadcast
 contract CertVerifierDeployer is Script, Test {
 
-    address eigenDACertVerifier;
-
-    address eigenDAServiceManager;
-    address eigenDAThresholdRegistry;
-    address eigenDARelayRegistry;
-    address registryCoordinator ;
-    address operatorStateRetriever; 
-
+    address eigenDAServiceManager; // v1 && v2
+    address eigenDAThresholdRegistry; // v1 && v2
+    address eigenDARelayRegistry; // v2
+    address registryCoordinator; // v2
+    address operatorStateRetriever;  // v2
+    
     SecurityThresholds defaultSecurityThresholds;
     bytes quorumNumbersRequired;
 
+    // deployment target
+    address eigenDACertVerifier;
+
+
+    /**
+     * @TODO Add precursor checks using casted interfaces to ensure addreses actually map to what
+     *       the cert verifier expects! This is especially critical since rollups will be using
+     *       this for individual deployments in the future. This may not make sense to add until
+     *       EigenDA V2 contracts are deployed on mainnet.
+     * @dev loads addreses from json env to deploy new EigenDACertVerifier contract
+     * @param json json file used for extracting deployment address context
+     * @param outputPath target destination for outputing deployment results
+     */
     function run(string memory json, string memory outputPath) external {
 
+        // 1 - load dependency contracts from env
         string memory path = string.concat("./script/deploy/certverifier/config/", json);
         string memory data = vm.readFile(path);
 
@@ -58,6 +70,8 @@ contract CertVerifierDeployer is Script, Test {
         raw = stdJson.parseRaw(data, ".quorumNumbersRequired");
         quorumNumbersRequired = abi.decode(raw, (bytes));
 
+        // 2 - deploy cert verifier
+
         vm.startBroadcast();
 
         eigenDACertVerifier = address(
@@ -73,12 +87,14 @@ contract CertVerifierDeployer is Script, Test {
         ));
 
         vm.stopBroadcast();
-
+        
         console.log("Deployed new EigenDACertVerifier at address: ", eigenDACertVerifier);
 
+        // 3 - output deployment context
+
         string memory outputPath = string.concat("./script/deploy/certverifier/output/", outputPath);
-        string memory parent_object = "parent object";
-        string memory finalJson = vm.serializeAddress(parent_object, "eigenDACertVerifier", address(eigenDACertVerifier));
+        string memory parentObject = "parent object";
+        string memory finalJson = vm.serializeAddress(parentObject, "eigenDACertVerifier", address(eigenDACertVerifier));
         vm.writeJson(finalJson, outputPath);
     }
 }
