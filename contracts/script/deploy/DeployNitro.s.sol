dd0291eedbf32448d74450dd2ee5b34065a9e3aa
Patrick Kim <pakim249@gmail.com>
2025-03-27 17:46:58 +0000

    refactor: use fn calls

--- contracts/script/deploy/DeployNitro.s.sol
+++ contracts/script/deploy/DeployNitro.s.sol
@@ -5,6 +5,7 @@ import "forge-std/Script.sol";
 import "src/interfaces/IEigenDAStructs.sol";
 import "src/core/EigenDAThresholdRegistry.sol";
 import "src/core/EigenDACertVerifier.sol";
+import "src/core/EigenDAServiceManager.sol";
 import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
@@ -13,11 +14,9 @@ import "forge-std/console2.sol";
 contract DeployNitroCertVerifier is Script {
 
     address constant INITIAL_OWNER = 0x85C2AE9B88baDf751228e307Ae9ab76B74d84f5c;
-    address constant EIGEN_DA_SERVICE_MANAGER = 0x870679E138bCdf293b7Ff14dD44b70FC97e12fc0;
-    bytes constant QUORUM_ADVERSARY_THRESHOLD_PERCENTAGE = hex"212121";
-    bytes constant QUORUM_CONFIRMATION_THRESHOLD_PERCENTAGE = hex"373737";
-    bytes constant QUORUM_NUMBERS_REQUIRED = hex"0001";
 
+    EigenDAServiceManager constant EIGEN_DA_SERVICE_MANAGER = EigenDAServiceManager(0x870679E138bCdf293b7Ff14dD44b70FC97e12fc0);
+    
     address proxyAdmin;
 
     address thresholdRegistryProxy;
@@ -25,6 +24,7 @@ contract DeployNitroCertVerifier is Script {
     
     address certVerifier;
     
+    /// @dev This script is only to be run once, and the contracts deployed will be deprecated when EigenDA V2 is live on mainnet.
     function run() external {
         vm.startBroadcast(INITIAL_OWNER);
         proxyAdmin = address(new ProxyAdmin());
@@ -38,9 +38,9 @@ contract DeployNitroCertVerifier is Script {
                     EigenDAThresholdRegistry.initialize,
                     (
                         INITIAL_OWNER,
-                        QUORUM_ADVERSARY_THRESHOLD_PERCENTAGE,
-                        QUORUM_CONFIRMATION_THRESHOLD_PERCENTAGE,
-                        QUORUM_NUMBERS_REQUIRED,
+                        EIGEN_DA_SERVICE_MANAGER.quorumAdversaryThresholdPercentages(),
+                        EIGEN_DA_SERVICE_MANAGER.quorumConfirmationThresholdPercentages(),
+                        EIGEN_DA_SERVICE_MANAGER.quorumNumbersRequired(),
                         VERSIONED_BLOB_PARAMS()
                     )
                 )
@@ -51,8 +51,8 @@ contract DeployNitroCertVerifier is Script {
         certVerifier = address(
             new EigenDACertVerifier(
                 IEigenDAThresholdRegistry(thresholdRegistryProxy),
-                IEigenDABatchMetadataStorage(EIGEN_DA_SERVICE_MANAGER),
-                IEigenDASignatureVerifier(EIGEN_DA_SERVICE_MANAGER),
+                IEigenDABatchMetadataStorage(address(EIGEN_DA_SERVICE_MANAGER)),
+                IEigenDASignatureVerifier(address(EIGEN_DA_SERVICE_MANAGER)),
                 IEigenDARelayRegistry(address(0)), // UNUSED IN V1
                 OperatorStateRetriever(address(0)), // UNUSED IN V1
                 IRegistryCoordinator(address(0)), // UNUSED IN V1
