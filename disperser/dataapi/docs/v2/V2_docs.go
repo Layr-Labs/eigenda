b6eb103d03fdaec680d37408685763b3caf3dc50
Jian Xiao <99709935+jianoaix@users.noreply.github.com>
2025-03-31 14:11:48 -0700

    Enhance the metrics summary (#1398)

--- disperser/dataapi/docs/v2/V2_docs.go
+++ disperser/dataapi/docs/v2/V2_docs.go
@@ -358,7 +358,7 @@ const docTemplateV2 = `{
                     "200": {
                         "description": "OK",
                         "schema": {
-                            "$ref": "#/definitions/v2.Metric"
+                            "$ref": "#/definitions/v2.MetricSummary"
                         }
                     },
                     "400": {
@@ -1321,11 +1321,20 @@ const docTemplateV2 = `{
                 }
             }
         },
-        "v2.Metric": {
+        "v2.MetricSummary": {
             "type": "object",
             "properties": {
-                "throughput": {
+                "average_bytes_per_second": {
                     "type": "number"
+                },
+                "end_timestamp_sec": {
+                    "type": "integer"
+                },
+                "start_timestamp_sec": {
+                    "type": "integer"
+                },
+                "total_bytes_posted": {
+                    "type": "integer"
                 }
             }
         },
