e0994570bc1135b4562e24acd811dfc9ac74c58a
Cody Littley <cody@eigenlabs.org>
2025-04-03 14:28:51 -0500

    add new file
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

--- litt/util/unsafe_string.go
+++ litt/util/unsafe_string.go
@@ -5,8 +5,5 @@ import "unsafe"
 // UnsafeBytesToString converts a byte slice to a string without copying the data.
 // Note that once converted in this way, it is not safe to modify the byte slice for any reason.
 func UnsafeBytesToString(b []byte) string {
-	if len(b) == 0 {
-		return ""
-	}
-	return unsafe.String(&b[0], len(b))
+	return *(*string)(unsafe.Pointer(&b))
 }
