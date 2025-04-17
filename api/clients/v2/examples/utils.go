fe6be13a6592c0fa6afae9456c6e3fb557c3817f
litt3 <102969658+litt3@users.noreply.github.com>
2025-04-17 08:41:47 -0400

    Clarify meaning of 'size'
    Signed-off-by: litt3 <102969658+litt3@users.noreply.github.com>

--- api/clients/v2/examples/utils.go
+++ api/clients/v2/examples/utils.go
@@ -6,9 +6,9 @@ import (
 	"github.com/Layr-Labs/eigenda/api/clients/v2/coretypes"
 )
 
-// createRandomPayload creates a payload with random data of the specified size
-func createRandomPayload(size int) (*coretypes.Payload, error) {
-	payloadBytes := make([]byte, size)
+// createRandomPayload creates a payload with random data of the specified size in bytes
+func createRandomPayload(byteCount int) (*coretypes.Payload, error) {
+	payloadBytes := make([]byte, byteCount)
 	_, err := rand.Read(payloadBytes)
 	if err != nil {
 		return nil, err
