6a2428957fcf28340ce3f11af38766909532dfe0
hopeyen <60078528+hopeyen@users.noreply.github.com>
2025-05-07 15:17:53 -0500

    feat: validator/disperser clock synchrony (#1509)
    * tempt: scripts
    
    * Revert "tempt: scripts"
    
    This reverts commit a89efcc43e24bacdb46dd55cb163f3843d2a9aa7.
    
    * feat: NTP offset for (some) time
    
    * feat: disperser client uses NTP synced time
    
    * rfr: limit NTP to metering
    
    * feat: ntp synced clock struct
    
    * chore: code comment updates
    
    * feat: minor updates

--- api/clients/v2/disperser_client.go
+++ api/clients/v2/disperser_client.go
@@ -22,6 +22,8 @@ type DisperserClientConfig struct {
 	Hostname          string
 	Port              string
 	UseSecureGrpcFlag bool
+	NtpServer         string
+	NtpSyncInterval   time.Duration
 }
 
 // DisperserClient manages communication with the disperser server.
@@ -57,6 +59,7 @@ type disperserClient struct {
 	prover             encoding.Prover
 	accountant         *Accountant
 	accountantLock     sync.Mutex
+	ntpClock           *core.NTPSyncedClock
 }
 
 var _ DisperserClient = &disperserClient{}
@@ -95,11 +98,33 @@ func NewDisperserClient(config *DisperserClientConfig, signer corev2.BlobRequest
 		return nil, api.NewErrorInvalidArg("signer must be provided")
 	}
 
+	// Set default NTP config if not provided
+	if config.NtpServer == "" {
+		config.NtpServer = "pool.ntp.org"
+	}
+	if config.NtpSyncInterval == 0 {
+		config.NtpSyncInterval = 5 * time.Minute
+	}
+
+	// Initialize NTP synced clock
+	loggerConfig := common.DefaultLoggerConfig()
+	logger, err := common.NewLogger(loggerConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create logger: %w", err)
+	}
+	logger = logger.With("component", "DisperserClient")
+
+	ntpClock, err := core.NewNTPSyncedClock(context.Background(), config.NtpServer, config.NtpSyncInterval, logger)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create NTP clock: %w", err)
+	}
+
 	return &disperserClient{
 		config:     config,
 		signer:     signer,
 		prover:     prover,
 		accountant: accountant,
+		ntpClock:   ntpClock,
 		// conn and client are initialized lazily
 	}, nil
 }
@@ -186,7 +211,7 @@ func (c *disperserClient) DisperseBlobWithProbe(
 	}
 
 	symbolLength := encoding.GetBlobLengthPowerOf2(uint(len(data)))
-	payment, err := c.accountant.AccountBlob(ctx, time.Now().UnixNano(), uint64(symbolLength), quorums)
+	payment, err := c.accountant.AccountBlob(ctx, c.ntpClock.Now().UnixNano(), uint64(symbolLength), quorums)
 	if err != nil {
 		c.accountantLock.Unlock()
 		return nil, [32]byte{}, fmt.Errorf("error accounting blob: %w", err)
