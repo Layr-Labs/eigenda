1fc383aac7acd52e515663cfde6f4d7ca3bc8562
Cody Littley <56973212+cody-littley@users.noreply.github.com>
2025-05-09 10:24:24 -0500

    fix: auditor finding 7 (#1544)
    Fix auditor finding 7.
    
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

--- node/node_v2.go
+++ node/node_v2.go
@@ -157,12 +157,14 @@ func (n *Node) DownloadBundles(
 			// TODO (cody-littley) this is flaky, and will fail if any relay fails. We should retry failures
 			return nil, nil, fmt.Errorf("failed to get chunks from relays: %v", resp.err)
 		}
+
 		if len(resp.bundles) != len(resp.metadata) {
 			return nil, nil, fmt.Errorf("number of bundles and metadata do not match (%d != %d)",
 				len(resp.bundles), len(resp.metadata))
 		}
-		for i, bundle := range resp.bundles {
-			metadata := resp.metadata[i]
+
+		for j, bundle := range resp.bundles {
+			metadata := resp.metadata[j]
 			blobShards[metadata.blobShardIndex].Bundles[metadata.quorum], err = new(core.Bundle).Deserialize(bundle)
 			if err != nil {
 				return nil, nil, fmt.Errorf("failed to deserialize bundle: %v", err)
