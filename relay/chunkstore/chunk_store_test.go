cf77bcbd752fa9ae010f5ca4325ae6b7c412a9f3
pschork <354473+pschork@users.noreply.github.com>
2025-05-11 14:07:54 -0700

    FEAT: Experimental WASM build target for relay
    This changes allows us to compile relay to WASM
    
    Cloudflare Zero-Fee Egress would require hosting the relay as a [CloudFlare Worker](https://workers.cloudflare.com/) via WASM (go is not supported)
    
    WASM is itself a sandbox and does not allow for OS-level or related system calls. This means Inabox/dockertest is not supported.
    See annotations to `chunkstore/chunk_store_test.go` and `relay_test_utils.go` to exclude them from WASM build target.
    
    Running `relay.wasm` on terminal requires a nodejs wrapper defined under `wasm/run_wasm.js`. Hosted WASM services (ie on Cloudflare) would not require this.

--- relay/chunkstore/chunk_store_test.go
+++ relay/chunkstore/chunk_store_test.go
@@ -1,3 +1,5 @@
+//go:build !js
+// +build !js
 package chunkstore
 
 import (
---
+++ relay/wasm/run_wasm.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+
+// Load wasm_exec.js from same dir as this script
+const goPath = path.resolve(__dirname, "wasm_exec.js");
+require(goPath); // defines global `Go`
+
+const go = new Go();
+
+// Inject environment variables
+go.env = {
+  ...process.env, // include your shell's env
+};
+
+go.argv = ["relay.wasm", ...process.argv.slice(2)];
+
+// Read relay.wasm from same dir as script
+const wasmPath = path.resolve(__dirname, "relay.wasm");
+
+(async () => {
+  const wasmBuffer = fs.readFileSync(wasmPath);
+  const { instance } = await WebAssembly.instantiate(wasmBuffer, go.importObject);
+  try {
+    await go.run(instance);
+  } catch (err) {
+    console.error("WASM runtime error:", err);
+  }
+})();
