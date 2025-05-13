370cb10609ff27d027707f49c0fcf55c3d21cf0a
Patrick Kim <pakim249@gmail.com>
2025-05-13 19:54:43 +0000

    chore: readd all old stuff

--- contracts/compile.sh
+++ contracts/compile.sh
@@ -29,12 +29,8 @@ contracts="PaymentVault \
   AVSDirectory \
   DelegationManager \
   BitmapUtils \
-  EigenDACertVerifier \
-  EigenDACertVerifierV1 \
-  EigenDACertVerifierV2 \
-  EigenDACertVerifierRouter \
-  OperatorStateRetriever \
   EigenDAStateRetriever \
+  OperatorStateRetriever \
   RegistryCoordinator \
   BLSApkRegistry \
   IIndexRegistry \
@@ -42,11 +38,12 @@ contracts="PaymentVault \
   BN254 \
   EigenDAServiceManager \
   IEigenDAServiceManager \
+  MockRollup \
   EjectionManager \
+  EigenDACertVerifierV1 \
   EigenDACertVerifierV2 \
-  EigenDACertVerifierV3 \
+  EigenDACertVerifier \
   EigenDACertVerifierRouter \
-  IEigenDACertTypeBindings \
   EigenDAThresholdRegistry \
   EigenDARelayRegistry \
   IEigenDARelayRegistry \
