80e89f9cc9594025a08b27434bde7c831e61a8f3
Patrick Kim <pakim249@gmail.com>
2025-05-27 09:03:56 -0400

    Update contracts/src/periphery/cert/interfaces/IEigenDACertVerifier.sol
    Co-authored-by: Samuel Laferriere <samlaf92@gmail.com>

--- contracts/src/periphery/cert/interfaces/IEigenDACertVerifier.sol
+++ contracts/src/periphery/cert/interfaces/IEigenDACertVerifier.sol
@@ -11,6 +11,7 @@ import {EigenDATypesV1 as DATypesV1} from "src/core/libraries/v1/EigenDATypesV1.
 
 /// @notice The IEigenDACertVerifier interface provides the getters necessary to transform a BlobStatusReply received after a dispersal
 ///         into a Blob Certificate that can be verified by the EigenDACertVerifier that implements this interface version.
+// IEigenDACertVerifier provides the getters necessary to transform a BlobStatusReply received after a dispersal into a Cert that can be verified by the EigenDACertVerifier that implements this interface version.
 interface IEigenDACertVerifier {
     /// @notice Returns the EigenDAThresholdRegistry contract.
     function eigenDAThresholdRegistry() external view returns (IEigenDAThresholdRegistry);
