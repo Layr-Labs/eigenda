bf2a87cdf4f1f2c4bf13d902eb5748c86d46cb76
Samuel Laferriere <samlaf92@gmail.com>
2025-05-30 21:03:31 +0800

    docs: explain why our CertVerifierInternalError doesn't distinguish retryable errors

--- api/clients/v2/verification/errors.go
+++ api/clients/v2/verification/errors.go
@@ -12,6 +12,16 @@ func (e *CertVerifierInputError) Error() string {
 }
 
 // CertVerifierInternalError represents a 5xx-like error (unexpected, internal, infra, etc.)
+//
+// Our recommendation is to always retry Internal errors in case they were due to a temporary (network) issue.
+// TODO: we would want to distinguish temporary vs permanent errors here, to inform the client
+// as to whether its worth retrying the request. However, this is currently not possible because the
+// underlying geth binding library does not provide this information. For example, a Call()
+// does a bunch of things before the actual call, like abi encoding the inputs, which can fail,
+// and geth itself does not provide temporary/retryable semantics on its returned errors.
+// See https://github.com/ethereum/go-ethereum/blob/a9523b6428238a762e1a1e55e46ead47630c3a23/accounts/abi/bind/base.go#L169
+// It seems incredibly difficult in golang (maybe due to golang's laxity with error handling) to distinguish temporary errors. 
+// See the net package for exampple, which deprecated its Temporary() method: https://pkg.go.dev/net#Error
 type CertVerifierInternalError struct {
 	Msg string
 	// Err is optional and only present if an underlying error is available.
