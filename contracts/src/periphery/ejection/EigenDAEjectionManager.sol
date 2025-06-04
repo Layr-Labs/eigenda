17d7187f9ef041fe1c279a7abab2b455adaf3e16
Patrick Kim <pakim249@gmail.com>
2025-06-04 17:07:06 +0000

    feat: more ejection updates

--- contracts/src/periphery/ejection/EigenDAEjectionManager.sol
+++ contracts/src/periphery/ejection/EigenDAEjectionManager.sol
@@ -26,47 +26,58 @@ contract EigenDAEjectionManager {
 
     /// WATCHER FUNCTIONS
 
+    /// @notice Starts the ejection process for an operator. Takes a deposit from the watcher.
     function startEjection(address operator, bytes memory quorums) external onlyWatcher(msg.sender) {
         _takeDeposit(msg.sender);
         operator.startEjection(quorums);
     }
 
+    /// @notice Cancels the ejection process initiated by a watcher.
     function cancelEjectionByWatcher(address operator) external onlyWatcher(msg.sender) {
         _returnDeposit(msg.sender);
         operator.cancelEjection();
     }
 
+    /// @notice Completes the ejection process for an operator. Transfers the deposit back to the watcher.
     function completeEjection(address operator, bytes memory quorums) external onlyWatcher(msg.sender) {
         operator.completeEjection(quorums);
-        IRegistryCoordinator(_registryCoordinator).ejectOperator(operator, quorums);
+        _tryEjectOperator(operator, quorums);
         _returnDeposit(msg.sender);
     }
 
+    /// @notice Starts the churn process for an operator. Takes a deposit from the watcher.
     function startChurn(address operator, bytes memory quorums) external onlyWatcher(msg.sender) {
         _takeDeposit(msg.sender);
         operator.startChurn(quorums);
     }
 
+    /// @notice Cancels the churn process initiated by a watcher.
     function cancelChurnByWatcher(address operator) external onlyWatcher(msg.sender) {
         operator.cancelChurn();
         _returnDeposit(msg.sender);
     }
 
+    /// @notice Completes the churn process for an operator. Transfers the deposit back to the watcher.
     function completeChurn(address operator, bytes memory quorums) external onlyWatcher(msg.sender) {
         operator.completeChurn(quorums);
-        IRegistryCoordinator(_registryCoordinator).ejectOperator(operator, quorums);
+        _tryEjectOperator(operator, quorums);
         _returnDeposit(msg.sender);
     }
 
     /// OPERATOR FUNCTIONS
 
+    /// @notice Cancels the ejection process initiated by the operator. Transfers the deposit to the operator.
     function cancelEjection() external {
         msg.sender.cancelEjection();
         _returnDeposit(msg.sender);
     }
 
-    function cancelChurn(address lowerStakeOperator) external {
-        lowerStakeOperator; // TODO: OPERATOR MUST PROVE THAT CHURN IS INVALID BY PROVIDING A LOWER STAKE OPERATOR
+    /// @notice Completes the ejection process for the operator. Transfers the deposit to the operator.
+    function cancelChurn(address lowerStakeOperator, bytes memory quorums) external {
+        require(
+            _isOperatorWeightsGreater(lowerStakeOperator, msg.sender, quorums),
+            "EigenDAEjectionManager: Operator does not have greater weights"
+        );
         msg.sender.cancelChurn();
         _returnDeposit(msg.sender);
     }
@@ -98,11 +109,11 @@ contract EigenDAEjectionManager {
     }
 
     function churnDelay() external view returns (uint64) {
-        return EigenDAEjectionLib.churnParams().proceedingDelay;
+        return EigenDAEjectionLib.churnParams().delay;
     }
 
     function churnCooldown() external view returns (uint64) {
-        return EigenDAEjectionLib.churnParams().proceedingCooldown;
+        return EigenDAEjectionLib.churnParams().cooldown;
     }
 
     function ejectionInitiated(address operator) external view returns (bool) {
@@ -118,14 +129,44 @@ contract EigenDAEjectionManager {
     }
 
     function ejectionDelay() external view returns (uint64) {
-        return EigenDAEjectionLib.ejectionParams().proceedingDelay;
+        return EigenDAEjectionLib.ejectionParams().delay;
     }
 
     function ejectionCooldown() external view returns (uint64) {
-        return EigenDAEjectionLib.ejectionParams().proceedingCooldown;
+        return EigenDAEjectionLib.ejectionParams().cooldown;
     }
 
     /// INTERNAL FUNCTIONS
+
+    function _isOperatorWeightsGreater(address operator1, address operator2, bytes memory quorumNumbers)
+        internal
+        view
+        returns (bool)
+    {
+        uint96[] memory weights1 = _getOperatorWeights(operator1, quorumNumbers);
+        uint96[] memory weights2 = _getOperatorWeights(operator2, quorumNumbers);
+
+        for (uint256 i = 0; i < weights1.length; i++) {
+            if (weights1[i] <= weights2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function _getOperatorWeights(address operator, bytes memory quorumNumbers)
+        internal
+        view
+        returns (uint96[] memory weights)
+    {
+        weights = new uint96[](quorumNumbers.length);
+        for (uint256 i; i < quorumNumbers.length; i++) {
+            uint8 quorumNumber = uint8(quorumNumbers[i]);
+            weights[i] = IRegistryCoordinator(_registryCoordinator).stakeRegistry().weightOfOperatorForQuorum(
+                quorumNumber, operator
+            );
+        }
+    }
 
     function _takeDeposit(address sender) internal virtual {
         IERC20(_depositToken).safeTransferFrom(sender, address(this), _depositAmount);
@@ -137,5 +178,10 @@ contract EigenDAEjectionManager {
 
     function _onlyWatcher(address sender) internal view virtual {
         sender; // TODO: PLACEHOLDER UNTIL ACCESS CONTROL IS IMPLEMENTED
+    }
+
+    /// @notice Attempts to eject an operator. If the ejection fails, it catches the error and does nothing.
+    function _tryEjectOperator(address operator, bytes memory quorums) internal {
+        try IRegistryCoordinator(_registryCoordinator).ejectOperator(operator, quorums) {} catch {}
     }
 }
