02e12d8cf8831ebf16c1f648412f28920bce2652
Patrick Kim <pakim249@gmail.com>
2025-06-12 20:22:31 +0000

    feat: add sig verification method

--- contracts/src/periphery/ejection/EigenDAEjectionManager.sol
+++ contracts/src/periphery/ejection/EigenDAEjectionManager.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.9;
 import {EigenDAEjectionLib, EigenDAEjectionTypes} from "src/periphery/ejection/libraries/EigenDAEjectionLib.sol";
 import {SafeERC20, IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IRegistryCoordinator} from "lib/eigenlayer-middleware/src/interfaces/IRegistryCoordinator.sol";
+import {IBLSApkRegistry} from "lib/eigenlayer-middleware/src/interfaces/IBLSApkRegistry.sol";
+import {BLSSignatureChecker} from "lib/eigenlayer-middleware/src/BLSSignatureChecker.sol";
+import {BN254} from "lib/eigenlayer-middleware/src/libraries/BN254.sol";
 
 contract EigenDAEjectionManager {
     using EigenDAEjectionLib for address;
@@ -12,11 +15,19 @@ contract EigenDAEjectionManager {
     address internal immutable _depositToken;
     uint256 internal immutable _depositAmount;
     address internal immutable _registryCoordinator;
+    address internal immutable _signatureVerifier;
 
-    constructor(address depositToken_, uint256 depositAmount_, address registryCoordinator_) {
+    bytes32 internal constant CANCEL_EJECTION_TYPEHASH =
+        keccak256("CancelEjection(address operator, uint64 proceedingTime, uint64 lastProceedingInitiated, bytes quorums, address recipient)");
+    bytes32 internal constant CANCEL_CHURN_TYPEHASH =
+        keccak256("CancelChurn(address operator, address lowerStakeOperator, bytes quorums, uint64 proceedingTime, uint64 lastProceedingInitiated, address recipient)");
+
+
+    constructor(address depositToken_, uint256 depositAmount_, address registryCoordinator_, address signatureVerifier_) {
         _depositToken = depositToken_;
         _depositAmount = depositAmount_;
         _registryCoordinator = registryCoordinator_;
+        _signatureVerifier = signatureVerifier_;
     }
 
     modifier onlyWatcher(address sender) {
@@ -66,10 +77,27 @@ contract EigenDAEjectionManager {
 
     /// OPERATOR FUNCTIONS
 
+    function cancelEjectionWithSig(address operator, BN254.G2Point memory apkG2, BN254.G1Point memory sigma, address recipient) external {
+        (BN254.G1Point memory apk, ) = IRegistryCoordinator(_registryCoordinator).blsApkRegistry().getRegisteredPubkey(operator);
+        _verifySig(_cancelEjectionMessageHash(operator, recipient), apk, apkG2, sigma);
+
+        operator.cancelEjection();
+        _returnDeposit(recipient);
+    }
+
     /// @notice Cancels the ejection process initiated by the operator. Transfers the deposit to the operator.
     function cancelEjection() external {
         msg.sender.cancelEjection();
         _returnDeposit(msg.sender);
+    }
+
+
+    function cancelChurnWithSig(address operator, address lowerStakeOperator, bytes memory quorums, BN254.G2Point memory apkG2, BN254.G1Point memory sigma, address recipient) external {
+        (BN254.G1Point memory apk, ) = IRegistryCoordinator(_registryCoordinator).blsApkRegistry().getRegisteredPubkey(operator);
+        _verifySig(_cancelChurnMessageHash(operator, lowerStakeOperator, quorums, recipient), apk, apkG2, sigma);
+        
+        operator.cancelChurn();
+        _returnDeposit(recipient);
     }
 
     /// @notice Completes the ejection process for the operator. Transfers the deposit to the operator.
@@ -183,5 +211,38 @@ contract EigenDAEjectionManager {
     /// @notice Attempts to eject an operator. If the ejection fails, it catches the error and does nothing.
     function _tryEjectOperator(address operator, bytes memory quorums) internal {
         try IRegistryCoordinator(_registryCoordinator).ejectOperator(operator, quorums) {} catch {}
+    }
+
+    function _cancelEjectionMessageHash(
+        address operator,
+        address recipient
+    ) internal view returns (bytes32) {
+        return keccak256(
+            abi.encode(
+                CANCEL_EJECTION_TYPEHASH,EigenDAEjectionLib.ejectionStorage().operatorProceedingParams[operator], recipient
+            ));
+    }
+
+    function _cancelChurnMessageHash(
+        address operator,
+        address lowerStakeOperator,
+        bytes memory quorums,
+        address recipient
+    ) internal view returns (bytes32) {
+        return keccak256(
+            abi.encode(
+                CANCEL_CHURN_TYPEHASH,EigenDAEjectionLib.churnStorage().operatorProceedingParams[operator], lowerStakeOperator, quorums, recipient
+            ));
+    }
+
+    function _verifySig(
+        bytes32 messageHash,
+        BN254.G1Point memory apk,
+        BN254.G2Point memory apkG2,
+        BN254.G1Point memory sigma
+    ) internal view {
+        (bool paired, bool valid) = BLSSignatureChecker(_signatureVerifier).trySignatureAndApkVerification(messageHash, apk, apkG2, sigma);
+        require(paired, "EigenDAEjectionManager: Pairing failed");
+        require(valid, "EigenDAEjectionManager: Invalid signature");
     }
 }
