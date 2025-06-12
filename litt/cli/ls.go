74d3e8b182e62e4359c2522aa58e8c3d1b933e24
Cody Littley <cody@eigenlabs.org>
2025-06-12 07:45:05 -0500

    Added locking for LS.

--- litt/cli/ls.go
+++ litt/cli/ls.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"path"
 	"path/filepath"
 	"sort"
 
 	"github.com/Layr-Labs/eigenda/litt/disktable/segment"
+	"github.com/Layr-Labs/eigenda/litt/littbuilder"
 	"github.com/Layr-Labs/eigenda/litt/util"
 	"github.com/urfave/cli/v2"
 )
@@ -15,13 +17,23 @@ import (
 
 // Returns a list of LittDB tables at the specified LittDB path. Tables are alphabetically sorted by their names.
 // Returns an error if the path does not exist or if no tables are found.
-func ls(path string) ([]string, error) {
+func ls(rootPath string, fsync bool) ([]string, error) {
+
+	// Forbid touching tables in active use.
+	lockPath := path.Join(rootPath, littbuilder.LockfileName)
+	lock, err := util.NewFileLock(lockPath, fsync)
+	if err != nil {
+		return nil, fmt.Errorf("failed to acquire lock on %s: %v", rootPath, err)
+	}
+	defer func() {
+		_ = lock.Release()
+	}()
 
 	// LittDB has one directory under the root directory per table, with the name
 	// of the table being the name of the directory.
-	possibleTables, err := os.ReadDir(path)
+	possibleTables, err := os.ReadDir(rootPath)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read dir %s: %v", path, err)
+		return nil, fmt.Errorf("failed to read dir %s: %v", rootPath, err)
 	}
 
 	// Each table directory will contain a "segments" directory. Infer that any directory containing this directory
@@ -29,7 +41,11 @@ func ls(path string) ([]string, error) {
 	// there is no need to enforce that here.
 	tables := make([]string, 0, len(possibleTables))
 	for _, entry := range possibleTables {
-		segmentPath := filepath.Join(path, entry.Name(), segment.SegmentDirectory)
+		if !entry.IsDir() {
+			continue
+		}
+
+		segmentPath := filepath.Join(rootPath, entry.Name(), segment.SegmentDirectory)
 		exists, err := util.Exists(segmentPath)
 		if err != nil {
 			return nil, fmt.Errorf("failed to check if segment path %s exists: %v", segmentPath, err)
@@ -49,17 +65,15 @@ func lsCommand(ctx *cli.Context) error {
 	if ctx.NArg() != 1 {
 		return fmt.Errorf("ls command requires exactly one argument: <path>")
 	}
-	path := ctx.Args().Get(0)
-	path, err := util.SanitizePath(path)
+	rootPath := ctx.Args().Get(0)
+	rootPath, err := util.SanitizePath(rootPath)
 	if err != nil {
-		return fmt.Errorf("failed to sanitize path %s: %v", path, err)
+		return fmt.Errorf("failed to sanitize path %s: %v", rootPath, err)
 	}
 
-	// TODO grab litt lock
-
-	tables, err := ls(path)
+	tables, err := ls(rootPath, true)
 	if err != nil {
-		return fmt.Errorf("failed to list tables in path %s: %v", path, err)
+		return fmt.Errorf("failed to list tables in path %s: %v", rootPath, err)
 	}
 
 	for _, table := range tables {
