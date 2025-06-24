3070f4af8cb5af71d3ff58d7f4091875d524ec48
Patrick Kim <pakim249@gmail.com>
2025-06-24 19:40:04 +0000

    feat: restructure entry points

--- contracts/src/core/EigenDAAddressDirectory.sol
+++ contracts/src/core/EigenDAAddressDirectory.sol
@@ -2,32 +2,66 @@
 pragma solidity ^0.8.9;
 
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import {AddressDirectoryStorage} from "src/core/libraries/v3/address-directory/AddressDirectoryStorage.sol";
+import {AddressDirectoryLib} from "src/core/libraries/v3/address-directory/AddressDirectoryLib.sol";
 import {IEigenDAAddressDirectory} from "src/core/interfaces/IEigenDAAddressDirectory.sol";
 
 contract EigenDAAddressDirectory is OwnableUpgradeable, IEigenDAAddressDirectory {
+    using AddressDirectoryLib for string;
+    using AddressDirectoryLib for bytes32;
+
     function initialize(address _initialOwner) external initializer {
         _transferOwnership(_initialOwner);
     }
 
     /// @inheritdoc IEigenDAAddressDirectory
-    function setAddress(string memory name, address value) external onlyOwner {
-        AddressDirectoryStorage.Layout storage s = AddressDirectoryStorage.layout();
-        bytes32 key = keccak256(abi.encodePacked(name));
-        s.addresses[key] = value;
-        emit AddressSet(name, value);
+    function addAddress(string memory name, address value) external onlyOwner {
+        bytes32 key = name.getKey();
+
+        if (value == address(0)) {
+            revert InvalidAddress(name);
+        }
+        if (key.getAddress() != address(0)) {
+            revert AddressAlreadyExists(name);
+        }
+
+        key.setAddress(value);
+
+        emit AddressAdded(name, key, value);
+    }
+
+    /// @inheritdoc IEigenDAAddressDirectory
+    function replaceAddress(string memory name, address value) external onlyOwner {
+        bytes32 key = name.getKey();
+        address oldValue = key.getAddress();
+
+        require(oldValue != address(0), "Address does not exist");
+        require(value != address(0), "Invalid address");
+        require(oldValue != value, "Address already set");
+
+        key.setAddress(value);
+
+        emit AddressReplaced(name, key, oldValue, value);
+    }
+
+    /// @inheritdoc IEigenDAAddressDirectory
+    function removeAddress(string memory name) external onlyOwner {
+        bytes32 key = name.getKey();
+        address existingAddress = key.getAddress();
+
+        require(existingAddress != address(0), "Address does not exist");
+
+        key.setAddress(address(0));
+
+        emit AddressRemoved(name, key);
     }
 
     /// @inheritdoc IEigenDAAddressDirectory
     function getAddress(string memory name) external view returns (address) {
-        AddressDirectoryStorage.Layout storage s = AddressDirectoryStorage.layout();
-        bytes32 key = keccak256(abi.encodePacked(name));
-        return s.addresses[key];
+        return name.getKey().getAddress();
     }
 
     /// @inheritdoc IEigenDAAddressDirectory
     function getAddress(bytes32 key) external view returns (address) {
-        AddressDirectoryStorage.Layout storage s = AddressDirectoryStorage.layout();
-        return s.addresses[key];
+        return key.getAddress();
     }
 }
--- contracts/src/core/interfaces/IEigenDAAddressDirectory.sol
+++ contracts/src/core/interfaces/IEigenDAAddressDirectory.sol
@@ -1,11 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-interface IEigenDAAddressDirectory {
-    event AddressSet(string name, address indexed value);
+/// @notice Interface for the EigenDA Directory
+///         This contract is currently responsible just for managing a directory of addresses by name.
+///         In the future, it may be extended to include access control as well.
+interface IEigenDADirectory {
+    error AddressAlreadyExists(string name);
+    error AddressDoesNotExist(string name);
+    error InvalidAddress(string name);
 
-    /// @notice Sets the address of a contract.
-    function setAddress(string memory name, address value) external;
+    event AddressAdded(string name, bytes32 indexed key, address indexed value);
+    event AddressReplaced(string name, bytes32 indexed key, address indexed oldValue, address indexed newValue);
+    event AddressRemoved(string name, bytes32 indexed key);
+
+    /// @notice Adds a new address to the directory by name.
+    function addAddress(string memory name, address value) external;
+
+    /// @notice Replaces an existing address in the directory by name.
+    function replaceAddress(string memory name, address value) external;
+
+    /// @notice Removes an address from the directory by name.
+    function removeAddress(string memory name) external;
 
     /// @notice Gets the address by keccak256 hash of the name.
     function getAddress(bytes32 key) external view returns (address);
