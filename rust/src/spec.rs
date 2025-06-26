48056a21fb2a0ff740b36214fe9a88e062e0315c
Rok Černič <rok.cernic@gmail.com>
2025-06-26 11:51:00 +0200

    feat: impl EthereumHash and EthereumAddress (https://github.com/eigerco/sov-eigenda-adapter/pull/1)

--- rust/src/spec.rs
+++ rust/src/spec.rs
@@ -1,6 +1,6 @@
-use core::fmt;
-use std::{fmt::Display, str::FromStr};
+use std::str::FromStr;
 
+use alloy::primitives::{FixedBytes, wrap_fixed_bytes};
 use borsh::{BorshDeserialize, BorshSerialize};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
@@ -18,7 +18,7 @@ pub struct EigenDaSpec;
 
 impl DaSpec for EigenDaSpec {
     /// The hash of a DA layer block
-    type SlotHash = EthereumBlockHash;
+    type SlotHash = EthereumHash;
 
     /// The block header type used by the DA layer
     type BlockHeader = EthereumBlockHeader;
@@ -27,7 +27,7 @@ impl DaSpec for EigenDaSpec {
     type BlobTransaction = BlobWithSender;
 
     /// How transactions can be identified on the DA layer.
-    type TransactionId = EthereumBlockHash;
+    type TransactionId = EthereumHash;
 
     /// The type used to represent addresses on the DA layer.
     type Address = EthereumAddress;
@@ -54,7 +54,7 @@ pub struct RollupParams;
 pub struct EthereumBlockHeader;
 
 impl BlockHeaderTrait for EthereumBlockHeader {
-    type Hash = EthereumBlockHash;
+    type Hash = EthereumHash;
 
     fn prev_hash(&self) -> Self::Hash {
         todo!()
@@ -73,61 +73,10 @@ impl BlockHeaderTrait for EthereumBlockHeader {
     }
 }
 
-/// A wrapper for the Ethereum block hash.
-#[derive(
-    Clone,
-    Copy,
-    Debug,
-    PartialEq,
-    Eq,
-    Serialize,
-    Deserialize,
-    Hash,
-    BorshDeserialize,
-    BorshSerialize,
-)]
-pub struct EthereumBlockHash;
-
-impl BlockHashTrait for EthereumBlockHash {}
-
-impl FromStr for EthereumBlockHash {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        todo!()
-    }
-}
-
-impl AsRef<[u8]> for EthereumBlockHash {
-    fn as_ref(&self) -> &[u8] {
-        todo!()
-    }
-}
-
-impl From<[u8; 32]> for EthereumBlockHash {
-    fn from(value: [u8; 32]) -> Self {
-        todo!()
-    }
-}
-
-impl From<EthereumBlockHash> for [u8; 32] {
-    fn from(value: EthereumBlockHash) -> Self {
-        todo!()
-    }
-}
-
-impl fmt::Display for EthereumBlockHash {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        todo!()
-    }
-}
-
 #[derive(
     Debug,
+    derive_more::Display,
     Clone,
-    JsonSchema,
-    BorshSerialize,
-    BorshDeserialize,
     Serialize,
     Deserialize,
     Hash,
@@ -137,35 +86,80 @@ impl fmt::Display for EthereumBlockHash {
     Ord,
     UniversalWallet,
 )]
-pub struct EthereumAddress;
+pub struct EthereumAddress(
+    #[sov_wallet(as_ty = "[u8; 20]", display = "hex")] alloy::primitives::Address,
+);
 
 impl BasicAddress for EthereumAddress {}
 
+impl BorshSerialize for EthereumAddress {
+    fn serialize<W: std::io::Write>(&self, writer: &mut W) -> std::io::Result<()> {
+        writer.write_all(&self.0.0.0)
+    }
+}
+
+impl BorshDeserialize for EthereumAddress {
+    fn deserialize_reader<R: std::io::Read>(reader: &mut R) -> std::io::Result<Self> {
+        let bytes = <[u8; 20]>::deserialize_reader(reader)?;
+        Ok(Self(bytes.into()))
+    }
+}
+
+impl JsonSchema for EthereumAddress {
+    fn schema_name() -> String {
+        "EthereumAddress".to_string()
+    }
+
+    fn json_schema(_generator: &mut schemars::r#gen::SchemaGenerator) -> schemars::schema::Schema {
+        serde_json::from_value(serde_json::json!({
+            "type": "string",
+            "pattern": "^0x[a-fA-F0-9]{40}$",
+            "description": "An Ethereum address",
+        }))
+        .expect("valid schema")
+    }
+}
+
 impl AsRef<[u8]> for EthereumAddress {
     fn as_ref(&self) -> &[u8] {
-        todo!()
+        self.0.as_ref()
     }
 }
 
 impl FromStr for EthereumAddress {
-    type Err = anyhow::Error;
+    type Err = alloy::primitives::AddressError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        todo!()
+        Ok(EthereumAddress(
+            alloy::primitives::Address::parse_checksummed(s, None)?,
+        ))
     }
 }
 
-impl<'a> TryFrom<&'a [u8]> for EthereumAddress {
+impl TryFrom<&[u8]> for EthereumAddress {
     type Error = anyhow::Error;
 
-    fn try_from(value: &'a [u8]) -> Result<Self, Self::Error> {
-        todo!()
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        Ok(EthereumAddress(alloy::primitives::Address::try_from(
+            value,
+        )?))
     }
 }
 
-impl Display for EthereumAddress {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        todo!()
+wrap_fixed_bytes!(pub struct EthereumHash<32>;);
+
+impl BlockHashTrait for EthereumHash {}
+
+impl BorshSerialize for EthereumHash {
+    fn serialize<W: std::io::Write>(&self, writer: &mut W) -> Result<(), std::io::Error> {
+        BorshSerialize::serialize(&self.0.0, writer)
+    }
+}
+
+impl BorshDeserialize for EthereumHash {
+    fn deserialize_reader<R: std::io::Read>(reader: &mut R) -> std::io::Result<Self> {
+        let bytes = <[u8; 32]>::deserialize_reader(reader)?;
+        Ok(Self(FixedBytes::from(bytes)))
     }
 }
 
@@ -176,7 +170,7 @@ pub struct BlobWithSender;
 impl BlobReaderTrait for BlobWithSender {
     type Address = EthereumAddress;
 
-    type BlobHash = EthereumBlockHash;
+    type BlobHash = EthereumHash;
 
     fn sender(&self) -> Self::Address {
         todo!()
@@ -197,5 +191,82 @@ impl BlobReaderTrait for BlobWithSender {
     #[cfg(feature = "native")]
     fn advance(&mut self, num_bytes: usize) -> &[u8] {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use alloy::hex::{self};
+    use sov_rollup_interface::sov_universal_wallet::schema::Schema;
+
+    use crate::spec::{EthereumAddress, EthereumHash};
+
+    const ADDRESS: &str = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+    const HASH: &str = "0x1234567890123456789012345678901234567890123456789012345678901234";
+
+    // TODO: Add more tests and cleanup. Maybe even use proptest.
+
+    #[test]
+    fn test_ethereum_address_schema() {
+        let address = EthereumAddress::from_str(ADDRESS).unwrap();
+        let schema = Schema::of_single_type::<EthereumAddress>().unwrap();
+
+        let borsh_bytes = borsh::to_vec(&address).unwrap();
+        let deserialized: EthereumAddress = borsh::from_slice(&borsh_bytes).unwrap();
+        assert_eq!(deserialized, address);
+
+        let displayed_from_schema = schema.display(0, &borsh_bytes).unwrap();
+
+        let lowercase_address = ADDRESS.to_lowercase();
+        assert_eq!(&displayed_from_schema, &lowercase_address);
+    }
+
+    #[test]
+    fn test_address_display_from_string() {
+        let address = EthereumAddress::from_str(ADDRESS).unwrap();
+        let output = format!("{}", address);
+        assert_eq!(ADDRESS, output);
+    }
+
+    #[test]
+    fn test_ethereum_address_try_from() {
+        let bytes = hex::decode(&ADDRESS[2..]).unwrap();
+        let address = EthereumAddress::try_from(bytes.as_slice()).unwrap();
+        assert_eq!(address.to_string(), ADDRESS);
+    }
+
+    #[test]
+    fn test_ethereum_address_as_ref() {
+        let address = EthereumAddress::from_str(ADDRESS).unwrap();
+        let bytes = address.as_ref();
+        assert_eq!(bytes.len(), 20); // Ethereum addresses are 20 bytes
+        assert_eq!(hex::encode(bytes), ADDRESS[2..].to_lowercase());
+    }
+
+    #[test]
+    fn test_ethereum_address_invalid_input() {
+        // Test invalid length
+        let result = EthereumAddress::from_str("0x1234");
+        assert!(result.is_err());
+
+        // Test invalid hex
+        let result = EthereumAddress::from_str("0xg39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_ethereum_hash_serialization() {
+        let ethereum_hash = EthereumHash::from_str(HASH).unwrap();
+        let serde_serialized = serde_json::to_string(&ethereum_hash).unwrap();
+        let serde_deserialized: EthereumHash = serde_json::from_str(&serde_serialized).unwrap();
+
+        assert_eq!(ethereum_hash, serde_deserialized);
+
+        let borsh_serialized = borsh::to_vec(&ethereum_hash).unwrap();
+        let borsh_deserialized: EthereumHash = borsh::from_slice(&borsh_serialized).unwrap();
+
+        assert_eq!(ethereum_hash, borsh_deserialized);
     }
 }
