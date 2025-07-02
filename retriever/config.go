be3da55f2189a8cfccb738f4309de3099906606d
hopeyen <hopey1118@gmail.com>
2025-07-02 21:03:06 +0200

    fix(EigenDADirectory): add service manager flag back to retreiver

--- retriever/config.go
+++ retriever/config.go
@@ -42,9 +42,10 @@ func NewConfig(ctx *cli.Context) (*Config, error) {
 		MetricsConfig: MetricsConfig{
 			HTTPPort: ctx.GlobalString(flags.MetricsHTTPPortFlag.Name),
 		},
-		Timeout:          ctx.Duration(flags.TimeoutFlag.Name),
-		NumConnections:   ctx.Int(flags.NumConnectionsFlag.Name),
-		EigenDADirectory: ctx.GlobalString(flags.EigenDADirectoryFlag.Name),
-		EigenDAVersion:   version,
+		Timeout:                   ctx.Duration(flags.TimeoutFlag.Name),
+		NumConnections:            ctx.Int(flags.NumConnectionsFlag.Name),
+		EigenDADirectory:          ctx.GlobalString(flags.EigenDADirectoryFlag.Name),
+		EigenDAServiceManagerAddr: ctx.GlobalString(flags.EigenDAServiceManagerFlag.Name),
+		EigenDAVersion:            version,
 	}, nil
 }
