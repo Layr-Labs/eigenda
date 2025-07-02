c2b94e26106a7a1e31c5739c30172f98587f8a07
Patrick Kim <pakim249@gmail.com>
2025-07-02 20:01:25 +0000

    feat: add minimal deploy script

--- contracts/script/deploy/usage-authorization/DeployUsageAuthorizationRegistry.sol
+++ contracts/script/deploy/usage-authorization/DeployUsageAuthorizationRegistry.sol
@@ -4,30 +4,46 @@ pragma solidity =0.8.12;
 import {Script} from "forge-std/Script.sol";
 
 import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
-import {UsageAuthorizationRegistry} from "src/core/UsageAuthorizationRegistry.sol";
+import {UsageAuthorizationRegistry, UsageAuthorizationTypes} from "src/core/UsageAuthorizationRegistry.sol";
 import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 import {console2} from "forge-std/console2.sol";
+import {stdToml} from "forge-std/StdToml.sol";
 
 contract DeployUsageAuthorizationRegistry is Script {
+    using stdToml for string;
+
+    UsageAuthorizationRegistry usageAuthorizationRegistry;
+    UsageAuthorizationRegistry usageAuthorizationRegistryImpl;
+
+    string cfg;
+
+    function _initConfig() internal virtual {
+        cfg = vm.readFile(vm.envString("DEPLOY_CONFIG_PATH"));
+        console2.logString(cfg);
+    }
+
     function run() external {
+        _initConfig();
         vm.startBroadcast();
 
-        uint64 schedulePeriod = 1 hours; // Example schedule period
+        usageAuthorizationRegistryImpl = new UsageAuthorizationRegistry(uint64(cfg.readUint(".schedulePeriod")));
 
-        ProxyAdmin proxyAdmin = new ProxyAdmin();
-        UsageAuthorizationRegistry usageAuthorizationRegistry = new UsageAuthorizationRegistry(schedulePeriod);
-
-        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(
-            address(usageAuthorizationRegistry),
-            address(proxyAdmin),
-            abi.encodeWithSelector(UsageAuthorizationRegistry.initialize.selector, msg.sender)
+        usageAuthorizationRegistry = UsageAuthorizationRegistry(
+            address(
+                new TransparentUpgradeableProxy(
+                    address(usageAuthorizationRegistryImpl),
+                    address(cfg.readAddress(".proxyAdmin")),
+                    abi.encodeWithSelector(UsageAuthorizationRegistry.initialize.selector, msg.sender)
+                )
+            )
         );
 
         vm.stopBroadcast();
 
-        console2.log("ProxyAdmin deployed at:", address(proxyAdmin));
-        console2.log("UsageAuthorizationRegistry deployed at:", address(proxy));
-        console2.log("UsageAuthorizationRegistry implementation at:", address(usageAuthorizationRegistry));
+        console2.log("UsageAuthorizationRegistry deployed at:", address(usageAuthorizationRegistry));
+        console2.log("UsageAuthorizationRegistry implementation at:", address(usageAuthorizationRegistryImpl));
     }
 }
