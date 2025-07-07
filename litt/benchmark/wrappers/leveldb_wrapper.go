31ad088ef03085e14605ce55f9d7c7ee7d175126
Cody Littley <cody@eigenlabs.org>
2025-07-07 14:12:07 -0500

    Add metrics

--- litt/benchmark/wrappers/leveldb_wrapper.go
+++ litt/benchmark/wrappers/leveldb_wrapper.go
@@ -3,7 +3,9 @@ package wrappers
 import (
 	"errors"
 	"fmt"
+	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/Layr-Labs/eigenda/common"
@@ -11,14 +13,19 @@ import (
 	"github.com/Layr-Labs/eigenda/common/kvstore/tablestore"
 	"github.com/Layr-Labs/eigenda/litt/benchmark/config"
 	"github.com/Layr-Labs/eigenda/litt/util"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var _ DatabaseWrapper = (*LevelDBWrapper)(nil)
+var tableName = "benchmark"
 
 type LevelDBWrapper struct {
 	store      kvstore.TableStore
 	keyBuilder kvstore.KeyBuilder
 	ttl        time.Duration
+	metrics    *basicWrapperMetrics
 }
 
 var _ ThreadLocalDatabaseWrapper = (*ThreadLocalLevelDBWrapper)(nil)
@@ -28,6 +35,7 @@ type ThreadLocalLevelDBWrapper struct {
 	keyBuilder kvstore.KeyBuilder
 	batch      kvstore.TTLBatch[kvstore.Key]
 	ttl        time.Duration
+	metrics    *basicWrapperMetrics
 }
 
 var _ WrapperFactory = NewLevelDBWrapper
@@ -58,7 +66,7 @@ func NewLevelDBWrapper(cfg *config.BenchmarkConfig) (DatabaseWrapper, error) {
 	}
 
 	storeConfig := tablestore.DefaultConfig()
-	storeConfig.Schema = []string{"benchmark"}
+	storeConfig.Schema = []string{tableName}
 	storeConfig.Path = &path
 
 	store, err := tablestore.Start(logger, storeConfig)
@@ -66,17 +74,46 @@ func NewLevelDBWrapper(cfg *config.BenchmarkConfig) (DatabaseWrapper, error) {
 		return nil, fmt.Errorf("failed to start tablestore: %w", err)
 	}
 
-	keyBuilder, err := store.GetKeyBuilder("benchmark")
+	keyBuilder, err := store.GetKeyBuilder(tableName)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get key builder: %w", err)
 	}
 
 	ttl := time.Duration(cfg.TTLHours * float64(time.Hour))
 
+	var metrics *basicWrapperMetrics
+	if cfg.LittConfig.MetricsEnabled {
+		registry := prometheus.NewRegistry()
+		registry.MustRegister(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}))
+		registry.MustRegister(collectors.NewGoCollector())
+
+		logger.Infof("Starting metrics server at port %d", cfg.LittConfig.MetricsPort)
+		addr := fmt.Sprintf(":%d", cfg.LittConfig.MetricsPort)
+		mux := http.NewServeMux()
+		mux.Handle("/metrics", promhttp.HandlerFor(
+			registry,
+			promhttp.HandlerOpts{},
+		))
+		server := &http.Server{
+			Addr:    addr,
+			Handler: mux,
+		}
+
+		go func() {
+			err := server.ListenAndServe()
+			if err != nil && !strings.Contains(err.Error(), "http: Server closed") {
+				logger.Errorf("metrics server error: %v", err)
+			}
+		}()
+
+		metrics = newBasicWrapperMetrics(registry, tableName)
+	}
+
 	return &LevelDBWrapper{
 		store:      store,
 		keyBuilder: keyBuilder,
 		ttl:        ttl,
+		metrics:    metrics,
 	}, nil
 }
 
@@ -92,7 +129,12 @@ func (w *LevelDBWrapper) BuildThreadLocalWrapper() (ThreadLocalDatabaseWrapper, 
 }
 
 func (t *ThreadLocalLevelDBWrapper) Put(key []byte, value []byte) error {
+	start := time.Now()
 	t.batch.PutWithTTL(t.keyBuilder.Key(key), value, t.ttl)
+	elapsed := time.Since(start)
+	t.metrics.RecordWriteLatency(elapsed)
+	t.metrics.RecordBytesWritten(uint64(len(key) + len(value)))
+
 	return nil
 }
 
@@ -102,6 +144,8 @@ func (t *ThreadLocalLevelDBWrapper) Get(key []byte) (value []byte, exists bool, 
 	// inside a batch. Not important to implement this correctly, since the main point of this benchmark suite
 	// is to exercise LittDB, not the LevelDB wrapper.
 
+	start := time.Now()
+
 	value, err = t.store.Get(t.keyBuilder.Key(key))
 	if err != nil {
 		if errors.Is(err, kvstore.ErrNotFound) {
@@ -109,6 +153,11 @@ func (t *ThreadLocalLevelDBWrapper) Get(key []byte) (value []byte, exists bool, 
 		}
 		return nil, false, fmt.Errorf("failed to get key %s: %w", key, err)
 	}
+
+	end := time.Since(start)
+	t.metrics.RecordReadLatency(end)
+	t.metrics.RecordBytesRead(uint64(len(key) + len(value)))
+
 	return value, true, nil
 }
 
@@ -117,9 +166,15 @@ func (t *ThreadLocalLevelDBWrapper) Flush() error {
 		// No changes to flush
 		return nil
 	}
+	start := time.Now()
+
 	err := t.batch.Apply()
 	if err != nil {
 		return fmt.Errorf("failed to apply batch: %w", err)
 	}
+
+	elapsed := time.Since(start)
+	t.metrics.RecordFlushLatency(elapsed)
+
 	return nil
 }
---
+++ litt/benchmark/wrappers/wrapper_metrics.go
@@ -0,0 +1,131 @@
+package wrappers
+
+import (
+	"time"
+
+	"github.com/Layr-Labs/eigenda/common"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+// LittDB has a bunch of metrics it exports. This little utility replicates some of the more important metrics.
+// The names for these metrics mirror the names exported by LittDB, so that the metrics show up in the same dashboards.
+type basicWrapperMetrics struct {
+	tableName string
+
+	// The number of bytes written to disk since startup. Only includes values, not metadata.
+	bytesWrittenCounter *prometheus.CounterVec
+
+	// The number of bytes read from disk since startup.
+	bytesReadCounter *prometheus.CounterVec
+
+	// Reports on the write latency of the database.
+	writeLatency *prometheus.SummaryVec
+
+	// Reports on the latency of a flush operation.
+	flushLatency *prometheus.SummaryVec
+
+	// Reports on the read latency of the database. This metric includes both cache hits and cache misses.
+	readLatency *prometheus.SummaryVec
+}
+
+func newBasicWrapperMetrics(registry *prometheus.Registry, tableName string) *basicWrapperMetrics {
+	if registry == nil {
+		return nil
+	}
+
+	objectives := map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001}
+
+	bytesReadCounter := promauto.With(registry).NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: "litt",
+			Name:      "bytes_read",
+			Help:      "The number of bytes read from disk since startup.",
+		},
+		[]string{"table"},
+	)
+
+	readLatency := promauto.With(registry).NewSummaryVec(
+		prometheus.SummaryOpts{
+			Namespace: "litt",
+			Name:      "read_latency_ms",
+			Help: "Reports on the read latency of the database. " +
+				"This metric includes both cache hits and cache misses.",
+			Objectives: objectives,
+		},
+		[]string{"table"},
+	)
+
+	bytesWrittenCounter := promauto.With(registry).NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: "litt",
+			Name:      "bytes_written",
+			Help:      "The number of bytes written to disk since startup. Only includes values, not metadata.",
+		},
+		[]string{"table"},
+	)
+
+	writeLatency := promauto.With(registry).NewSummaryVec(
+		prometheus.SummaryOpts{
+			Namespace:  "litt",
+			Name:       "write_latency_ms",
+			Help:       "Reports on the write latency of the database.",
+			Objectives: objectives,
+		},
+		[]string{"table"},
+	)
+
+	flushLatency := promauto.With(registry).NewSummaryVec(
+		prometheus.SummaryOpts{
+			Namespace:  "litt",
+			Name:       "flush_latency_ms",
+			Help:       "Reports on the latency of a flush operation.",
+			Objectives: objectives,
+		},
+		[]string{"table"},
+	)
+
+	return &basicWrapperMetrics{
+		bytesWrittenCounter: bytesWrittenCounter,
+		bytesReadCounter:    bytesReadCounter,
+		writeLatency:        writeLatency,
+		flushLatency:        flushLatency,
+		readLatency:         readLatency,
+		tableName:           tableName,
+	}
+}
+
+func (m *basicWrapperMetrics) RecordBytesWritten(bytes uint64) {
+	if m == nil {
+		return
+	}
+	m.bytesWrittenCounter.WithLabelValues(m.tableName).Add(float64(bytes))
+}
+
+func (m *basicWrapperMetrics) RecordBytesRead(bytes uint64) {
+	if m == nil {
+		return
+	}
+	m.bytesReadCounter.WithLabelValues(m.tableName).Add(float64(bytes))
+}
+
+func (m *basicWrapperMetrics) RecordWriteLatency(latency time.Duration) {
+	if m == nil {
+		return
+	}
+	m.writeLatency.WithLabelValues(m.tableName).Observe(common.ToMilliseconds(latency))
+}
+
+func (m *basicWrapperMetrics) RecordFlushLatency(latency time.Duration) {
+	if m == nil {
+		return
+	}
+	m.flushLatency.WithLabelValues(m.tableName).Observe(common.ToMilliseconds(latency))
+}
+
+func (m *basicWrapperMetrics) RecordReadLatency(latency time.Duration) {
+	if m == nil {
+		return
+	}
+	m.readLatency.WithLabelValues(m.tableName).Observe(common.ToMilliseconds(latency))
+}
