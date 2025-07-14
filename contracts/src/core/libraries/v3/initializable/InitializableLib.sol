e6f41ce9d5e1ed1b04bbdf8cc81aa0b71868286f
Patrick Kim <pakim249@gmail.com>
2025-07-14 16:34:55 +0000

    fix: doc

--- contracts/src/core/libraries/v3/initializable/InitializableLib.sol
+++ contracts/src/core/libraries/v3/initializable/InitializableLib.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
-/// @notice Defines the storage layout for an address directory based on ERC-7201
+/// @notice Defines a storage layout based on ERC-7201
 ///         https://eips.ethereum.org/EIPS/eip-7201
 library InitializableStorage {
-    /// @custom: storage-location erc7201:address.directory.storage
+    /// @custom: storage-location erc7201:initializable.storage
     struct Layout {
         uint8 initialized;
     }
