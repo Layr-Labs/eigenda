0de84e7a9f320637819da00d4428774f73a9d8c5
Samuel Laferriere <9342524+samlaf@users.noreply.github.com>
2025-07-15 22:19:34 +0800

    chore: move test/integration_test under test/v1

--- test/v1/integration_test.go
+++ test/v1/integration_test.go
@@ -103,9 +103,9 @@ func init() {
 // makeTestEncoder makes an encoder currently using the only supported backend.
 func mustMakeTestComponents() (encoding.Prover, encoding.Verifier) {
 	config := &kzg.KzgConfig{
-		G1Path:          "../inabox/resources/kzg/g1.point",
-		G2Path:          "../inabox/resources/kzg/g2.point",
-		CacheDir:        "../inabox/resources/kzg/SRSTables",
+		G1Path:          "../../inabox/resources/kzg/g1.point",
+		G2Path:          "../../inabox/resources/kzg/g2.point",
+		CacheDir:        "../../inabox/resources/kzg/SRSTables",
 		SRSOrder:        3000,
 		SRSNumberToLoad: 3000,
 		NumWorker:       uint64(runtime.GOMAXPROCS(0)),
@@ -273,7 +273,7 @@ func mustMakeDisperser(t *testing.T, cst core.IndexedChainState, store disperser
 		dockertestPool, dockertestResource, err = deploy.StartDockertestWithLocalstackContainer(localStackPort)
 		if err != nil {
 			teardown()
-			panic("failed to start localstack container")
+			panic("failed to start localstack container" + err.Error())
 		}
 	}
 
