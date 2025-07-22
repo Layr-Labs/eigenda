7e6f1334f7789b7e6f1eac0abae5f9daf373db98
Cody Littley <cody@eigenlabs.org>
2025-07-22 11:46:12 -0500

    Made suggested changes.

--- litt/cli/litt_cli.go
+++ litt/cli/litt_cli.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 
-	"github.com/Layr-Labs/eigenda/common"
+	"github.com/Layr-Labs/eigensdk-go/logging"
 	"github.com/urfave/cli/v2"
 )
 
 // buildCliParser creates a command line parser for the LittDB CLI tool.
-func buildCLIParser() *cli.App {
+func buildCLIParser(logger logging.Logger) *cli.App {
 	app := &cli.App{
 		Name:  "litt",
 		Usage: "LittDB command line interface",
@@ -21,7 +20,7 @@ func buildCLIParser() *cli.App {
 				Usage:   "Enable debug mode. Program will pause for a debugger to attach.",
 			},
 		},
-		Before: handleDebugMode,
+		Before: buildHandleDebugMode(logger),
 		Commands: []*cli.Command{
 			{
 				Name:      "ls",
@@ -57,7 +56,7 @@ func buildCLIParser() *cli.App {
 				Name:  "rebase",
 				Usage: "Restructure LittDB file system layout.",
 				ArgsUsage: "--src <source-path1> ... --src <source-pathN> " +
-					"--dest <destination-path1> ... --dest <destination-pathN>",
+					"--dest <destination-path1> ... --dest <destination-pathN> [--preserve] [--quiet]",
 				Flags: []cli.Flag{
 					&cli.StringSliceFlag{
 						Name:     "src",
@@ -72,16 +71,14 @@ func buildCLIParser() *cli.App {
 						Required: true,
 					},
 					&cli.BoolFlag{
-						Name:     "preserve",
-						Aliases:  []string{"p"},
-						Usage:    "If enabled, then the old files are not removed.",
-						Required: false,
+						Name:    "preserve",
+						Aliases: []string{"p"},
+						Usage:   "If enabled, then the old files are not removed.",
 					},
 					&cli.BoolFlag{
-						Name:     "quiet",
-						Aliases:  []string{"q"},
-						Usage:    "Reduces the verbosity of the output.",
-						Required: false,
+						Name:    "quiet",
+						Aliases: []string{"q"},
+						Usage:   "Reduces the verbosity of the output.",
 					},
 				},
 				Action: nil, // rebaseCommand, // TODO this will be added in a follow up PR
@@ -94,9 +91,10 @@ func buildCLIParser() *cli.App {
 				Action:    nil, // benchmarkCommand, // TODO this will be added in a follow up PR
 			},
 			{
-				Name:      "prune",
-				Usage:     "Delete data from a LittDB database/snapshot.",
-				ArgsUsage: "--src <path1> ... --src <pathN> --max-age <duration in seconds>",
+				Name:  "prune",
+				Usage: "Delete data from a LittDB database/snapshot.",
+				ArgsUsage: "--src <path1> ... --src <pathN> --max-age <durationInSeconds> " +
+					"[--table <table1> ... --table <tableN>]",
 				Flags: []cli.Flag{
 					&cli.StringSliceFlag{
 						Name:     "src",
@@ -105,17 +103,15 @@ func buildCLIParser() *cli.App {
 						Required: true,
 					},
 					&cli.StringSliceFlag{
-						Name:     "table",
-						Aliases:  []string{"t"},
-						Usage:    "Prune this table. If not specified, all tables will be pruned.",
-						Required: false,
+						Name:    "table",
+						Aliases: []string{"t"},
+						Usage:   "Prune this table. If not specified, all tables will be pruned.",
 					},
 					&cli.Uint64Flag{
 						Name:    "max-age",
 						Aliases: []string{"a"},
 						Usage: "Maximum age of segments to keep, in seconds. " +
 							"Segments older than this will be deleted.",
-						Value:    0, // Default to 0, meaning no age limit
 						Required: true,
 					},
 				},
@@ -126,8 +122,8 @@ func buildCLIParser() *cli.App {
 				Usage: "Push data to a remote location using ssh and rsync.",
 				ArgsUsage: "--src <source-path1> ... --src <source-pathN> " +
 					"--dst <remote-path1> ... --dst <remote-pathN> " +
-					"[-i path/to/key] [-p port] [--no-gc] [--quiet] [--threads 42] [--throttle 100]" +
-					"<user>@<host>",
+					"[-i path/to/key] [-p port] [--no-gc] [--quiet] [--threads <threadCount>] " +
+					"[--throttle <maxMBPerSecond>] <user>@<host>",
 				Args: true,
 				Flags: []cli.Flag{
 					&cli.StringSliceFlag{
@@ -160,10 +156,9 @@ func buildCLIParser() *cli.App {
 						Usage:   "If true, do not delete files pushed to the remote host.",
 					},
 					&cli.BoolFlag{
-						Name:     "quiet",
-						Aliases:  []string{"q"},
-						Usage:    "Reduces the verbosity of the output.",
-						Required: false,
+						Name:    "quiet",
+						Aliases: []string{"q"},
+						Usage:   "Reduces the verbosity of the output.",
 					},
 					&cli.Uint64Flag{
 						Name:    "threads",
@@ -186,8 +181,9 @@ func buildCLIParser() *cli.App {
 					"Optionally calls 'litt prune' remotely to manage data retention.",
 				ArgsUsage: "--src <source-path1> ... --src <source-pathN> " +
 					"--dst <remote-path1> ... --dst <remote-pathN> " +
-					"[-i path/to/key] [-p port] [--no-gc] [--quiet] [--threads 42] [--throttle 100]" +
-					"[--max-age 100000] [--litt-binary /path/to/remote/bin/litt] [--period 300]" +
+					"[-i <pathToKey>] [-p <port>] [--no-gc] [--quiet] [--threads <threadCount>] " +
+					"[--throttle <maxMBPerSecond>] [--max-age <maxAgeInSeconds>] [--litt-binary " +
+					"</path/to/remote/bin/litt]> [--period <howOftenToPushInSeconds>]" +
 					"<user>@<host>",
 				Flags: []cli.Flag{
 					&cli.StringSliceFlag{
@@ -220,10 +216,9 @@ func buildCLIParser() *cli.App {
 						Usage:   "If true, do not delete files pushed to the remote host.",
 					},
 					&cli.BoolFlag{
-						Name:     "quiet",
-						Aliases:  []string{"q"},
-						Usage:    "Reduces the verbosity of the output.",
-						Required: false,
+						Name:    "quiet",
+						Aliases: []string{"q"},
+						Usage:   "Reduces the verbosity of the output.",
 					},
 					&cli.Uint64Flag{
 						Name:    "threads",
@@ -242,22 +237,19 @@ func buildCLIParser() *cli.App {
 						Aliases: []string{"a"},
 						Usage: "If non-zero, remotely run 'litt prune' to delete segments " +
 							"older than this age in seconds.",
-						Value:    0, // Default to 0, meaning no age limit
-						Required: false,
+						Value: 0, // Default to 0, meaning no age limit
 					},
 					&cli.StringFlag{
-						Name:     "litt-binary",
-						Aliases:  []string{"b"},
-						Usage:    "The remote location of the 'litt' CLI binary to use for pruning.",
-						Value:    "litt",
-						Required: false,
+						Name:    "litt-binary",
+						Aliases: []string{"b"},
+						Usage:   "The remote location of the 'litt' CLI binary to use for pruning.",
+						Value:   "litt",
 					},
 					&cli.Uint64Flag{
-						Name:     "period",
-						Aliases:  []string{"P"},
-						Usage:    "The period in seconds between sync operations.",
-						Value:    300,
-						Required: false,
+						Name:    "period",
+						Aliases: []string{"P"},
+						Usage:   "The period in seconds between sync operations.",
+						Value:   300,
 					},
 				},
 				Action: nil, // syncCommand, // TODO this will be added in a follow up PR
@@ -267,25 +259,25 @@ func buildCLIParser() *cli.App {
 	return app
 }
 
-// If the --debug flag is set, this function will block until SIGUSR1 is received to allow a debugger to attach.
-func handleDebugMode(ctx *cli.Context) error {
-	debugModeEnabled := ctx.Bool("debug")
+// Builds a function that is executed if the --debug flag is set. Causes the program to halt until ENTER is pressed.
+func buildHandleDebugMode(logger logging.Logger) func(*cli.Context) error {
 
-	if !debugModeEnabled {
+	// This double nesting is required in order to bind this method to a logger, while still conforming to the
+	// interface expected by urfave/cli.
+	return func(ctx *cli.Context) error {
+		debugModeEnabled := ctx.Bool("debug")
+
+		if !debugModeEnabled {
+			return nil
+		}
+
+		pid := os.Getpid()
+		logger.Infof("Waiting for debugger to attach (pid: %d).\n", pid)
+
+		logger.Infof("Press Enter to continue...")
+		reader := bufio.NewReader(os.Stdin)
+		_, _ = reader.ReadString('\n') // block until newline is read
+
 		return nil
 	}
-
-	logger, err := common.NewLogger(common.DefaultConsoleLoggerConfig())
-	if err != nil {
-		return fmt.Errorf("failed to create logger: %v", err)
-	}
-
-	pid := os.Getpid()
-	logger.Infof("Waiting for debugger to attach (pid: %d).\n", pid)
-
-	logger.Infof("Press Enter to continue...")
-	reader := bufio.NewReader(os.Stdin)
-	_, _ = reader.ReadString('\n') // block until newline is read
-
-	return nil
 }
