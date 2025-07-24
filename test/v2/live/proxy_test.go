7ab4c7a15ff6a3b75a30cba9ed06ca3ee205da39
Cody Littley <cody@eigenlabs.org>
2025-07-24 15:01:21 -0500

    more proxy tests

--- test/v2/live/proxy_test.go
+++ test/v2/live/proxy_test.go
@@ -4,15 +4,62 @@ import (
 	"testing"
 
 	"github.com/Layr-Labs/eigenda/common/testutils/random"
+	"github.com/Layr-Labs/eigenda/encoding"
+	"github.com/Layr-Labs/eigenda/encoding/utils/codec"
 	"github.com/Layr-Labs/eigenda/test/v2/client"
 	"github.com/docker/go-units"
 	"github.com/stretchr/testify/require"
 )
 
-// TODO test zero sized blob
-// TODO test small blob
-// TODO test max sized blob
-// TODO test blob that is too large
+// Disperse an empty payload. Blob will not be empty, since payload encoding entails adding bytes
+func emptyPayloadProxyDispersalTest(t *testing.T, environment string) {
+	payload := []byte{}
+
+	config, err := client.GetConfig(environment)
+	require.NoError(t, err)
+
+	c := client.GetTestClient(t, environment)
+	checkAndSetCertVerifierAddress(t, c, config.EigenDACertVerifierAddressQuorums0_1)
+
+	err = c.DisperseAndVerifyWithProxy(t.Context(), payload)
+	require.NoError(t, err)
+}
+
+func TestEmptyPayloadProxyDispersal(t *testing.T) {
+	environments, err := client.GetEnvironmentConfigPaths()
+	require.NoError(t, err)
+
+	for _, environment := range environments {
+		t.Run(getEnvironmentName(environment), func(t *testing.T) {
+			emptyPayloadProxyDispersalTest(t, environment)
+		})
+	}
+}
+
+// Disperse a 1 byte payload (no padding).
+func microscopicBlobProxyDispersalTest(t *testing.T, environment string) {
+	payload := []byte{1}
+
+	config, err := client.GetConfig(environment)
+	require.NoError(t, err)
+
+	c := client.GetTestClient(t, environment)
+	checkAndSetCertVerifierAddress(t, c, config.EigenDACertVerifierAddressQuorums0_1)
+
+	err = c.DisperseAndVerifyWithProxy(t.Context(), payload)
+	require.NoError(t, err)
+}
+
+func TestMicroscopicBlobProxyDispersal(t *testing.T) {
+	environments, err := client.GetEnvironmentConfigPaths()
+	require.NoError(t, err)
+
+	for _, environment := range environments {
+		t.Run(getEnvironmentName(environment), func(t *testing.T) {
+			microscopicBlobProxyDispersalTest(t, environment)
+		})
+	}
+}
 
 // Disperse a small payload (between 1KB and 2KB).
 func smallBlobProxyDispersalTest(t *testing.T, environment string) {
@@ -36,6 +83,36 @@ func TestSmallBlobProxyDispersal(t *testing.T) {
 	for _, environment := range environments {
 		t.Run(getEnvironmentName(environment), func(t *testing.T) {
 			smallBlobProxyDispersalTest(t, environment)
+		})
+	}
+}
+
+// Disperse a blob that is exactly at the maximum size after padding (16MB)
+func maximumSizedBlobProxyDispersalTest(t *testing.T, environment string) {
+	config, err := client.GetConfig(environment)
+	require.NoError(t, err)
+
+	maxPermissibleDataLength, err := codec.BlobSymbolsToMaxPayloadSize(
+		uint32(config.MaxBlobSize) / encoding.BYTES_PER_SYMBOL)
+	require.NoError(t, err)
+
+	rand := random.NewTestRandom()
+	payload := rand.Bytes(int(maxPermissibleDataLength))
+
+	c := client.GetTestClient(t, environment)
+	checkAndSetCertVerifierAddress(t, c, config.EigenDACertVerifierAddressQuorums0_1)
+
+	err = c.DisperseAndVerifyWithProxy(t.Context(), payload)
+	require.NoError(t, err)
+}
+
+func TestMaximumSizedBlobProxyDispersal(t *testing.T) {
+	environments, err := client.GetEnvironmentConfigPaths()
+	require.NoError(t, err)
+
+	for _, environment := range environments {
+		t.Run(getEnvironmentName(environment), func(t *testing.T) {
+			maximumSizedBlobProxyDispersalTest(t, environment)
 		})
 	}
 }
