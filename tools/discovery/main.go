e3bfc732d3a4659be178297e8a70985143c9d1ef
Samuel Laferriere <9342524+samlaf@users.noreply.github.com>
2025-07-28 23:44:04 +0800

    docs: clearer networkflag usage string

--- tools/discovery/main.go
+++ tools/discovery/main.go
@@ -40,7 +40,7 @@ func main() {
 		},
 		&cli.StringFlag{
 			Name: NetworkFlagName,
-			Usage: fmt.Sprintf(`The EigenDA network to discover (%s, %s, %s, %s).
+			Usage: fmt.Sprintf(`The EigenDA network to discover (one of: %s, %s, %s, %s).
 Must match the chain-id of the ethereum rpc url provided. Used to select the hardcoded default EigenDADirectory address.
 That address can be overridden by providing the --%s flag.`,
 				proxycmn.MainnetEigenDANetwork,
