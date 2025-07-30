f65a5f3150a7432141ade9e7aa8c86237321a40e
Patrick Kim <pakim249@gmail.com>
2025-07-30 20:24:34 +0000

    feat: use access control enumerable

--- contracts/src/core/EigenDAAccessControl.sol
+++ contracts/src/core/EigenDAAccessControl.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {AccessControlEnumerable} from "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import {AccessControlConstants} from "src/core/libraries/v3/access-control/AccessControlConstants.sol";
 
 /// @title EigenDAAccessControl
 /// @notice This contract is to serve as the centralized source of truth for access control in all EigenDA contracts.
-contract EigenDAAccessControl is AccessControl {
+contract EigenDAAccessControl is AccessControlEnumerable {
     constructor(address owner) {
         // The DEFAULT_ADMIN_ROLE can set the admin role for all other roles, and should be put behind a timelock.
         _grantRole(DEFAULT_ADMIN_ROLE, owner);
