42e3a2a7dd7a3f7fb95950680fe94f38adc03522
Cody Littley <cody@eigenlabs.org>
2025-07-31 11:19:54 -0500

    implemented unlock
    Signed-off-by: Cody Littley <cody@eigenlabs.org>

--- litt/cli/unlock.go
+++ litt/cli/unlock.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/Layr-Labs/eigenda/common"
+	"github.com/Layr-Labs/eigenda/litt/util"
 	"github.com/Layr-Labs/eigensdk-go/logging"
 	"github.com/urfave/cli/v2"
 )
@@ -20,18 +24,38 @@ func unlockCommand(ctx *cli.Context) error {
 		return fmt.Errorf("at least one source path is required")
 	}
 
-	return Unlock(logger, sources, true)
+	return Unlock(logger, sources)
 }
 
 // Unlocks a LittDB file system.
 //
 // DANGER: calling this method opens the door for unsafe concurrent operations on LittDB files.
 // With great power comes great responsibility.
-func Unlock(logger logging.Logger, sourcePaths []string, fsync bool) error {
+func Unlock(logger logging.Logger, sourcePaths []string) error {
+	for _, sourcePath := range sourcePaths {
+		err := filepath.WalkDir(sourcePath, func(path string, d os.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+			if d.IsDir() {
+				return nil
+			}
 
-	return nil
-}
+			if strings.HasSuffix(path, util.LockfileName) {
+				logger.Infof("Removing lock file %s", path)
+				if removeErr := os.Remove(path); removeErr != nil {
+					logger.Error("Failed to remove lock file", "path", path, "error", removeErr)
+					return fmt.Errorf("failed to remove lock file %s: %w", path, removeErr)
+				}
+			}
 
-func UnlockTable(logger logging.Logger, sourcePaths []string, tableName string, fsync bool) error {
+			return nil
+		})
+
+		if err != nil {
+			return fmt.Errorf("failed to walk directory %s: %w", sourcePath, err)
+		}
+	}
+
 	return nil
 }
