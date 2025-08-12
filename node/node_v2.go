f60cd5739a7cb5c4d2b3ebd18e0327406053f683
Cody Littley <cody@eigenlabs.org>
2025-08-12 10:58:33 -0500

    Fix semaphore use for StoreChunks()

--- node/node_v2.go
+++ node/node_v2.go
@@ -33,54 +33,43 @@ type RawBundle struct {
 	Bundle          []byte
 }
 
-func (n *Node) DownloadBundles(
-	ctx context.Context,
+// Determines where to find the chunks we need to download for a given batch.
+func (n *Node) DetermineChunkLocations(
 	batch *corev2.Batch,
 	operatorState *core.OperatorState,
 	probe *common.SequenceProbe,
-) ([]*corev2.BlobShard, []*RawBundle, error) {
+) (downloadSizeInBytes uint64, relayRequests map[corev2.RelayKey]*relayRequest, err error) {
 
 	probe.SetStage("prepare_to_download")
 
 	relayClient, ok := n.RelayClient.Load().(relay.RelayClient)
 
 	if !ok || relayClient == nil {
-		return nil, nil, fmt.Errorf("relay client is not set")
+		return 0, nil, fmt.Errorf("relay client is not set")
 	}
 
 	blobVersionParams := n.BlobVersionParams.Load()
 	if blobVersionParams == nil {
-		return nil, nil, fmt.Errorf("blob version params is nil")
+		return 0, nil, fmt.Errorf("blob version params is nil")
 	}
 
-	blobShards := make([]*corev2.BlobShard, len(batch.BlobCertificates))
-	rawBundles := make([]*RawBundle, len(batch.BlobCertificates))
-	requests := make(map[corev2.RelayKey]*relayRequest)
-
-	// Tally the number of bytes we are about to download.
-	var downloadSizeInBytes uint64
+	relayRequests = make(map[corev2.RelayKey]*relayRequest)
 
 	for i, cert := range batch.BlobCertificates {
 		blobKey, err := cert.BlobHeader.BlobKey()
 		if err != nil {
-			return nil, nil, fmt.Errorf("failed to get blob key: %v", err)
+			return 0, nil, fmt.Errorf("failed to get blob key: %w", err)
 		}
 
 		if len(cert.RelayKeys) == 0 {
-			return nil, nil, fmt.Errorf("no relay keys in the certificate")
-		}
-		blobShards[i] = &corev2.BlobShard{
-			BlobCertificate: cert,
-		}
-		rawBundles[i] = &RawBundle{
-			BlobCertificate: cert,
+			return 0, nil, fmt.Errorf("no relay keys in the certificate")
 		}
 		relayIndex := rand.Intn(len(cert.RelayKeys))
 		relayKey := cert.RelayKeys[relayIndex]
 
 		blobParams, ok := blobVersionParams.Get(cert.BlobHeader.BlobVersion)
 		if !ok {
-			return nil, nil, fmt.Errorf("blob version %d not found", cert.BlobHeader.BlobVersion)
+			return 0, nil, fmt.Errorf("blob version %d not found", cert.BlobHeader.BlobVersion)
 		}
 
 		assgn, err := corev2.GetAssignmentForBlob(operatorState, blobParams, cert.BlobHeader.QuorumNumbers, n.Config.ID)
@@ -91,17 +80,17 @@ func (n *Node) DownloadBundles(
 
 		chunkLength, err := blobParams.GetChunkLength(uint32(cert.BlobHeader.BlobCommitments.Length))
 		if err != nil {
-			return nil, nil, fmt.Errorf("failed to get chunk length: %w", err)
+			return 0, nil, fmt.Errorf("failed to get chunk length: %w", err)
 		}
 		downloadSizeInBytes += uint64(assgn.NumChunks() * chunkLength)
 
-		req, ok := requests[relayKey]
+		req, ok := relayRequests[relayKey]
 		if !ok {
 			req = &relayRequest{
 				chunkRequests: make([]*relay.ChunkRequestByIndex, 0),
 				metadata:      make([]*requestMetadata, 0),
 			}
-			requests[relayKey] = req
+			relayRequests[relayKey] = req
 		}
 		// Chunks from one blob are requested to the same relay
 		req.chunkRequests = append(req.chunkRequests, &relay.ChunkRequestByIndex{
@@ -115,27 +104,38 @@ func (n *Node) DownloadBundles(
 
 	}
 
-	// storeChunksSemaphore can be nil during unit tests, since there are a bunch of places where the Node struct
-	// is instantiated directly without using the constructor.
-	if n.storeChunksSemaphore != nil {
-		// So far, we've only downloaded metadata for the blob. Before downloading the actual chunks, make sure there
-		// is capacity in the store chunks buffer. This is an OOM safety measure.
+	return downloadSizeInBytes, relayRequests, nil
+}
 
-		probe.SetStage("acquire_buffer_capacity")
-		semaphoreCtx, cancel := context.WithTimeout(ctx, n.Config.StoreChunksBufferTimeout)
-		defer cancel()
-		err := n.storeChunksSemaphore.Acquire(semaphoreCtx, int64(downloadSizeInBytes))
-		if err != nil {
-			return nil, nil, fmt.Errorf("failed to acquire buffer capacity: %w", err)
+func (n *Node) DownloadChunksFromRelays(
+	ctx context.Context,
+	batch *corev2.Batch,
+	relayRequests map[corev2.RelayKey]*relayRequest,
+	probe *common.SequenceProbe,
+) (blobShards []*corev2.BlobShard, rawBundles []*RawBundle, err error) {
+
+	blobShards = make([]*corev2.BlobShard, len(batch.BlobCertificates))
+	rawBundles = make([]*RawBundle, len(batch.BlobCertificates))
+	for i, cert := range batch.BlobCertificates {
+		blobShards[i] = &corev2.BlobShard{
+			BlobCertificate: cert,
 		}
+		rawBundles[i] = &RawBundle{
+			BlobCertificate: cert,
+		}
+	}
+
+	relayClient, ok := n.RelayClient.Load().(relay.RelayClient)
+	if !ok || relayClient == nil {
+		return nil, nil, fmt.Errorf("relay client is not set")
 	}
 
 	probe.SetStage("download")
 
-	bundleChan := make(chan response, len(requests))
-	for relayKey := range requests {
+	bundleChan := make(chan response, len(relayRequests))
+	for relayKey := range relayRequests {
 		relayKey := relayKey
-		req := requests[relayKey]
+		req := relayRequests[relayKey]
 		n.DownloadPool.Submit(func() {
 			ctxTimeout, cancel := context.WithTimeout(ctx, n.Config.ChunkDownloadTimeout)
 			defer cancel()
@@ -157,14 +157,14 @@ func (n *Node) DownloadBundles(
 		})
 	}
 
-	responses := make([]response, len(requests))
-	for i := 0; i < len(requests); i++ {
+	responses := make([]response, len(relayRequests))
+	for i := 0; i < len(relayRequests); i++ {
 		responses[i] = <-bundleChan
 	}
 
 	probe.SetStage("deserialize")
 
-	for i := 0; i < len(requests); i++ {
+	for i := 0; i < len(relayRequests); i++ {
 		resp := responses[i]
 		if resp.err != nil {
 			// TODO (cody-littley) this is flaky, and will fail if any relay fails. We should retry failures
@@ -172,8 +172,9 @@ func (n *Node) DownloadBundles(
 		}
 
 		if len(resp.bundles) != len(resp.metadata) {
-			return nil, nil, fmt.Errorf("number of bundles and metadata do not match (%d != %d)",
-				len(resp.bundles), len(resp.metadata))
+			return nil, nil,
+				fmt.Errorf("number of bundles and metadata do not match (%d != %d)",
+					len(resp.bundles), len(resp.metadata))
 		}
 
 		for j, bundle := range resp.bundles {
