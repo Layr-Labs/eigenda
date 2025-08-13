4a3bc7a15d9991e6f930469d023d3ca9b1cb32b8
anim001k <140460766+anim001k@users.noreply.github.com>
2025-08-13 15:03:46 +0200

    Update server.go

--- common/pprof/server.go
+++ common/pprof/server.go
@@ -3,8 +3,9 @@ package pprof
 import (
 	"fmt"
 	"net/http"
-
-	_ "net/http/pprof"
+	pprofhttp "net/http/pprof"
+	"os"
+	"time"
 
 	"github.com/Layr-Labs/eigensdk-go/logging"
 )
@@ -23,9 +24,27 @@ func NewPprofProfiler(httpPort string, logger logging.Logger) *PprofProfiler {
 
 // Start the pprof server
 func (p *PprofProfiler) Start() {
-	pprofAddr := fmt.Sprintf("%s:%s", "0.0.0.0", p.httpPort)
+	host := os.Getenv("EIGENDA_PPROF_HOST")
+	if host == "" {
+		host = "127.0.0.1"
+	}
+	addr := fmt.Sprintf("%s:%s", host, p.httpPort)
 
-	if err := http.ListenAndServe(pprofAddr, nil); err != nil {
-		p.logger.Error("pprof server failed", "error", err, "pprofAddr", pprofAddr)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/debug/pprof/", pprofhttp.Index)
+	mux.HandleFunc("/debug/pprof/cmdline", pprofhttp.Cmdline)
+	mux.HandleFunc("/debug/pprof/profile", pprofhttp.Profile)
+	mux.HandleFunc("/debug/pprof/symbol", pprofhttp.Symbol)
+	mux.HandleFunc("/debug/pprof/trace", pprofhttp.Trace)
+
+	srv := &http.Server{
+		Addr:              addr,
+		Handler:           mux,
+		ReadHeaderTimeout: 5 * time.Second,
+		IdleTimeout:       60 * time.Second,
+	}
+
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		p.logger.Error("pprof server failed", "error", err, "pprofAddr", addr)
 	}
 }
