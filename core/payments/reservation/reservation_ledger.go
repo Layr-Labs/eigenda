570127842f9fc6a67dedeee5738fd1a45f8b9b5a
litt3 <102969658+litt3@users.noreply.github.com>
2025-08-15 09:56:03 -0400

    Fix merge
    Signed-off-by: litt3 <102969658+litt3@users.noreply.github.com>

--- core/payments/reservation/reservation_ledger.go
+++ core/payments/reservation/reservation_ledger.go
@@ -8,9 +8,9 @@ import (
 	"github.com/Layr-Labs/eigenda/core"
 )
 
-// Keeps track of the state of a given reservation
+// Tracks usage of a single account reservation
 //
-// This is a goroutine safe wrapper around the LeakyBucket algorithm.
+// This struct is goroutine safe.
 type ReservationLedger struct {
 	config ReservationLedgerConfig
 
@@ -21,15 +21,16 @@ type ReservationLedger struct {
 	leakyBucket *LeakyBucket
 }
 
-// Creates a new reservation ledger, which represents the reservation of a single user with a leaky bucket
+// Creates a new reservation ledger, which represents the reservation of a single user with a [LeakyBucket]
 func NewReservationLedger(
 	config ReservationLedgerConfig,
+	// now should be from a source that includes monotonic timestamp for best results
 	now time.Time,
 ) (*ReservationLedger, error) {
 	leakyBucket, err := NewLeakyBucket(
 		config.reservation.symbolsPerSecond,
 		config.bucketCapacityDuration,
-		config.biasBehavior,
+		config.startFull,
 		config.overfillBehavior,
 		now,
 	)
@@ -45,34 +46,27 @@ func NewReservationLedger(
 
 // Debit the reservation with a number of symbols.
 //
-// Algorithmically, that means adding a number of symbols to the leaky bucket.
+// [ReservationLedger.CheckInvariants] should be called prior to calling [ReservationLedger.Debit], to make sure the
+// dispersal is permitted under the parameters of the reservation. If [ReservationLedger.CheckInvariants] succeeds,
+// then [ReservationLedger.Debit] is called to make sure the dispersal doesn't exceed reservation capacity.
 //
 // Returns (true, nil) if the reservation has enough capacity to perform the debit.
 // Returns (false, nil) if the bucket lacks capacity to permit the fill.
 // Returns (false, error) if an error occurs. Possible errors include:
-//   - ErrQuorumNotPermitted: requested quorums are not permitted by the reservation
-//   - ErrTimeOutOfRange: dispersal time is outside the reservation's valid time range
-//   - ErrLockAcquisition: failed to acquire the internal reservation lock
-//   - ErrTimeMovedBackward: current time is before a previously observed time
+//   - [TimeMovedBackwardError]: current time is before a previously observed time (only possible if input time
+//     instances don't included monotonic timestamps)
 //   - Generic errors for all other unexpected behavior
 //
 // If the bucket doesn't have enough capacity to accommodate the fill, symbolCount IS NOT added to the bucket, i.e. a
 // failed debit doesn't count against the meter.
 func (rl *ReservationLedger) Debit(
+	// now should be from a source that includes monotonic timestamp for best results.
+	// This a local time from the perspective of the entity that owns this ledger instance, to be used with the local
+	// leaky bucket: it should NOT be sourced from the PaymentHeader
 	now time.Time,
+	// the number of symbols to debit
 	symbolCount uint32,
-	quorums []core.QuorumID,
 ) (bool, error) {
-	err := rl.config.reservation.CheckQuorumsPermitted(quorums)
-	if err != nil {
-		return false, fmt.Errorf("check quorums permitted: %w", err)
-	}
-
-	err = rl.config.reservation.CheckTime(now)
-	if err != nil {
-		return false, fmt.Errorf("check time: %w", err)
-	}
-
 	rl.lock.Lock()
 	defer rl.lock.Unlock()
 
@@ -85,6 +79,11 @@ func (rl *ReservationLedger) Debit(
 }
 
 // Credit the reservation with a number of symbols. This method "undoes" a previous debit, following a failed dispersal.
+//
+// Note that this method doesn't reset the state of the ledger to be the same as when the debit was made: it just
+// "refunds" the amount of symbols that were originally debited. Since the leaky bucket backing the reservation can't
+// get emptier than "empty", it may be the case that only a portion of the debit is reverted, with the final capacity
+// being clamped to 0.
 func (rl *ReservationLedger) RevertDebit(now time.Time, symbolCount uint32) error {
 	rl.lock.Lock()
 	defer rl.lock.Unlock()
@@ -92,6 +91,31 @@ func (rl *ReservationLedger) RevertDebit(now time.Time, symbolCount uint32) erro
 	err := rl.leakyBucket.RevertFill(now, symbolCount)
 	if err != nil {
 		return fmt.Errorf("revert fill: %w", err)
+	}
+
+	return nil
+}
+
+// Checks whether a dispersal with given properties is permitted under the parameters of the reservation.
+//
+// This check should be called prior to calling [ReservationLedger.Debit].
+//
+// Returns [QuorumNotPermittedError] if requested quorums are not permitted by the reservation
+// Returns [TimeOutOfRangeError] if dispersal time is outside the reservation's valid time range
+func (rl *ReservationLedger) CheckInvariants(
+	// the quorums listed in the BlobHeader
+	quorums []core.QuorumID,
+	// the timestamp included in the PaymentHeader
+	dispersalTime time.Time,
+) error {
+	err := rl.config.reservation.CheckQuorumsPermitted(quorums)
+	if err != nil {
+		return fmt.Errorf("check quorums permitted: %w", err)
+	}
+
+	err = rl.config.reservation.CheckTime(dispersalTime)
+	if err != nil {
+		return fmt.Errorf("check time: %w", err)
 	}
 
 	return nil
