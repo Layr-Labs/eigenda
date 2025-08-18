7579da28fd0ee48889f1370c58c22f8e76e5dfbd
litt3 <102969658+litt3@users.noreply.github.com>
2025-08-18 09:52:48 -0400

    Tweak doc
    Signed-off-by: litt3 <102969658+litt3@users.noreply.github.com>

--- core/payments/ondemand/cumulative_payment_store.go
+++ core/payments/ondemand/cumulative_payment_store.go
@@ -5,7 +5,7 @@ import (
 	"math/big"
 )
 
-// Knows how to store and retrieve a cumulative payment value, stored as a *big.Int representing a number of wei
+// Knows how to get and set a cumulative payment value, stored as a *big.Int representing a number of wei
 type CumulativePaymentStore interface {
 	// Gets the stored cumulative payment in wei
 	GetCumulativePayment(ctx context.Context) (*big.Int, error)
