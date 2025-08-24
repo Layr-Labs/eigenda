0502266cc289811b838e56fa297b59da663687b5
Alvarez <140459501+prestoalvarez@users.noreply.github.com>
2025-08-24 13:58:14 +0200

    Update cli.go

--- api/proxy/store/generated_key/eigenda/verify/cli.go
+++ api/proxy/store/generated_key/eigenda/verify/cli.go
@@ -49,7 +49,7 @@ func VerifierCLIFlags(envPrefix, category string) []cli.Flag {
 			Name: EigenDACertVerifierV1FlagName,
 			Usage: `Address of EigenDACertVerifierV1 contract. Only necessary if using custom quorums/thresholds 
 					for certificate verification. If no address is provided then the default 
-					EigenDAServiceManager parameters will be uesd.`,
+					EigenDAServiceManager parameters will be used.`,
 			EnvVars:  []string{withEnvPrefix(envPrefix, "CERT_VERIFIER_V1")},
 			Category: category,
 		},
