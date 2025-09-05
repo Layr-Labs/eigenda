d8090af76ed69920983bb3781399a91d84d20d10
Samuel Laferriere <9342524+samlaf@users.noreply.github.com>
2025-09-05 16:29:46 -0400

    fix(cert-verifier): add try/catch to never revert and classify all checkDACert calls as valid/invalid/bug (#1938)
    * refactor: cert verifier lib reverts, certVerifier try/catches
    
    This is meant to keep the library cleaner, and using modern custom error practices. We only need the outside CertVerifier to not revert, which we achieve with a try/catch.
    
    * try/catch abi.decode
    
    by moving it to CertVerifier and making it external
    
    * docs: document EigenDACertVerifier contract
    
    * bump CertVerifier semver
    
    * certVerifier constructor reverts on InvalidQuorumNumbersRequired
    
    * certVerifierLib: robustify checkSecurityParams
    
    * simplify and document quorum subsets
    
    * document external block.number > RBN requirement
    
    * feat: add bug status code instead of rethrowing
    
    After discussing with ethen realized this will have better behavior as it will make it impossible for a malicious batch poster to stall the pipeline.
    
    We can also match BUG status codes to failover signals on the batcher write path.
    
    * remove unneeded comment
    
    * test: fix unit test
    
    * style: make fmt
    
    * regenerate bindings
    
    * add comment to get rid of UNUSED statuscodes when bump to 4.0.0
    
    * move contract docs to checkDACert
    
    * easier to understand INVALID_CERT comment
    
    * fix offchain to understand new status codes
    
    * fix inabox certverification error checks
    
    * rename BUG -> INTERNAL_ERROR
    
    * rename decodeCert -> _decodeCert
    
    Also mention that it isnt meant to be part of the public API.
    
    * better explanation for OOG error TODO
    
    * style: fix line length lint errors
    
    * gen bindings

--- contracts/src/integrations/cert/EigenDACertVerifier.sol
+++ contracts/src/integrations/cert/EigenDACertVerifier.sol
@@ -16,7 +16,10 @@ import {EigenDATypesV2 as DATypesV2} from "src/core/libraries/v2/EigenDATypesV2.
 import {IEigenDASemVer} from "src/core/interfaces/IEigenDASemVer.sol";
 
 import {EigenDACertVerificationLib as CertLib} from "src/integrations/cert/libraries/EigenDACertVerificationLib.sol";
+import {EigenDACertTypes as CT} from "src/integrations/cert/EigenDACertTypes.sol";
 
+/// @title EigenDACertVerifier
+/// @notice Verifies EigenDA certificates
 contract EigenDACertVerifier is
     IEigenDACertVerifier,
     IEigenDACertVerifierBase,
@@ -24,6 +27,7 @@ contract EigenDACertVerifier is
     IEigenDASemVer
 {
     error InvalidSecurityThresholds();
+    error InvalidQuorumNumbersRequired(uint256 length);
 
     IEigenDAThresholdRegistry internal immutable _eigenDAThresholdRegistry;
 
@@ -34,8 +38,26 @@ contract EigenDACertVerifier is
     bytes internal _quorumNumbersRequired;
 
     uint8 internal constant MAJOR_VERSION = 3;
-    uint8 internal constant MINOR_VERSION = 0;
+    uint8 internal constant MINOR_VERSION = 1;
     uint8 internal constant PATCH_VERSION = 0;
+
+    /// @notice Status codes for certificate verification results
+    /// @dev checkDACert calls are classified into: success (200), invalid_cert (400), and internal_error (500).
+    enum StatusCode {
+        NULL_ERROR, // Unused error code. If this is returned, there is a bug in the code.
+        SUCCESS, // 200: Verification succeeded
+        // The below 4 status codes are kept for backwards compatibility, but are no longer used.
+        // We previously had plans to have more granular error codes, but decided this was not necessary,
+        // and the only signal useful to offchain is to separate certs into: success, invalid (400), and bugs (500).
+        // TODO(4.0.0): get rid of these
+        UNUSED_HISTORICAL_INVALID_INCLUSION_PROOF,
+        UNUSED_HISTORICAL_SECURITY_ASSUMPTIONS_NOT_MET,
+        UNUSED_HISTORICAL_BLOB_QUORUMS_NOT_SUBSET,
+        UNUSED_HISTORICAL_REQUIRED_QUORUMS_NOT_SUBSET,
+        INVALID_CERT, // 400: Certificate is invalid due to some revert from the verification library
+        INTERNAL_ERROR // 500: Bug or misconfiguration in the CertVerifier contract itself. This includes solidity panics and evm reverts.
+
+    }
 
     constructor(
         IEigenDAThresholdRegistry initEigenDAThresholdRegistry,
@@ -46,22 +68,81 @@ contract EigenDACertVerifier is
         if (initSecurityThresholds.confirmationThreshold <= initSecurityThresholds.adversaryThreshold) {
             revert InvalidSecurityThresholds();
         }
+        if (initQuorumNumbersRequired.length == 0 || initQuorumNumbersRequired.length > 256) {
+            revert InvalidQuorumNumbersRequired(initQuorumNumbersRequired.length);
+        }
         _eigenDAThresholdRegistry = initEigenDAThresholdRegistry;
         _eigenDASignatureVerifier = initEigenDASignatureVerifier;
         _securityThresholds = initSecurityThresholds;
         _quorumNumbersRequired = initQuorumNumbersRequired;
     }
 
+    /// @notice Decodes a certificate from bytes to an EigenDACertV3
+    /// @dev This function is external for the purpose of try/catch'ing it inside checkDACert,
+    /// and should be considered an implementation detail. Do not rely on this function being
+    /// part of the public interface of this contract.
+    function _decodeCert(bytes calldata data) external pure returns (CT.EigenDACertV3 memory cert) {
+        return abi.decode(data, (CT.EigenDACertV3));
+    }
+
     /// @inheritdoc IEigenDACertVerifierBase
+    /// @dev checkDACert is designed to be zk provable by risczero's Steel library,
+    /// which does not support zk proving reverting calls: https://github.com/risc0/risc0-ethereum/issues/438.
+    /// It try catches checkDACertReverts, and maps any reverts to status codes.
+    /// This means invalid certs can easily be proven so by looking at the status code returned,
+    /// which is also useful for optimistic rollup one step prover contracts.
+    /// @dev Make sure to call this at a block number that is > RBN, otherwise this function will
+    /// return an INVALID_CERT status code because of a require in the BLSSignatureChecker library that we use.
+    /// TODO(4.0.0): return (uint8, bytes) instead to include the revert reason.
     function checkDACert(bytes calldata abiEncodedCert) external view returns (uint8) {
-        (CertLib.StatusCode status,) = CertLib.checkDACert(
-            _eigenDAThresholdRegistry,
-            _eigenDASignatureVerifier,
-            abiEncodedCert,
-            _securityThresholds,
-            _quorumNumbersRequired
+        CT.EigenDACertV3 memory daCert;
+        // We try catch this here because decoding error would appear as a Panic,
+        // which we consider bugs in the try/catch for the checkDACertReverts call below.
+        try this._decodeCert(abiEncodedCert) returns (CT.EigenDACertV3 memory _daCert) {
+            daCert = _daCert;
+        } catch {
+            return uint8(StatusCode.INVALID_CERT);
+        }
+
+        // The try catch below is used to filter certs into 3 status codes:
+        // 1. success
+        // 2. invalid cert (any failing require statement; we assume all require statements return either a string or custom error)
+        // 3. internal error (everything else, including solidity panics and low-level evm reverts, basically anything unexpected)
+        // TODO(samlaf): certVerifier should be set with a maxGas param that will be passed here, to enforce deterministic behavior
+        // between different execution environments: EVM running onchain during optimistic rollup fraud proofs, zkVM, eth-call with higher gas limit.
+        try this.checkDACertReverts(daCert) {
+            return uint8(StatusCode.SUCCESS);
+        } catch Error(string memory) /*reason*/ {
+            // This matches any require(..., "string reason") revert that is pre custom errors,
+            // which many of our current eigenlayer-middleware dependencies like the BLSSignatureChecker still use. See:
+            // https://github.com/Layr-Labs/eigenlayer-middleware/blob/fe5834371caed60c1d26ab62b5519b0cbdcb42fa/src/BLSSignatureChecker.sol#L96
+            return uint8(StatusCode.INVALID_CERT);
+        } catch Panic(uint256) /*errorCode*/ {
+            // This matches any panic (e.g. arithmetic overflow, division by zero, invalid array access, etc.),
+            // which means a bug or misconfiguration of the CertVerifier contract itself.
+            return uint8(StatusCode.INTERNAL_ERROR);
+        } catch (bytes memory reason) {
+            if (reason.length == 0) {
+                // This matches low-level evm reverts like out-of-gas or stack too few values.
+                // See https://rareskills.io/post/try-catch-solidity for more info.
+                return uint8(StatusCode.INTERNAL_ERROR);
+            } else if (reason.length < 4) {
+                // Don't think this is possible...
+                return uint8(StatusCode.INTERNAL_ERROR);
+            }
+            // Any revert here is from custom errors coming from a failed require(..., SomeCustomError()) statement.
+            // This mean that the cert is invalid.
+            return uint8(StatusCode.INVALID_CERT);
+        }
+    }
+
+    /// @notice Check a DA cert's validity
+    /// @param daCert The EigenDA certificate
+    /// @dev This function will revert if the certificate is invalid.
+    function checkDACertReverts(CT.EigenDACertV3 calldata daCert) external view {
+        CertLib.checkDACert(
+            _eigenDAThresholdRegistry, _eigenDASignatureVerifier, daCert, _securityThresholds, _quorumNumbersRequired
         );
-        return uint8(status);
     }
 
     /// @inheritdoc IEigenDACertVerifier
