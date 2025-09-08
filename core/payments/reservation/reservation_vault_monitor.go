f9c9e7691987802e7102817d5661571891fe91c2
litt3 <102969658+litt3@users.noreply.github.com>
2025-09-08 16:21:40 -0400

    Add TODOs
    Signed-off-by: litt3 <102969658+litt3@users.noreply.github.com>

--- core/payments/reservation/reservation_vault_monitor.go
+++ core/payments/reservation/reservation_vault_monitor.go
@@ -50,6 +50,9 @@ func NewReservationVaultMonitor(
 }
 
 // Fetches the latest state from the PaymentVault, and updates the ledgers with it
+//
+// TODO(litt3): If the number of accounts returned by getAccountsToUpdate ever gets very large, a potential
+// optimization would be to create batches of accounts, and use multiple RPC calls to fetch the reservations.
 func (vm *ReservationVaultMonitor) refreshReservations(ctx context.Context) error {
 	accountIDs := vm.getAccountsToUpdate()
 	if len(accountIDs) == 0 {
