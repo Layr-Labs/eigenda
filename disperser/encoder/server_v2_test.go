55c609f3a9bdb99e20a2d96de8bfb20ba194375e
Samuel Laferriere <9342524+samlaf@users.noreply.github.com>
2025-09-17 17:57:19 -0400

    chore: make all v2 services use v2 prover

--- disperser/encoder/server_v2_test.go
+++ disperser/encoder/server_v2_test.go
@@ -17,7 +17,7 @@ import (
 	"github.com/Layr-Labs/eigenda/disperser/encoder"
 	"github.com/Layr-Labs/eigenda/encoding"
 	"github.com/Layr-Labs/eigenda/encoding/kzg"
-	"github.com/Layr-Labs/eigenda/encoding/kzg/prover"
+	"github.com/Layr-Labs/eigenda/encoding/kzg/prover/v2"
 	"github.com/Layr-Labs/eigenda/encoding/utils/codec"
 	"github.com/Layr-Labs/eigenda/relay/chunkstore"
 	gethcommon "github.com/ethereum/go-ethereum/common"
