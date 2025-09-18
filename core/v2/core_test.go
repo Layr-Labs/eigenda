7cf85bd2c90d729b4c57f106166a770c59df851c
Samuel Laferriere <9342524+samlaf@users.noreply.github.com>
2025-09-18 13:01:19 -0400

    test: fix test harnesses missing g2.trailing.path

--- core/v2/core_test.go
+++ core/v2/core_test.go
@@ -74,6 +74,7 @@ func makeTestComponents() (*prover.Prover, *verifier.Verifier, error) {
 	proverConfig := &prover.KzgConfig{
 		G1Path:          "../../resources/srs/g1.point",
 		G2Path:          "../../resources/srs/g2.point",
+		G2TrailingPath:  "../../resources/srs/g2.trailing.point",
 		CacheDir:        "../../resources/srs/SRSTables",
 		SRSNumberToLoad: 8192,
 		NumWorker:       uint64(runtime.GOMAXPROCS(0)),
--- retriever/v2/server_test.go
+++ retriever/v2/server_test.go
@@ -36,6 +36,7 @@ func makeTestComponents() (*prover.Prover, *verifier.Verifier, error) {
 	config := &kzg.KzgConfig{
 		G1Path:          "../../resources/srs/g1.point",
 		G2Path:          "../../resources/srs/g2.point",
+		G2TrailingPath:  "../../resources/srs/g2.trailing.point",
 		CacheDir:        "../../resources/srs/SRSTables",
 		SRSOrder:        3000,
 		SRSNumberToLoad: 3000,
