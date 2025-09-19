74014d17608de4f63e29f5e55ae31ddacb0215e7
Cody Littley <cody@eigenlabs.org>
2025-09-19 10:36:46 -0500

    lint

--- disperser/common/v2/blobstore/s3_blob_store.go
+++ disperser/common/v2/blobstore/s3_blob_store.go
@@ -49,7 +49,7 @@ func (b *BlobStore) GetBlob(ctx context.Context, key corev2.BlobKey) ([]byte, er
 	}
 
 	if err != nil {
-		return nil, fmt.Errorf("%s %s: %w", ErrBlobNotFound, b.bucketName, err)
+		return nil, fmt.Errorf("%s, bucket: %s,: %w", ErrBlobNotFound.Error(), b.bucketName, err)
 	}
 	return data, nil
 }
