2a76309767a67789a88b92c4f57cb3a21c2cf27d
Daniel Mancia <21249320+dmanc@users.noreply.github.com>
2025-09-19 16:31:38 -0700

    increase timeout

--- inabox/tests/integration_suite_test.go
+++ inabox/tests/integration_suite_test.go
@@ -115,7 +115,7 @@ func setupSuite() {
 	logger.Info("bootstrapping test environment")
 
 	// Create a context with 1 minute timeout for setup
-	ctx, cancelSetup := context.WithTimeout(context.Background(), 1*time.Minute)
+	ctx, cancelSetup := context.WithTimeout(context.Background(), 5*time.Minute)
 	defer cancelSetup()
 
 	// Channel to track setup completion
@@ -506,7 +506,7 @@ func teardownSuite() {
 	logger.Info("Tearing down test environment")
 
 	// Create a context with 1 minute timeout for teardown
-	ctx, cancelTeardown := context.WithTimeout(context.Background(), 1*time.Minute)
+	ctx, cancelTeardown := context.WithTimeout(context.Background(), 5*time.Minute)
 	defer cancelTeardown()
 
 	// Channel to track teardown completion
