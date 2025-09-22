2cf127f2811e5f2a57cf877df174ae0e9736e423
Samuel Laferriere <9342524+samlaf@users.noreply.github.com>
2025-09-22 18:49:20 -0400

    test: better TestLengthProof function
    Now we test with all powers of 2 blob lengths, instead of some random numbers.

--- encoding/kzg/verifier/v2/length_test.go
+++ encoding/kzg/verifier/v2/length_test.go
@@ -1,18 +1,23 @@
 package verifier_test
 
 import (
+	"fmt"
+	"strconv"
 	"testing"
 
 	"github.com/Layr-Labs/eigenda/encoding"
 	"github.com/Layr-Labs/eigenda/encoding/kzg/prover/v2"
 	"github.com/Layr-Labs/eigenda/encoding/kzg/verifier/v2"
 	"github.com/Layr-Labs/eigenda/encoding/rs"
-	"github.com/stretchr/testify/assert"
+	"github.com/Layr-Labs/eigenda/test/random"
 	"github.com/stretchr/testify/require"
 )
 
 func TestLengthProof(t *testing.T) {
 	harness := getTestHarness()
+	testRand := random.NewTestRandom(134)
+	maxNumSymbols := uint64(1 << 19) // our stored G1 and G2 files only contain this many pts
+	harness.proverV2KzgConfig.SRSNumberToLoad = maxNumSymbols
 
 	group, err := prover.NewProver(harness.proverV2KzgConfig, nil)
 	require.Nil(t, err)
@@ -20,26 +25,32 @@ func TestLengthProof(t *testing.T) {
 	v, err := verifier.NewVerifier(harness.verifierV2KzgConfig, nil)
 	require.Nil(t, err)
 
-	params := encoding.ParamsFromSysPar(harness.numSys, harness.numPar, uint64(len(harness.paddedGettysburgAddressBytes)))
+	// We use numsys=1 and numpar=0 to mean that we don't erasure code, so the data stays the same size.
+	params := encoding.ParamsFromSysPar(1, 0, maxNumSymbols*encoding.BYTES_PER_SYMBOL)
 	enc, err := group.GetKzgEncoder(params)
 	require.Nil(t, err)
 
-	numBlob := 5
-	for z := 0; z < numBlob; z++ {
-		extra := make([]byte, z*32*2)
-		inputBytes := append(harness.paddedGettysburgAddressBytes, extra...)
-		inputFr, err := rs.ToFrArray(inputBytes)
-		require.Nil(t, err)
+	// for numSymbols := uint64(1); numSymbols <= 1; numSymbols *= 2 {
+	for numSymbols := uint64(1); numSymbols < maxNumSymbols; numSymbols *= 2 {
+		t.Run("numSymbols="+strconv.Itoa(int(numSymbols)), func(t *testing.T) {
+			inputBytes := testRand.Bytes(int(numSymbols) * encoding.BYTES_PER_SYMBOL)
+			for i := range numSymbols {
+				inputBytes[i*encoding.BYTES_PER_SYMBOL] = 0
+			}
+			inputFr, err := rs.ToFrArray(inputBytes)
+			require.Nil(t, err)
+			require.Equal(t, uint64(len(inputFr)), numSymbols)
 
-		_, lengthCommitment, lengthProof, _, _, err := enc.Encode(inputFr)
-		require.Nil(t, err)
+			_, lengthCommitment, lengthProof, _, _, err := enc.Encode(inputFr)
+			require.Nil(t, err)
 
-		blobLen := encoding.NextPowerOf2(len(inputFr))
-		assert.NoError(t, v.VerifyLengthProof(lengthCommitment, lengthProof, uint64(blobLen)),
-			"low degree verification failed\n")
+			fmt.Println("lengthCommitment", lengthCommitment, "lengthProof", lengthProof)
 
-		blobLen = len(inputFr) - 10
-		assert.Error(t, v.VerifyLengthProof(lengthCommitment, lengthProof, uint64(blobLen)),
-			"low degree verification failed\n")
+			require.NoError(t, v.VerifyLengthProof(lengthCommitment, lengthProof, numSymbols),
+				"low degree verification failed\n")
+
+			require.Error(t, v.VerifyLengthProof(lengthCommitment, lengthProof, numSymbols*2),
+				"low degree verification failed\n")
+		})
 	}
 }
