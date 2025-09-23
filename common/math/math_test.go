5bd575c46eb8e35825cfcd247d01020d7f6834b0
Samuel Laferriere <9342524+samlaf@users.noreply.github.com>
2025-09-22 22:22:56 -0400

    move encoding/test_utils to common/math

--- common/math/math_test.go
+++ common/math/math_test.go
@@ -4,7 +4,8 @@ import (
 	gomath "math"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/exp/constraints"
 )
 
 func TestIsPowerOfTwo(t *testing.T) {
@@ -23,6 +24,42 @@ func TestIsPowerOfTwo(t *testing.T) {
 			expectedResult = logBase2 == truncatedLogBase2
 		}
 
-		assert.Equal(t, expectedResult, result, "IsPowerOfTwo(%d) returned unexpected result '%t'.", i, result)
+		require.Equal(t, expectedResult, result, "IsPowerOfTwo(%d) returned unexpected result '%t'.", i, result)
 	}
+}
+
+func TestNextPowerOf2(t *testing.T) {
+	testHeight := uint64(65536)
+
+	// 2 ^ 16 = 65536
+	// i.e., the last element generated here == testHeight
+	powers := generatePowersOfTwo(uint64(17))
+
+	powerIndex := 0
+	for i := uint64(1); i <= testHeight; i++ {
+		nextPowerOf2 := NextPowOf2u64(i)
+		require.Equal(t, nextPowerOf2, powers[powerIndex])
+
+		if i == powers[powerIndex] {
+			powerIndex++
+		}
+	}
+
+	// sanity check the test logic
+	require.Equal(t, powerIndex, len(powers))
+
+	// extra sanity check, since we *really* rely on NextPowerOf2 returning
+	// the same value, if it's already a power of 2
+	require.Equal(t, uint64(16), NextPowOf2u64(16))
+}
+
+// GeneratePowersOfTwo creates a slice of integers, containing powers of 2 (starting with element == 1), with
+// powersToGenerate number of elements
+func generatePowersOfTwo[T constraints.Integer](powersToGenerate T) []T {
+	powers := make([]T, powersToGenerate)
+	for i := T(0); i < powersToGenerate; i++ {
+		powers[i] = 1 << i
+	}
+
+	return powers
 }
