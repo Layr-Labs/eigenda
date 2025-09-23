20f6753830a7d5deb341a316e2827ce2b60ef152
Samuel Laferriere <9342524+samlaf@users.noreply.github.com>
2025-09-23 15:38:13 -0400

    chore: move prover_fuzz_test test to prover_test

--- encoding/kzg/prover/v2/prover_test.go
+++ encoding/kzg/prover/v2/prover_test.go
@@ -101,3 +101,39 @@ func BenchmarkEncode(b *testing.B) {
 		_, _, _ = p.EncodeAndProve(blobs[i%numSamples], params)
 	}
 }
+
+func FuzzOnlySystematic(f *testing.F) {
+	harness := getTestHarness()
+
+	f.Add(harness.paddedGettysburgAddressBytes)
+	f.Fuzz(func(t *testing.T, input []byte) {
+		group, err := prover.NewProver(harness.proverV2KzgConfig, nil)
+		require.NoError(t, err)
+
+		params := encoding.ParamsFromSysPar(10, 3, uint64(len(input)))
+		enc, err := group.GetKzgEncoder(params)
+		if err != nil {
+			t.Errorf("Error making rs: %q", err)
+		}
+
+		//encode the data
+		_, _, _, frames, _, err := enc.EncodeBytes(input)
+
+		for _, frame := range frames {
+			assert.NotEqual(t, len(frame.Coeffs), 0)
+		}
+
+		if err != nil {
+			t.Errorf("Error Encoding:\n Data:\n %q \n Err: %q", input, err)
+		}
+
+		//sample the correct systematic frames
+		samples, indices := sampleFrames(frames, uint64(len(frames)))
+
+		data, err := enc.Decode(samples, indices, uint64(len(input)))
+		if err != nil {
+			t.Errorf("Error Decoding:\n Data:\n %q \n Err: %q", input, err)
+		}
+		assert.Equal(t, input, data, "Input data was not equal to the decoded data")
+	})
+}
