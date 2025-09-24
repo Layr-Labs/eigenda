f832f9fbc0ba8c80fee4ec59d2dd67bd38cb8cc0
litt3 <102969658+litt3@users.noreply.github.com>
2025-09-24 13:49:01 -0400

    Try enabling controller payments
    Signed-off-by: litt3 <102969658+litt3@users.noreply.github.com>

--- inabox/deploy/config.go
+++ inabox/deploy/config.go
@@ -216,11 +216,13 @@ func (env *Config) generateDisperserV2Vars(ind int, logPath, dbPath, grpcPort st
 		DISPERSER_SERVER_EIGENDA_SERVICE_MANAGER:     env.EigenDA.ServiceManager,
 		DISPERSER_SERVER_DISPERSER_VERSION:           "2",
 
-		DISPERSER_SERVER_ENABLE_PAYMENT_METERER:  "true",
-		DISPERSER_SERVER_RESERVED_ONLY:           "false",
-		DISPERSER_SERVER_RESERVATIONS_TABLE_NAME: "e2e_v2_reservation",
-		DISPERSER_SERVER_ON_DEMAND_TABLE_NAME:    "e2e_v2_ondemand",
-		DISPERSER_SERVER_GLOBAL_RATE_TABLE_NAME:  "e2e_v2_global_reservation",
+		DISPERSER_SERVER_ENABLE_PAYMENT_METERER:           "true",
+		DISPERSER_SERVER_RESERVED_ONLY:                    "false",
+		DISPERSER_SERVER_RESERVATIONS_TABLE_NAME:          "e2e_v2_reservation",
+		DISPERSER_SERVER_ON_DEMAND_TABLE_NAME:             "e2e_v2_ondemand",
+		DISPERSER_SERVER_GLOBAL_RATE_TABLE_NAME:           "e2e_v2_global_reservation",
+		DISPERSER_SERVER_USE_CONTROLLER_MEDIATED_PAYMENTS: "true",
+		DISPERSER_SERVER_CONTROLLER_ADDRESS:               "localhost:30000", // Controller service address for new payment system
 
 		// V2 inabox test is setup with a client that doesn't setup a client for some reason,
 		// so it calls the grpc GetBlobCommitment to generate commitments.
