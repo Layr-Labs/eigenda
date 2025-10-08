861a46ff95a88308053c3bfb94c2a2bf41264a5c
Cody Littley <cody@eigenlabs.org>
2025-10-08 08:56:58 -0500

    Create bootstrap CLI parser

--- ejector/main/main.go
+++ ejector/main/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/Layr-Labs/eigenda/common/config"
+	"github.com/Layr-Labs/eigenda/ejector"
+)
+
+const ejectorEnvVarPrefix = "EJECTOR"
+
 func main() {
+
+	cfg, err := config.Bootstrap(ejector.DefaultEjectorConfig, ejectorEnvVarPrefix)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Ejector configuration: %+v\n", cfg) // TODO
+
+	// ctx := context.Background()
+
+	// TODO initialize
+	// ejector := ejector.NewEjector(
+	// 	ctx context.Context,
+	// 	logger logging.Logger,
+	// 	ejectionManager *ejector.ThreadedEjectionManager,
+	// 	signingRateLookupV1 ejector.SigningRateLookup,
+	// 	signingRateLookupV2 ejector.SigningRateLookup,
+	// 	period time.Duration,
+	// 	ejectionCriteriaTimeWindow time.Duration,
+	// 	validatorIDToAddressCache *eth.ValidatorIDToAddressCache,
+	// )
+
 	// TODO
 }
