9537441c6b1ea633ae1808ea120222a559bac4dc
Cody Littley <cody@eigenlabs.org>
2025-10-09 10:52:56 -0500

    Strip out placeholder code

--- ejector/ejection_transactor.go
+++ ejector/ejection_transactor.go
@@ -2,12 +2,7 @@ package ejector
 
 import (
 	"context"
-	"crypto/ecdsa"
-	"fmt"
-	"math/big"
 
-	contractEigenDAEjectionManager "github.com/Layr-Labs/eigenda/contracts/bindings/IEigenDAEjectionManager"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	gethcommon "github.com/ethereum/go-ethereum/common"
 )
 
@@ -26,91 +21,4 @@ type EjectionTransactor interface {
 
 	// Complete the ejection proceedings against the operator with the given address.
 	CompleteEjection(ctx context.Context, addressToEject gethcommon.Address) error
-}
-
-var _ EjectionTransactor = &ejectionTransactor{}
-
-// ejectionTransactor is the production implementation of the EjectionTransactor interface.
-type ejectionTransactor struct {
-	// Used to execute eth reads
-	caller *contractEigenDAEjectionManager.ContractIEigenDAEjectionManagerCaller
-	// Used to execute eth writes
-	transactor *contractEigenDAEjectionManager.ContractIEigenDAEjectionManagerTransactor
-	// A function that can sign transactions from selfAddress.
-	signer bind.SignerFn
-}
-
-// Create a new EjectionTransactor.
-func NewEjectionTransactor(
-	ctx context.Context,
-	client bind.ContractBackend,
-	ejectionContractAddress gethcommon.Address,
-	selfAddress gethcommon.Address,
-	privateKey *ecdsa.PrivateKey,
-	chainID *big.Int,
-) (EjectionTransactor, error) {
-
-	var zeroAddress gethcommon.Address
-	if selfAddress == zeroAddress {
-		return nil, fmt.Errorf("selfAddress must be non-zero")
-	}
-	if privateKey == nil {
-		return nil, fmt.Errorf("privateKey must be non-nil")
-	}
-
-	caller, err := contractEigenDAEjectionManager.NewContractIEigenDAEjectionManagerCaller(
-		ejectionContractAddress, client)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create ejection manager caller: %w", err)
-	}
-
-	transactor, err := contractEigenDAEjectionManager.NewContractIEigenDAEjectionManagerTransactor(
-		ejectionContractAddress, client)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create ejection manager transactor: %w", err)
-	}
-
-	transactOpts, err := bind.NewKeyedTransactorWithChainID(privateKey, chainID)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create transact opts: %w", err)
-	}
-
-	signer := transactOpts.Signer
-
-	return &ejectionTransactor{
-		caller:     caller,
-		transactor: transactor,
-		signer:     signer,
-	}, nil
-}
-
-// CompleteEjection implements EjectionTransactor.
-func (e *ejectionTransactor) CompleteEjection(
-	ctx context.Context,
-	addressToEject gethcommon.Address,
-) error {
-	panic("unimplemented")
-}
-
-// IsEjectionInProgress implements EjectionTransactor.
-func (e *ejectionTransactor) IsEjectionInProgress(
-	ctx context.Context,
-	addressToCheck gethcommon.Address,
-) (bool, error) {
-	panic("unimplemented")
-}
-
-// IsValidatorPresentInAnyQuorum implements EjectionTransactor.
-func (e *ejectionTransactor) IsValidatorPresentInAnyQuorum(
-	ctx context.Context,
-	addressToCheck gethcommon.Address,
-) (bool, error) {
-	panic("unimplemented")
-}
-
-// StartEjection implements EjectionTransactor.
-func (e *ejectionTransactor) StartEjection(
-	ctx context.Context,
-	addressToEject gethcommon.Address) error {
-	panic("unimplemented")
 }
