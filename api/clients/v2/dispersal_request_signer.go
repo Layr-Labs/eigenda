2c87682f262dda7ff6e5c5393761196d01e0d17b
Daniel Mancia <21249320+dmanc@users.noreply.github.com>
2025-10-10 12:06:46 -0700

    Add back verify to constructor

--- api/clients/v2/dispersal_request_signer.go
+++ api/clients/v2/dispersal_request_signer.go
@@ -66,6 +66,10 @@ func NewDispersalRequestSigner(
 	ctx context.Context,
 	config DispersalRequestSignerConfig,
 ) (DispersalRequestSigner, error) {
+	if err := config.Verify(); err != nil {
+		return nil, fmt.Errorf("invalid config: %w", err)
+	}
+
 	var kmsClient *kms.Client
 	if config.Endpoint != "" {
 		kmsClient = kms.New(kms.Options{
