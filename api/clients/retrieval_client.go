c77e46ee95422c21c96f576f410f3ae0653251f7
Luke Williams <iquidus@users.noreply.github.com>
2025-10-15 16:17:28 -0400

    chore(refactor): minor refactor inside `RetrieveBlobChunks` func

--- api/clients/retrieval_client.go
+++ api/clients/retrieval_client.go
@@ -197,28 +197,30 @@ func (r *retrievalClient) RetrieveBlobChunks(ctx context.Context,
 	for i := 0; i < len(operators); i++ {
 		select {
 		case <-ctx.Done():
-			return nil, fmt.Errorf("context cancelled whilst retrieving chuncks: %w", ctx.Err())
-		case reply := <-chunksChan:
-			if reply.Err != nil {
-				r.logger.Warn("failed to get chunks from operator", "operator", reply.OperatorID.Hex(), "err", reply.Err)
-				continue
-			}
-			assignment, ok := assignments[reply.OperatorID]
-			if !ok {
-				return nil, fmt.Errorf("no assignment to operator %s", reply.OperatorID.Hex())
-			}
-
-			err = r.verifier.VerifyFrames(reply.Chunks, assignment.GetIndices(), blobHeader.BlobCommitments, encodingParams)
-			if err != nil {
-				r.logger.Warn("failed to verify chunks from operator", "operator", reply.OperatorID.Hex(), "err", err)
-				continue
-			} else {
-				r.logger.Info("verified chunks from operator", "operator", reply.OperatorID.Hex())
-			}
-
-			chunks = append(chunks, reply.Chunks...)
-			indices = append(indices, assignment.GetIndices()...)
+			return nil, ctx.Err()
+		default:
 		}
+
+		reply := <-chunksChan
+		if reply.Err != nil {
+			r.logger.Warn("failed to get chunks from operator", "operator", reply.OperatorID.Hex(), "err", reply.Err)
+			continue
+		}
+		assignment, ok := assignments[reply.OperatorID]
+		if !ok {
+			return nil, fmt.Errorf("no assignment to operator %s", reply.OperatorID.Hex())
+		}
+
+		err = r.verifier.VerifyFrames(reply.Chunks, assignment.GetIndices(), blobHeader.BlobCommitments, encodingParams)
+		if err != nil {
+			r.logger.Warn("failed to verify chunks from operator", "operator", reply.OperatorID.Hex(), "err", err)
+			continue
+		} else {
+			r.logger.Info("verified chunks from operator", "operator", reply.OperatorID.Hex())
+		}
+
+		chunks = append(chunks, reply.Chunks...)
+		indices = append(indices, assignment.GetIndices()...)
 	}
 
 	return &BlobChunks{
