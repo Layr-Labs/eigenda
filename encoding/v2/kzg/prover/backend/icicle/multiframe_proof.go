Layr-Labs/eigenda

commit 68ab9349e46aabe9f648ee54fde6dfb9ee1bd6f9

refactor(prover-v2): cleanup gnark/icicle backend code (#2197)
* refactor(prover-v2): cleanup gnark/icicle backend code

The dependency, constructor, and dependency flow for building these backends was extremely convoluted and hard to follow. I created a simple kzg/prover/backend package which has one interface and 2 implementations (gnark and icicle), following ben-johnson style. It makes the code all self-contained and a lot easier to reason about imo.

* fix icicle bug

encoding/v2/kzg/prover/backend/icicle/multiframe_proof.go
@@ -13,12 +13,23 @@ import (
 	"github.com/Layr-Labs/eigensdk-go/logging"
 	"github.com/consensys/gnark-crypto/ecc/bn254"
 	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
+
 	"github.com/ingonyama-zk/icicle/v3/wrappers/golang/core"
 	iciclebn254 "github.com/ingonyama-zk/icicle/v3/wrappers/golang/curves/bn254"
+	"github.com/ingonyama-zk/icicle/v3/wrappers/golang/curves/bn254/ecntt"
+	"github.com/ingonyama-zk/icicle/v3/wrappers/golang/curves/bn254/msm"
 	"github.com/ingonyama-zk/icicle/v3/wrappers/golang/runtime"
 )
 
-type KzgMultiProofIcicleBackend struct {
+const (
+	// MAX_NTT_SIZE is the maximum NTT domain size needed to compute FFTs for the
+	// largest supported blobs. Assuming a coding ratio of 1/8 and symbol size of 32 bytes:
+	// - Encoded size: 2^{MAX_NTT_SIZE} * 32 bytes ≈ 1 GB
+	// - Original blob size: 2^{MAX_NTT_SIZE} * 32 / 8 = 2^{MAX_NTT_SIZE + 2} ≈ 128 MB
+	MAX_NTT_SIZE = 25
+)
+
+type KzgMultiProofBackend struct {
 	Logger         logging.Logger
 	Fs             *fft.FFTSettings
 	FlatFFTPointsT []iciclebn254.Affine
@@ -29,13 +40,40 @@ type KzgMultiProofIcicleBackend struct {
 	NumWorker      uint64
 }
 
+func NewMultiProofBackend(logger logging.Logger,
+	fs *fft.FFTSettings, fftPointsT [][]bn254.G1Affine,
+	gpuEnabled bool, numWorker uint64,
+) (*KzgMultiProofBackend, error) {
+	icicleDevice, err := icicle.NewIcicleDevice(icicle.IcicleDeviceConfig{
+		Logger:     logger,
+		GPUEnable:  gpuEnabled,
+		NTTSize:    MAX_NTT_SIZE,
+		FFTPointsT: fftPointsT,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("configure icicle device: %w", err)
+	}
+
+	// Set up icicle multiproof backend
+	return &KzgMultiProofBackend{
+		Logger:         logger,
+		Fs:             fs,
+		FlatFFTPointsT: icicleDevice.FlatFFTPointsT,
+		NttCfg:         icicleDevice.NttCfg,
+		MsmCfg:         icicleDevice.MsmCfg,
+		Device:         icicleDevice.Device,
+		GpuLock:        sync.Mutex{},
+		NumWorker:      numWorker,
+	}, nil
+}
+
 type WorkerResult struct {
 	err error
 }
 
 // This function supports batching over multiple blobs.
 // All blobs must have same size and concatenated passed as polyFr
-func (p *KzgMultiProofIcicleBackend) ComputeMultiFrameProofV2(polyFr []fr.Element, numChunks, chunkLen, numWorker uint64) ([]bn254.G1Affine, error) {
+func (p *KzgMultiProofBackend) ComputeMultiFrameProofV2(polyFr []fr.Element, numChunks, chunkLen, numWorker uint64) ([]bn254.G1Affine, error) {
 	begin := time.Now()
 
 	dimE := numChunks
@@ -75,7 +113,7 @@ func (p *KzgMultiProofIcicleBackend) ComputeMultiFrameProofV2(polyFr []fr.Elemen
 		var flattenStoreCopyToDevice core.DeviceSlice
 		flattenCoeffStoreCopy.CopyToDevice(&flattenStoreCopyToDevice, true)
 
-		sumVec, err := p.MsmBatchOnDevice(flattenStoreCopyToDevice, p.FlatFFTPointsT, int(numPoly)*int(dimE)*2)
+		sumVec, err := p.msmBatchOnDevice(flattenStoreCopyToDevice, p.FlatFFTPointsT, int(numPoly)*int(dimE)*2)
 		if err != nil {
 			icicleErr = fmt.Errorf("msm error: %w", err)
 			return
@@ -88,7 +126,7 @@ func (p *KzgMultiProofIcicleBackend) ComputeMultiFrameProofV2(polyFr []fr.Elemen
 
 		// Compute the first ecntt, and set new batch size for ntt
 		p.NttCfg.BatchSize = int32(numPoly)
-		sumVecInv, err := p.ECNttOnDevice(sumVec, true, int(dimE)*2*int(numPoly))
+		sumVecInv, err := p.ecnttOnDevice(sumVec, true, int(dimE)*2*int(numPoly))
 		if err != nil {
 			icicleErr = fmt.Errorf("first ECNtt error: %w", err)
 			return
@@ -101,7 +139,7 @@ func (p *KzgMultiProofIcicleBackend) ComputeMultiFrameProofV2(polyFr []fr.Elemen
 		prunedSumVecInv := sumVecInv.Range(0, int(dimE), false)
 
 		// Compute the second ecntt on the reduced size array
-		flatProofsBatch, err := p.ECNttToGnarkOnDevice(prunedSumVecInv, false, int(numPoly)*int(dimE))
+		flatProofsBatch, err := p.ecnttOnDevice(prunedSumVecInv, false, int(numPoly)*int(dimE))
 		if err != nil {
 			icicleErr = fmt.Errorf("second ECNtt error: %w", err)
 			return
@@ -137,7 +175,7 @@ func (p *KzgMultiProofIcicleBackend) ComputeMultiFrameProofV2(polyFr []fr.Elemen
 }
 
 // Modify the function signature to return a flat array
-func (p *KzgMultiProofIcicleBackend) computeCoeffStore(polyFr []fr.Element, numWorker, l, dimE uint64) ([]fr.Element, error) {
+func (p *KzgMultiProofBackend) computeCoeffStore(polyFr []fr.Element, numWorker, l, dimE uint64) ([]fr.Element, error) {
 	totalSize := dimE * 2 * l // Total size of the flattened array
 	coeffStore := make([]fr.Element, totalSize)
 
@@ -171,7 +209,7 @@ func (p *KzgMultiProofIcicleBackend) computeCoeffStore(polyFr []fr.Element, numW
 }
 
 // Modified worker function to write directly to the flat array
-func (p *KzgMultiProofIcicleBackend) proofWorker(
+func (p *KzgMultiProofBackend) proofWorker(
 	polyFr []fr.Element,
 	jobChan <-chan uint64,
 	l uint64,
@@ -210,7 +248,7 @@ func (p *KzgMultiProofIcicleBackend) proofWorker(
 //
 // TODO(samlaf): better document/explain/refactor/rename this function,
 // to explain how it fits into the overall scheme.
-func (p *KzgMultiProofIcicleBackend) getSlicesCoeff(polyFr []fr.Element, dimE, j, l uint64) ([]fr.Element, error) {
+func (p *KzgMultiProofBackend) getSlicesCoeff(polyFr []fr.Element, dimE, j, l uint64) ([]fr.Element, error) {
 	toeplitzExtendedVec := make([]fr.Element, 2*dimE)
 
 	m := uint64(len(polyFr)) - 1 // there is a constant term
@@ -228,4 +266,49 @@ func (p *KzgMultiProofIcicleBackend) getSlicesCoeff(polyFr []fr.Element, dimE, j
 		return nil, fmt.Errorf("fft: %w", err)
 	}
 	return out, nil
+}
+
+// MsmBatchOnDevice function supports batch across blobs.
+// totalSize is the number of output points, which equals to numPoly * 2 * dimE , dimE is number of chunks
+func (c *KzgMultiProofBackend) msmBatchOnDevice(rowsFrIcicleCopy core.DeviceSlice, rowsG1Icicle []iciclebn254.Affine, totalSize int) (core.DeviceSlice, error) {
+	rowsG1IcicleCopy := core.HostSliceFromElements[iciclebn254.Affine](rowsG1Icicle)
+
+	var p iciclebn254.Projective
+	var out core.DeviceSlice
+
+	_, err := out.Malloc(p.Size(), totalSize)
+	if err != runtime.Success {
+		return out, fmt.Errorf("allocating bytes on device failed: %v", err.AsString())
+	}
+
+	err = msm.Msm(rowsFrIcicleCopy, rowsG1IcicleCopy, &c.MsmCfg, out)
+	if err != runtime.Success {
+		return out, fmt.Errorf("msm error: %v", err.AsString())
+	}
+
+	return out, nil
+}
+
+func (c *KzgMultiProofBackend) ecnttOnDevice(batchPoints core.DeviceSlice, isInverse bool, totalSize int) (core.DeviceSlice, error) {
+	var p iciclebn254.Projective
+	var out core.DeviceSlice
+
+	output, err := out.Malloc(p.Size(), totalSize)
+	if err != runtime.Success {
+		return out, fmt.Errorf("allocating bytes on device failed: %v", err.AsString())
+	}
+
+	if isInverse {
+		err := ecntt.ECNtt(batchPoints, core.KInverse, &c.NttCfg, output)
+		if err != runtime.Success {
+			return out, fmt.Errorf("inverse ecntt failed: %v", err.AsString())
+		}
+	} else {
+		err := ecntt.ECNtt(batchPoints, core.KForward, &c.NttCfg, output)
+		if err != runtime.Success {
+			return out, fmt.Errorf("forward ecntt failed: %v", err.AsString())
+		}
+	}
+
+	return output, nil
 }

encoding/v2/kzg/prover/prover.go
@@ -12,7 +12,9 @@ import (
 	"github.com/Layr-Labs/eigenda/encoding"
 	"github.com/Layr-Labs/eigenda/encoding/v2/fft"
 	"github.com/Layr-Labs/eigenda/encoding/v2/kzg"
-	gnarkprover "github.com/Layr-Labs/eigenda/encoding/v2/kzg/prover/gnark"
+	"github.com/Layr-Labs/eigenda/encoding/v2/kzg/prover/backend"
+	"github.com/Layr-Labs/eigenda/encoding/v2/kzg/prover/backend/gnark"
+	"github.com/Layr-Labs/eigenda/encoding/v2/kzg/prover/backend/icicle"
 	"github.com/Layr-Labs/eigenda/encoding/v2/rs"
 	"github.com/Layr-Labs/eigensdk-go/logging"
 	"github.com/consensys/gnark-crypto/ecc/bn254"
@@ -133,39 +135,26 @@ func (p *Prover) newProver(params encoding.EncodingParams) (*ParametrizedProver,
 	}
 	fs := fft.NewFFTSettings(n)
 
-	switch p.Config.BackendType {
-	case encoding.GnarkBackend:
-		return p.createGnarkBackendProver(params, fs)
-	case encoding.IcicleBackend:
-		return p.createIcicleBackendProver(params, fs)
-	default:
-		return nil, fmt.Errorf("unsupported backend type: %v", p.Config.BackendType)
-	}
-
-}
-
-func (p *Prover) createGnarkBackendProver(
-	params encoding.EncodingParams, fs *fft.FFTSettings,
-) (*ParametrizedProver, error) {
-	if p.Config.GPUEnable {
-		return nil, errors.New("GPU is not supported in gnark backend")
-	}
-
 	_, fftPointsT, err := p.setupFFTPoints(params)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("setup fft points: %w", err)
 	}
 
-	// Create subgroup FFT settings
-	t := uint8(gomath.Log2(float64(2 * params.NumChunks)))
-	sfs := fft.NewFFTSettings(t)
-
-	// Set KZG Prover gnark backend
-	multiproofBackend := &gnarkprover.KzgMultiProofGnarkBackend{
-		Logger:     p.logger,
-		Fs:         fs,
-		FFTPointsT: fftPointsT,
-		SFs:        sfs,
+	var multiproofsBackend backend.KzgMultiProofsBackendV2
+	switch p.Config.BackendType {
+	case encoding.GnarkBackend:
+		if p.Config.GPUEnable {
+			return nil, errors.New("GPU is not supported in gnark backend")
+		}
+		multiproofsBackend = gnark.NewMultiProofBackend(p.logger, fs, fftPointsT)
+	case encoding.IcicleBackend:
+		multiproofsBackend, err = icicle.NewMultiProofBackend(
+			p.logger, fs, fftPointsT, p.Config.GPUEnable, p.KzgConfig.NumWorker)
+		if err != nil {
+			return nil, fmt.Errorf("create icicle backend prover: %w", err)
+		}
+	default:
+		return nil, fmt.Errorf("unsupported backend type: %v", p.Config.BackendType)
 	}
 
 	return &ParametrizedProver{
@@ -174,14 +163,9 @@ func (p *Prover) createGnarkBackendProver(
 		encodingParams:             params,
 		encoder:                    p.encoder,
 		computeMultiproofNumWorker: p.KzgConfig.NumWorker,
-		kzgMultiProofBackend:       multiproofBackend,
+		kzgMultiProofBackend:       multiproofsBackend,
 	}, nil
-}
 
-func (p *Prover) createIcicleBackendProver(
-	params encoding.EncodingParams, fs *fft.FFTSettings,
-) (*ParametrizedProver, error) {
-	return CreateIcicleBackendProver(p, params, fs)
 }
 
 func (g *Prover) preloadProversFromSRSTableCache() error {

encoding/v2/kzg/prover/backend/gnark/multiframe_proof.go
@@ -13,7 +13,7 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-type KzgMultiProofGnarkBackend struct {
+type KzgMultiProofBackend struct {
 	Logger logging.Logger
 	Fs     *fft.FFTSettings
 	// FFTPointsT contains the transposed SRSTable points, of size [2*dimE][l]=[2*numChunks][chunkLen].
@@ -21,7 +21,16 @@ type KzgMultiProofGnarkBackend struct {
 	//   "Note that the vector multiplied by the matrix is independent from the polynomial coefficients,
 	//   so its Fourier transform can be precomputed"
 	FFTPointsT [][]bn254.G1Affine
-	SFs        *fft.FFTSettings
+}
+
+func NewMultiProofBackend(
+	logger logging.Logger, fs *fft.FFTSettings, fftPointsT [][]bn254.G1Affine,
+) *KzgMultiProofBackend {
+	return &KzgMultiProofBackend{
+		Logger:     logger,
+		Fs:         fs,
+		FFTPointsT: fftPointsT,
+	}
 }
 
 // Computes a KZG multi-reveal proof for chunks containing in each frame.
@@ -37,7 +46,7 @@ type KzgMultiProofGnarkBackend struct {
 // 1. https://dankradfeist.de/ethereum/2020/06/16/kate-polynomial-commitments.html (single multiproof theory)
 // 2. https://eprint.iacr.org/2023/033.pdf (how to compute the single multiproof fast)
 // 3. https://github.com/khovratovich/Kate/blob/master/Kate_amortized.pdf (fast multiple multiproofs)
-func (p *KzgMultiProofGnarkBackend) ComputeMultiFrameProofV2(
+func (p *KzgMultiProofBackend) ComputeMultiFrameProofV2(
 	polyFr []fr.Element, numChunks, chunkLen, numWorker uint64,
 ) ([]bn254.G1Affine, error) {
 	// We describe the steps in the computation by following section 2.2 of
@@ -112,7 +121,7 @@ func (p *KzgMultiProofGnarkBackend) ComputeMultiFrameProofV2(
 
 // Helper function to handle coefficient computation.
 // Returns a [2*dimE][l] slice.
-func (p *KzgMultiProofGnarkBackend) computeCoeffStore(
+func (p *KzgMultiProofBackend) computeCoeffStore(
 	polyFr []fr.Element, numWorker, l, dimE uint64,
 ) ([][]fr.Element, error) {
 	coeffStore := make([][]fr.Element, dimE*2)
@@ -152,7 +161,7 @@ func (p *KzgMultiProofGnarkBackend) computeCoeffStore(
 //
 // TODO(samlaf): better document/explain/refactor/rename this function,
 // to explain how it fits into the overall scheme.
-func (p *KzgMultiProofGnarkBackend) getSlicesCoeff(polyFr []fr.Element, dimE, j, l uint64) ([]fr.Element, error) {
+func (p *KzgMultiProofBackend) getSlicesCoeff(polyFr []fr.Element, dimE, j, l uint64) ([]fr.Element, error) {
 	toeplitzExtendedVec := make([]fr.Element, 2*dimE)
 
 	m := uint64(len(polyFr)) - 1 // there is a constant term

encoding/v2/kzg/prover/backend/icicle/noicicle.go
@@ -1,0 +1,32 @@
+//go:build !icicle
+
+package icicle
+
+import (
+	"errors"
+
+	"github.com/Layr-Labs/eigenda/encoding/v2/fft"
+	"github.com/Layr-Labs/eigensdk-go/logging"
+	"github.com/consensys/gnark-crypto/ecc/bn254"
+	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
+)
+
+// KzgMultiProofBackend cannot be constructed without icicle build tag.
+// We still define the struct and methods to satisfy the interface,
+// just to make it clear that this backend could exist but is not available in this build.
+type KzgMultiProofBackend struct{}
+
+func (*KzgMultiProofBackend) ComputeMultiFrameProofV2(
+	blobFr []fr.Element, numChunks, chunkLen, numWorker uint64,
+) ([]bn254.G1Affine, error) {
+	// Not supported
+	return nil, errors.New("icicle backend called without icicle build tag")
+}
+
+func NewMultiProofBackend(logger logging.Logger,
+	fs *fft.FFTSettings, fftPointsT [][]bn254.G1Affine,
+	gpuEnabled bool, numWorker uint64,
+) (*KzgMultiProofBackend, error) {
+	// Not supported
+	return nil, errors.New("icicle backend called without icicle build tag")
+}

encoding/v2/kzg/prover/backend/proof_backend.go
@@ -1,0 +1,25 @@
+// Note that all functions are suffixed with V2 to avoid passing a V1 backend to a V2 prover.
+// The main difference is that the V2 prover requires blobs to be of power-of-two size.
+package backend
+
+import (
+	"github.com/Layr-Labs/eigenda/encoding/v2/kzg/prover/backend/gnark"
+	"github.com/Layr-Labs/eigenda/encoding/v2/kzg/prover/backend/icicle"
+	"github.com/consensys/gnark-crypto/ecc/bn254"
+	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
+)
+
+// Proof device represents a backend capable of computing KZG multiproofs.
+type KzgMultiProofsBackendV2 interface {
+	ComputeMultiFrameProofV2(blobFr []fr.Element, numChunks, chunkLen, numWorker uint64) ([]bn254.G1Affine, error)
+}
+
+// We implement two backends: gnark and icicle.
+//   - Gnark uses the gnark library and is the default CPU-based backend, and is always available.
+//   - Icicle uses the icicle library and can leverage GPU acceleration, but requires building with the icicle tag.
+//     Building with the icicle tag will inject the dynamic libraries required to use icicle.
+//
+// Both backends implement a NewMultiProofBackend constructor, which in the case of icicle
+// will return an error if the icicle build tag was not used.
+var _ KzgMultiProofsBackendV2 = &gnark.KzgMultiProofBackend{}
+var _ KzgMultiProofsBackendV2 = &icicle.KzgMultiProofBackend{}

encoding/v2/kzg/prover/icicle.go
@@ -1,58 +1,0 @@
-//go:build icicle
-
-package prover
-
-import (
-	"sync"
-
-	"github.com/Layr-Labs/eigenda/encoding"
-	"github.com/Layr-Labs/eigenda/encoding/icicle"
-	"github.com/Layr-Labs/eigenda/encoding/v2/fft"
-	icicleprover "github.com/Layr-Labs/eigenda/encoding/v2/kzg/prover/icicle"
-)
-
-const (
-	// MAX_NTT_SIZE is the maximum NTT domain size needed to compute FFTs for the
-	// largest supported blobs. Assuming a coding ratio of 1/8 and symbol size of 32 bytes:
-	// - Encoded size: 2^{MAX_NTT_SIZE} * 32 bytes ≈ 1 GB
-	// - Original blob size: 2^{MAX_NTT_SIZE} * 32 / 8 = 2^{MAX_NTT_SIZE + 2} ≈ 128 MB
-	MAX_NTT_SIZE = 25
-)
-
-func CreateIcicleBackendProver(p *Prover, params encoding.EncodingParams, fs *fft.FFTSettings) (*ParametrizedProver, error) {
-	_, fftPointsT, err := p.setupFFTPoints(params)
-	if err != nil {
-		return nil, err
-	}
-	icicleDevice, err := icicle.NewIcicleDevice(icicle.IcicleDeviceConfig{
-		Logger:     p.logger,
-		GPUEnable:  p.Config.GPUEnable,
-		NTTSize:    MAX_NTT_SIZE,
-		FFTPointsT: fftPointsT,
-		SRSG1:      p.G1SRS[:p.KzgConfig.SRSNumberToLoad],
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	// Set up icicle multiproof backend
-	multiproofBackend := &icicleprover.KzgMultiProofIcicleBackend{
-		Logger:         p.logger,
-		Fs:             fs,
-		FlatFFTPointsT: icicleDevice.FlatFFTPointsT,
-		NttCfg:         icicleDevice.NttCfg,
-		MsmCfg:         icicleDevice.MsmCfg,
-		Device:         icicleDevice.Device,
-		GpuLock:        sync.Mutex{},
-		NumWorker:      p.KzgConfig.NumWorker,
-	}
-
-	return &ParametrizedProver{
-		logger:                     p.logger,
-		srsNumberToLoad:            p.KzgConfig.SRSNumberToLoad,
-		encodingParams:             params,
-		encoder:                    p.encoder,
-		computeMultiproofNumWorker: p.KzgConfig.NumWorker,
-		kzgMultiProofBackend:       multiproofBackend,
-	}, nil
-}

encoding/v2/kzg/prover/icicle/ecntt.go
@@ -1,45 +1,0 @@
-//go:build icicle
-
-package icicle
-
-import (
-	"fmt"
-
-	"github.com/ingonyama-zk/icicle/v3/wrappers/golang/core"
-	iciclebn254 "github.com/ingonyama-zk/icicle/v3/wrappers/golang/curves/bn254"
-	ecntt "github.com/ingonyama-zk/icicle/v3/wrappers/golang/curves/bn254/ecntt"
-	"github.com/ingonyama-zk/icicle/v3/wrappers/golang/runtime"
-)
-
-func (c *KzgMultiProofIcicleBackend) ECNttToGnarkOnDevice(batchPoints core.DeviceSlice, isInverse bool, totalSize int) (core.DeviceSlice, error) {
-	output, err := c.ECNttOnDevice(batchPoints, isInverse, totalSize)
-	if err != nil {
-		return output, err
-	}
-
-	return output, nil
-}
-
-func (c *KzgMultiProofIcicleBackend) ECNttOnDevice(batchPoints core.DeviceSlice, isInverse bool, totalSize int) (core.DeviceSlice, error) {
-	var p iciclebn254.Projective
-	var out core.DeviceSlice
-
-	output, err := out.Malloc(p.Size(), totalSize)
-	if err != runtime.Success {
-		return out, fmt.Errorf("allocating bytes on device failed: %v", err.AsString())
-	}
-
-	if isInverse {
-		err := ecntt.ECNtt(batchPoints, core.KInverse, &c.NttCfg, output)
-		if err != runtime.Success {
-			return out, fmt.Errorf("inverse ecntt failed: %v", err.AsString())
-		}
-	} else {
-		err := ecntt.ECNtt(batchPoints, core.KForward, &c.NttCfg, output)
-		if err != runtime.Success {
-			return out, fmt.Errorf("forward ecntt failed: %v", err.AsString())
-		}
-	}
-
-	return output, nil
-}

encoding/v2/kzg/prover/icicle/msm.go
@@ -1,33 +1,0 @@
-//go:build icicle
-
-package icicle
-
-import (
-	"fmt"
-
-	"github.com/ingonyama-zk/icicle/v3/wrappers/golang/core"
-	iciclebn254 "github.com/ingonyama-zk/icicle/v3/wrappers/golang/curves/bn254"
-	"github.com/ingonyama-zk/icicle/v3/wrappers/golang/curves/bn254/msm"
-	"github.com/ingonyama-zk/icicle/v3/wrappers/golang/runtime"
-)
-
-// MsmBatchOnDevice function supports batch across blobs.
-// totalSize is the number of output points, which equals to numPoly * 2 * dimE , dimE is number of chunks
-func (c *KzgMultiProofIcicleBackend) MsmBatchOnDevice(rowsFrIcicleCopy core.DeviceSlice, rowsG1Icicle []iciclebn254.Affine, totalSize int) (core.DeviceSlice, error) {
-	rowsG1IcicleCopy := core.HostSliceFromElements[iciclebn254.Affine](rowsG1Icicle)
-
-	var p iciclebn254.Projective
-	var out core.DeviceSlice
-
-	_, err := out.Malloc(p.Size(), totalSize)
-	if err != runtime.Success {
-		return out, fmt.Errorf("allocating bytes on device failed: %v", err.AsString())
-	}
-
-	err = msm.Msm(rowsFrIcicleCopy, rowsG1IcicleCopy, &c.MsmCfg, out)
-	if err != runtime.Success {
-		return out, fmt.Errorf("msm error: %v", err.AsString())
-	}
-
-	return out, nil
-}

commit a7e5fbedc5b459238c17b384e4fa6229a37523fa

fix(encoder-v2-icicle): backend initialization bug
Introduced this bug in https://github.com/Layr-Labs/eigenda/pull/2197, when I refactored the icicle backend initialization logic. I thought the g1SRS was not needed, but it turns out it is.

We don't have any CI that runs gpu code, so this was not caught. Caught it while benchmarking on ec2 today. Bug was causing a divide by zero panic during msm, because the SRS table was not properly loaded.

Personally think the icicle backend config and initialization code is full of footguns, so plan to refactor it in a future PR.

encoding/v2/kzg/prover/backend/icicle/multiframe_proof.go
@@ -41,7 +41,7 @@ type KzgMultiProofBackend struct {
 }
 
 func NewMultiProofBackend(logger logging.Logger,
-	fs *fft.FFTSettings, fftPointsT [][]bn254.G1Affine,
+	fs *fft.FFTSettings, fftPointsT [][]bn254.G1Affine, g1SRS []bn254.G1Affine,
 	gpuEnabled bool, numWorker uint64,
 ) (*KzgMultiProofBackend, error) {
 	icicleDevice, err := icicle.NewIcicleDevice(icicle.IcicleDeviceConfig{
@@ -49,6 +49,7 @@ func NewMultiProofBackend(logger logging.Logger,
 		GPUEnable:  gpuEnabled,
 		NTTSize:    MAX_NTT_SIZE,
 		FFTPointsT: fftPointsT,
+		SRSG1:      g1SRS,
 	})
 	if err != nil {
 		return nil, fmt.Errorf("configure icicle device: %w", err)

encoding/v2/kzg/prover/prover.go
@@ -188,7 +188,7 @@ func (p *Prover) newProver(params encoding.EncodingParams) (*ParametrizedProver,
 		multiproofsBackend = gnark.NewMultiProofBackend(p.logger, fs, fftPointsT)
 	case encoding.IcicleBackend:
 		multiproofsBackend, err = icicle.NewMultiProofBackend(
-			p.logger, fs, fftPointsT, p.Config.GPUEnable, p.KzgConfig.NumWorker)
+			p.logger, fs, fftPointsT, p.G1SRS, p.Config.GPUEnable, p.KzgConfig.NumWorker)
 		if err != nil {
 			return nil, fmt.Errorf("create icicle backend prover: %w", err)
 		}