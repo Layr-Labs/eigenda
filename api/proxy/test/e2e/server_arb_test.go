645b00c7ee3ad0b7f6e49197fa43e3de97fd4bd5
ethenotethan <ethen@eigenlabs.org>
2025-10-24 17:29:26 +0700

    feat(proxy): Arb Commitment Type && invalid cert signal - fix header byte test

--- api/proxy/test/e2e/server_arb_test.go
+++ api/proxy/test/e2e/server_arb_test.go
@@ -35,7 +35,7 @@ func TestArbCustomDAGetSupportedHeaderBytesMethod(t *testing.T) {
 	err = rpcClient.Call(&supportedHeaderBytesResult,
 		arbitrum_altda.MethodGetSupportedHeaderBytes)
 	require.NoError(t, err)
-	require.Equal(t, supportedHeaderBytesResult.HeaderBytes[0], commitments.ArbCustomDAHeaderByte)
+	require.Equal(t, supportedHeaderBytesResult.HeaderBytes[0], uint8(commitments.ArbCustomDAHeaderByte))
 
 }
 
