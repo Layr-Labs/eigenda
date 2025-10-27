733d770efcd8bed98876a0933655afa91ae16764
Cody Littley <cody@eigenlabs.org>
2025-10-27 10:08:51 -0500

    Add several utilities for interacting with onchain data

--- core/eth/quorum_scanner.go
+++ core/eth/quorum_scanner.go
@@ -9,12 +9,10 @@ import (
 	"github.com/Layr-Labs/eigenda/core"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	gethcommon "github.com/ethereum/go-ethereum/common"
+	lru "github.com/hashicorp/golang-lru/v2"
 )
 
 // A utility that is capable of producing a list of all registered quorums.
-//
-// This utility is fully thread safe, but it does not cache results. Calling into this utility multiple times
-// with the same reference block number is wasteful.
 type QuorumScanner interface {
 
 	// Get all quorums registered at the given reference block number. Quorums are returned
@@ -24,12 +22,13 @@ type QuorumScanner interface {
 
 var _ QuorumScanner = (*quorumScanner)(nil)
 
+// A standard implementation of the QuorumScanner.
 type quorumScanner struct {
 	// A handle for communicating with the registry coordinator contract.
 	registryCoordinator *regcoordinator.ContractEigenDARegistryCoordinator
 }
 
-// Create a new QuorumScanner instance.
+// Create a new QuorumScanner instance. This instance is thread safe but not cached.
 func NewQuorumScanner(
 	contractBackend bind.ContractBackend,
 	registryCoordinatorAddress gethcommon.Address,
@@ -65,4 +64,46 @@ func (q *quorumScanner) GetQuorums(ctx context.Context, referenceBlockNumber uin
 	}
 
 	return quorums, nil
+}
+
+var _ QuorumScanner = (*cachedQuorumScanner)(nil)
+
+// A cached QuorumScanner implementation.
+type cachedQuorumScanner struct {
+	base  QuorumScanner
+	cache *lru.Cache[uint64, []core.QuorumID]
+}
+
+// Create a new cached QuorumScanner that wraps the given base QuorumScanner. This implementation is thread safe.
+func NewCachedQuorumScanner(base QuorumScanner, cacheSize int) (QuorumScanner, error) {
+	cache, err := lru.New[uint64, []core.QuorumID](cacheSize)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create cache: %w", err)
+	}
+	return &cachedQuorumScanner{
+		base:  base,
+		cache: cache,
+	}, nil
+}
+
+func (c *cachedQuorumScanner) GetQuorums(ctx context.Context, referenceBlockNumber uint64) ([]core.QuorumID, error) {
+	if quorums, ok := c.cache.Get(referenceBlockNumber); ok {
+		return quorums, nil
+	}
+
+	quorums, err := c.base.GetQuorums(ctx, referenceBlockNumber)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get quorums: %w", err)
+	}
+
+	c.cache.Add(referenceBlockNumber, quorums)
+	return quorums, nil
+}
+
+// Convert a list of quorums to a byte slice, where each byte is the ID of a quorum.
+// This is the format expected by many smart contract functions.
+func QuorumListToBytes(quorums []core.QuorumID) []byte {
+	result := make([]byte, len(quorums))
+	copy(result, quorums)
+	return result
 }
