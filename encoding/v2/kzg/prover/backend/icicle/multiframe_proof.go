Layr-Labs/eigenda

commit 7d14a309c68f97f0324316a50e0b45ff9ced9ced

perf(proofs-gpu): 400ms speedup for multiproof generation (#2220)
* refactor(encoder-v2): cleanup parametrized encoder constructor logic

Create backend package to host gnark and icicle implementations in a self-contained.

* fix BenchmarkMultiproofFrameGeneration

* feat: parallelize icicle Fr conversion utils

This speeds up the gpu RS fft by a lot (seeing ~2x speedup on g6.xlarge which only has 2 cpu cores).

* test: add BenchmarkRSBackendIcicle

This isolates the part of chunk generation that is on GPU, and compares cpu vs gpu performance.

* improve benchmark

* perf: ~30x icicle rs extend_poly throughput

* remove trace output from benchmark_eigenda_test.go

* revert icicle utils.go changes

We don't need them with the new implementation. conversion is done on gpu.

* make fmt

* add back gpuLock to ExtendPolyEval

We need a better solution than this but this is currently needed to prevent panic.

* fix benchmark fr lengths

Was benchmarking with too long blobs... so actually achieved speedup will be less than 20x.... my bad.

* bench: make proof benchmark work for icicle

* fix: remove validateInput in GetProofs()

It was doing the wrong check by checking that we have enough SrsNumberToLoad points, which is wrong because we can have loaded the SRS tables directly without having loaded any srs points directly.

Realized that there's some cleanup that needs to be done in the prover config and srs points loading... but should be done in future PR.

* chore: make gnarkAffineToIcicleAffine private

* perf(icicle-multiproof-backend): remove unnecessary fr conversion

Same trick that was applied in https://github.com/Layr-Labs/eigenda/pull/2214. icicle functions are now generic and can work with gnark's montgomery fr.Element points directly, so we save a few hundred ms by not converting them on CPU.

encoding/v2/kzg/prover/backend/icicle/multiframe_proof.go
@@ -30,8 +30,11 @@ const (
 )
 
 type KzgMultiProofBackend struct {
-	Logger         logging.Logger
-	Fs             *fft.FFTSettings
+	Logger logging.Logger
+	Fs     *fft.FFTSettings
+	// TODO(samlaf): we should send the srs table points to the device once in the constructor
+	// and keep a deviceSlice pointer to it. This would require a destructor to free the device memory.
+	// Also need to account how much memory this would use over all parametrized provers.
 	FlatFFTPointsT []iciclebn254.Affine
 	NttCfg         core.NTTConfig[[iciclebn254.SCALAR_LIMBS]uint32]
 	MsmCfg         core.MSMConfig
@@ -88,9 +91,6 @@ func (p *KzgMultiProofBackend) ComputeMultiFrameProofV2(polyFr []fr.Element, num
 	}
 	preprocessDone := time.Now()
 
-	flattenCoeffStoreSf := icicle.ConvertFrToScalarFieldsBytes(flattenCoeffStoreFr)
-	flattenCoeffStoreCopy := core.HostSliceFromElements[iciclebn254.ScalarField](flattenCoeffStoreSf)
-
 	var icicleFFTBatch []bn254.G1Affine
 	var icicleErr error
 
@@ -110,18 +110,11 @@ func (p *KzgMultiProofBackend) ComputeMultiFrameProofV2(polyFr []fr.Element, num
 			}
 		}()
 
-		// Copy the flatten coeff to device
-		var flattenStoreCopyToDevice core.DeviceSlice
-		flattenCoeffStoreCopy.CopyToDevice(&flattenStoreCopyToDevice, true)
-
-		sumVec, err := p.msmBatchOnDevice(flattenStoreCopyToDevice, p.FlatFFTPointsT, int(numPoly)*int(dimE)*2)
+		sumVec, err := p.msmBatchOnDevice(flattenCoeffStoreFr, p.FlatFFTPointsT, int(numPoly)*int(dimE)*2)
 		if err != nil {
 			icicleErr = fmt.Errorf("msm error: %w", err)
 			return
 		}
-
-		// Free the flatten coeff store
-		flattenStoreCopyToDevice.Free()
 
 		msmDone = time.Now()
 
@@ -133,6 +126,7 @@ func (p *KzgMultiProofBackend) ComputeMultiFrameProofV2(polyFr []fr.Element, num
 			return
 		}
 
+		// TODO(samlaf): could we change this to FreeAsync to overlap with the next ECNtt?
 		sumVec.Free()
 
 		firstECNttDone = time.Now()
@@ -270,9 +264,9 @@ func (p *KzgMultiProofBackend) getSlicesCoeff(polyFr []fr.Element, dimE, j, l ui
 }
 
 // MsmBatchOnDevice function supports batch across blobs.
-// totalSize is the number of output points, which equals to numPoly * 2 * dimE , dimE is number of chunks
-func (c *KzgMultiProofBackend) msmBatchOnDevice(rowsFrIcicleCopy core.DeviceSlice, rowsG1Icicle []iciclebn254.Affine, totalSize int) (core.DeviceSlice, error) {
-	rowsG1IcicleCopy := core.HostSliceFromElements[iciclebn254.Affine](rowsG1Icicle)
+// totalSize is the number of output points, which equals to numPoly * 2 * dimE , dimE is number of chunks.
+// Returns a deviceSlice of G1 points, which must be freed by the caller.
+func (c *KzgMultiProofBackend) msmBatchOnDevice(frs []fr.Element, g1Points []iciclebn254.Affine, totalSize int) (core.DeviceSlice, error) {
 
 	var p iciclebn254.Projective
 	var out core.DeviceSlice
@@ -282,7 +276,11 @@ func (c *KzgMultiProofBackend) msmBatchOnDevice(rowsFrIcicleCopy core.DeviceSlic
 		return out, fmt.Errorf("allocating bytes on device failed: %v", err.AsString())
 	}
 
-	err = msm.Msm(rowsFrIcicleCopy, rowsG1IcicleCopy, &c.MsmCfg, out)
+	frsHostSlice := core.HostSliceFromElements(frs)
+	// TODO(samlaf): we could send the srs table points to the device once in the constructor
+	// and keep a deviceSlice pointer to it.
+	g1PointsHostSlice := core.HostSliceFromElements(g1Points)
+	err = msm.Msm(frsHostSlice, g1PointsHostSlice, &c.MsmCfg, out)
 	if err != runtime.Success {
 		return out, fmt.Errorf("msm error: %v", err.AsString())
 	}

encoding/icicle/utils.go
@@ -40,12 +40,12 @@ func ConvertScalarFieldsToFrBytes(scalars []iciclebn254.ScalarField) []fr.Elemen
 func BatchConvertGnarkAffineToIcicleAffine(gAffineList []bn254.G1Affine) []iciclebn254.Affine {
 	icicleAffineList := make([]iciclebn254.Affine, len(gAffineList))
 	for i := 0; i < len(gAffineList); i++ {
-		GnarkAffineToIcicleAffine(&gAffineList[i], &icicleAffineList[i])
+		gnarkAffineToIcicleAffine(&gAffineList[i], &icicleAffineList[i])
 	}
 	return icicleAffineList
 }
 
-func GnarkAffineToIcicleAffine(g1 *bn254.G1Affine, iciAffine *iciclebn254.Affine) {
+func gnarkAffineToIcicleAffine(g1 *bn254.G1Affine, iciAffine *iciclebn254.Affine) {
 	var littleEndBytesX, littleEndBytesY [32]byte
 	fp.LittleEndian.PutElement(&littleEndBytesX, g1.X)
 	fp.LittleEndian.PutElement(&littleEndBytesY, g1.Y)

encoding/v2/bench/benchmark_eigenda_test.go
@@ -151,53 +151,72 @@ func BenchmarkBlobToChunksEncoding(b *testing.B) {
 	}
 }
 
+// TODO(samlaf): maybe move this to benchmark_icicle_test.go file?
+// That file is currently metal only, we should generalize it.
+func BenchmarkMultiproofGenerationIcicle(b *testing.B) {
+	if !icicle.IsAvailable {
+		b.Skip("code compiled without the icicle build tag")
+	}
+	encodingConfig := encoding.Config{
+		NumWorker:   uint64(runtime.NumCPU()),
+		BackendType: encoding.IcicleBackend,
+		GPUEnable:   true,
+	}
+	benchmarkMultiproofGeneration(b, encodingConfig)
+}
+
+func BenchmarkMultiproofGenerationGnark(b *testing.B) {
+	encodingConfig := encoding.Config{
+		NumWorker:   uint64(runtime.NumCPU()),
+		BackendType: encoding.GnarkBackend,
+		GPUEnable:   false,
+	}
+	benchmarkMultiproofGeneration(b, encodingConfig)
+}
+
 // The encoder service on the disperser generates a multiproof for each chunk.
 // This is the most intensive part of the encoding process.
-//
-// This Benchmark is very high-level, since GetFrames does both RS encoding and multiproof generation.
-// TODO(samlaf): refactor Prover to only do multiproof generation and keep RS encoding separate.
 //
 // The benchmark uses a silent logger, but you can switch to a normal logger to see
 // the log lines giving a breakdown of the different proof steps. E.g.:
 // Multiproof Time Decomp total=9.478006875s preproc=33.987083ms msm=1.496717042s fft1=5.912448708s fft2=2.034854042s
 // Where fft1 and fft2 are on G1, and preproc contains an FFT on Fr elements.
-func BenchmarkMultiproofFrameGeneration(b *testing.B) {
+func benchmarkMultiproofGeneration(b *testing.B, encodingConfig encoding.Config) {
 	proverConfig := prover.KzgConfig{
-		SRSNumberToLoad: 1 << 19,
+		// The loaded G1 point is not used because we require the SRSTables to be preloaded for the benchmark.
+		// We don't have enough SRS points in resourcs/srs/g1.point to compute the largest SRSTables anyways.
+		// Note that we can't input 0 here because the prover checks that at least 1 point is loaded.
+		// TODO(samlaf): fix this. We should be able to not load any G1 points if we are preloading the SRSTables.
+		SRSNumberToLoad: 1,
 		G1Path:          "../../../resources/srs/g1.point",
-		// we preload only the needed SRSTables right before b.Loop below.
-		PreloadEncoder: false,
+		// make sure to run `make download_srs_tables` to have the SRSTables available here.
+		PreloadEncoder: true,
 		CacheDir:       "../../../resources/srs/SRSTables",
 		NumWorker:      uint64(runtime.GOMAXPROCS(0)),
 	}
+	b.Log("Reading precomputed SRSTables, this may take a while...")
 	// use a non-silent logger to see the "Multiproof Time Decomp" log lines.
-	p, err := prover.NewProver(common.SilentLogger(), &proverConfig, nil)
+	p, err := prover.NewProver(common.TestLogger(b), &proverConfig, &encodingConfig)
 	require.NoError(b, err)
 
-	// We only have 16MiBs of SRS points. Since we use blob_version=0's 8x coding
-	// ratio, we can encode blobs of size at most 2MiB (2^21 bytes).
-	for _, blobPower := range []uint64{17, 20, 21} {
-		b.Run("Multiproof_size_2^"+fmt.Sprint(blobPower)+"_bytes", func(b *testing.B) {
-			blobSizeBytes := uint64(1) << blobPower
+	rand := random.NewTestRandomNoPrint(1337)
+	maxSizeBlobCoeffs := rand.FrElements(1 << 22)
+
+	for _, blobPowerBytes := range []uint64{17, 20, 21, 24} {
+		b.Run("Multiproof_size_2^"+fmt.Sprint(blobPowerBytes)+"_bytes", func(b *testing.B) {
+			// Reed-Solomon encoding with 8x redundancy: 2^3 = 8
+			rsExtendedBlobPowerBytes := blobPowerBytes + 3
+			rsExtendedBlobPowerFrs := rsExtendedBlobPowerBytes - 5 // 32 bytes per Fr element
+			rsExtendedBlobFrs := uint64(1) << rsExtendedBlobPowerFrs
 			params := encoding.EncodingParams{
-				NumChunks:   8192,                            // blob_version=0
-				ChunkLength: max(1, blobSizeBytes*8/8192/32), // chosen such that numChunks*ChunkLength=blobSize
+				NumChunks:   8192,                           // blob_version=0
+				ChunkLength: max(1, rsExtendedBlobFrs/8192), // chosen such that numChunks*ChunkLength=rsExtendedBlobFrs
 			}
-
-			rand := random.NewTestRandomNoPrint(1337)
-			blobBytes := rand.Bytes(int(blobSizeBytes))
-			for i := 0; i < len(blobBytes); i += 32 {
-				blobBytes[i] = 0 // to make them Fr elements
-			}
-
-			// Run this before entering the benchmark loop to preload the SRSTable for the params size.
-			b.Log("Generating frames for one blob to preload SRSTable...")
-			_, _, err = p.GetFrames(blobBytes, params)
+			parametrizedProver, err := p.GetKzgProver(params)
 			require.NoError(b, err)
-			b.Log("Preloading done, starting benchmark...")
 
 			for b.Loop() {
-				_, _, err = p.GetFrames(blobBytes, params)
+				_, err = parametrizedProver.GetProofs(maxSizeBlobCoeffs[:rsExtendedBlobFrs])
 				require.NoError(b, err)
 			}
 		})

encoding/v2/kzg/prover/parametrized_prover.go
@@ -21,10 +21,6 @@ type ParametrizedProver struct {
 }
 
 func (g *ParametrizedProver) GetProofs(inputFr []fr.Element) ([]encoding.Proof, error) {
-	if err := g.validateInput(inputFr); err != nil {
-		return nil, err
-	}
-
 	// pad inputFr to NumEvaluations(), which encodes the RS redundancy
 	paddedCoeffs := make([]fr.Element, g.encodingParams.NumEvaluations())
 	copy(paddedCoeffs, inputFr)
@@ -35,13 +31,4 @@ func (g *ParametrizedProver) GetProofs(inputFr []fr.Element) ([]encoding.Proof, 
 		return nil, fmt.Errorf("compute multi frame proof: %w", err)
 	}
 	return proofs, nil
-}
-
-func (g *ParametrizedProver) validateInput(inputFr []fr.Element) error {
-	if len(inputFr) > int(g.srsNumberToLoad) {
-		return fmt.Errorf("poly Coeff length %v is greater than Loaded SRS points %v",
-			len(inputFr), int(g.srsNumberToLoad))
-	}
-
-	return nil
 }

commit 42d2dcf4954b115c596ec6ec562525d8c090dfe6

fix(icicle-prover): fix msm bug
Bug was introduced in https://github.com/Layr-Labs/eigenda/pull/2220. We passed montgomery fr points to the msm, but forgot to set the config to indicate this.

encoding/v2/kzg/prover/backend/icicle/multiframe_proof.go
@@ -37,7 +37,6 @@ type KzgMultiProofBackend struct {
 	// Also need to account how much memory this would use over all parametrized provers.
 	FlatFFTPointsT []iciclebn254.Affine
 	NttCfg         core.NTTConfig[[iciclebn254.SCALAR_LIMBS]uint32]
-	MsmCfg         core.MSMConfig
 	Device         runtime.Device
 	GpuLock        sync.Mutex
 	NumWorker      uint64
@@ -64,7 +63,6 @@ func NewMultiProofBackend(logger logging.Logger,
 		Fs:             fs,
 		FlatFFTPointsT: icicleDevice.FlatFFTPointsT,
 		NttCfg:         icicleDevice.NttCfg,
-		MsmCfg:         icicleDevice.MsmCfg,
 		Device:         icicleDevice.Device,
 		GpuLock:        sync.Mutex{},
 		NumWorker:      numWorker,
@@ -276,11 +274,16 @@ func (c *KzgMultiProofBackend) msmBatchOnDevice(frs []fr.Element, g1Points []ici
 		return out, fmt.Errorf("allocating bytes on device failed: %v", err.AsString())
 	}
 
-	frsHostSlice := core.HostSliceFromElements(frs)
+	// The msm is computed synchronously (default config value is async=false).
+	// We could possibly share the same stream as the ecntt use (c.NttCfg.StreamHandle).
+	// TODO(samlaf): rethink how we use streams and async computations in general.
+	msmCfg := msm.GetDefaultMSMConfig()
+	msmCfg.AreScalarsMontgomeryForm = true
+	frsHostOrDeviceSlice := core.HostSliceFromElements(frs)
 	// TODO(samlaf): we could send the srs table points to the device once in the constructor
 	// and keep a deviceSlice pointer to it.
 	g1PointsHostSlice := core.HostSliceFromElements(g1Points)
-	err = msm.Msm(frsHostSlice, g1PointsHostSlice, &c.MsmCfg, out)
+	err = msm.Msm(frsHostOrDeviceSlice, g1PointsHostSlice, &msmCfg, out)
 	if err != runtime.Success {
 		return out, fmt.Errorf("msm error: %v", err.AsString())
 	}