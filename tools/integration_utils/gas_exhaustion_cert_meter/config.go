f12653f2da88071348180272a80326318f2e0f6b
ethenotethan <42627790+ethenotethan@users.noreply.github.com>
2025-11-06 04:25:58 +0700

    fix(contracts|offchain_ingestion): remove usage of CertVerifier from DA Directory

--- tools/integration_utils/gas_exhaustion_cert_meter/config.go
+++ tools/integration_utils/gas_exhaustion_cert_meter/config.go
@@ -57,6 +57,7 @@ func GetAddressByName(
 func ReadConfig(ctx *cli.Context, logger logging.Logger) (*Config, error) {
 
 	rpcURL := ctx.String(flags.EthRpcUrlFlag.Name)
+	v3CertVerifierAddr := gethcommon.HexToAddress(ctx.String(flags.CertVerifierAddrFlag.Name))
 	ethContext := context.Background()
 	client, err := ethclient.DialContext(ethContext, rpcURL)
 	if err != nil {
@@ -83,11 +84,6 @@ func ReadConfig(ctx *cli.Context, logger logging.Logger) (*Config, error) {
 	}
 
 	registryCoordinatorAddr, err := GetAddressByName(ethContext, client, directoryAddress, "REGISTRY_COORDINATOR")
-	if err != nil {
-		return nil, err
-	}
-
-	v3CertVerifierAddr, err := GetAddressByName(ethContext, client, directoryAddress, "CERT_VERIFIER")
 	if err != nil {
 		return nil, err
 	}
