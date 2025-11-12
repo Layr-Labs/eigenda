a5676b22ba283abf07e3d49c0b32d892e82a3979
iquidus <iquidus@users.noreply.github.com>
2025-11-12 09:28:37 -0500

    Revert "feat(contracts): Add recency window to cert verifier (#2270)" (#2302)
    This reverts commit 9e0c3dbc7de8c717aa5cf7946dda8c584101b005.

--- contracts/src/integrations/cert/EigenDACertVerifier.sol
+++ contracts/src/integrations/cert/EigenDACertVerifier.sol
@@ -42,10 +42,9 @@ contract EigenDACertVerifier is
     DATypesV1.SecurityThresholds internal _securityThresholds;
 
     bytes internal _quorumNumbersRequired;
-    uint32 internal _recencyWindow;
 
     uint8 internal constant MAJOR_VERSION = 3;
-    uint8 internal constant MINOR_VERSION = 2;
+    uint8 internal constant MINOR_VERSION = 1;
     uint8 internal constant PATCH_VERSION = 0;
 
     /// @notice Status codes for certificate verification results
@@ -69,8 +68,7 @@ contract EigenDACertVerifier is
         IEigenDAThresholdRegistry initEigenDAThresholdRegistry,
         IEigenDASignatureVerifier initEigenDASignatureVerifier,
         DATypesV1.SecurityThresholds memory initSecurityThresholds,
-        bytes memory initQuorumNumbersRequired,
-        uint32 initRecencyWindow
+        bytes memory initQuorumNumbersRequired
     ) {
         if (initSecurityThresholds.confirmationThreshold <= initSecurityThresholds.adversaryThreshold) {
             revert InvalidSecurityThresholds();
@@ -82,7 +80,6 @@ contract EigenDACertVerifier is
         _eigenDASignatureVerifier = initEigenDASignatureVerifier;
         _securityThresholds = initSecurityThresholds;
         _quorumNumbersRequired = initQuorumNumbersRequired;
-        _recencyWindow = initRecencyWindow;
     }
 
     /// @notice Decodes a certificate from bytes to an EigenDACertV3
@@ -173,11 +170,6 @@ contract EigenDACertVerifier is
     /// @inheritdoc IEigenDACertVerifier
     function quorumNumbersRequired() external view returns (bytes memory) {
         return _quorumNumbersRequired;
-    }
-
-    /// @inheritdoc IEigenDACertVerifier
-    function recencyWindow() external view returns (uint32) {
-        return _recencyWindow;
     }
 
     /// @inheritdoc IVersionedEigenDACertVerifier
