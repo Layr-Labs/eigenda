ab506b348c7ab0a2f5f78bba762f8efd24df388c
litt3 <102969658+litt3@users.noreply.github.com>
2025-11-26 15:31:31 -0500

    Create DisperserRegistry interface and initial impl
    Signed-off-by: litt3 <102969658+litt3@users.noreply.github.com>

---
+++ common/disperser/disperser_registry.go
@@ -0,0 +1,13 @@
+package disperser
+
+import "context"
+
+// DisperserRegistry provides access to disperser information from the DisperserRegistry contract.
+type DisperserRegistry interface {
+	// Returns the set of dispersers that network participants should interact with by default.
+	GetDefaultDispersers(ctx context.Context) (map[uint32]struct{}, error)
+	// Returns the set of dispersers that support on-demand payments
+	GetOnDemandDispersers(ctx context.Context) (map[uint32]struct{}, error)
+	// Returns the gRPC URI for a specific disperser
+	GetDisperserGrpcUri(ctx context.Context, disperserID uint32) (string, error)
+}
