8fab73591b2f5154aa1757445e1e14599eb7d791
Cody Littley <56973212+cody-littley@users.noreply.github.com>
2025-12-04 11:46:30 -0600

    chore: renamed dispatcher to controller (#2387)
    * Renamed dispatcher to controller
    
    * made suggested changes

--- inabox/tests/setup_disperser_harness.go
+++ inabox/tests/setup_disperser_harness.go
@@ -101,7 +101,7 @@ type DisperserHarness struct {
 	// Controller components
 	// TODO: Refactor into a single struct for controller components
 	EncodingManager  *controller.EncodingManager
-	Dispatcher       *controller.Dispatcher
+	Controller       *controller.Controller
 	ControllerServer *server.Server
 }
 
@@ -124,7 +124,7 @@ type EncoderComponents struct {
 // ControllerComponents contains the components created by startController
 type ControllerComponents struct {
 	EncodingManager  *controller.EncodingManager
-	Dispatcher       *controller.Dispatcher
+	Dispatcher       *controller.Controller
 	ControllerServer *server.Server
 	Address          string
 }
@@ -279,7 +279,7 @@ func SetupDisperserHarness(
 		return nil, fmt.Errorf("failed to start controller: %w", err)
 	}
 	harness.EncodingManager = controllerComponents.EncodingManager
-	harness.Dispatcher = controllerComponents.Dispatcher
+	harness.Controller = controllerComponents.Dispatcher
 	harness.ControllerServer = controllerComponents.ControllerServer
 
 	// Start API server goroutine
