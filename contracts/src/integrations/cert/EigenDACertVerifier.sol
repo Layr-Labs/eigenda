f4169ee7e1c0e846ba624b452bebe9b6ef2205d1
Luke Williams <iquidus@users.noreply.github.com>
2025-12-08 14:08:31 -0500

    remove cancelled 4.0.0 TODOs

--- contracts/src/integrations/cert/EigenDACertVerifier.sol
+++ contracts/src/integrations/cert/EigenDACertVerifier.sol
@@ -55,7 +55,6 @@ contract EigenDACertVerifier is
         // The below 4 status codes are kept for backwards compatibility, but are no longer used.
         // We previously had plans to have more granular error codes, but decided this was not necessary,
         // and the only signal useful to offchain is to separate certs into: success, invalid (400), and bugs (500).
-        // TODO(4.0.0): get rid of these
         UNUSED_HISTORICAL_INVALID_INCLUSION_PROOF,
         UNUSED_HISTORICAL_SECURITY_ASSUMPTIONS_NOT_MET,
         UNUSED_HISTORICAL_BLOB_QUORUMS_NOT_SUBSET,
@@ -100,7 +99,6 @@ contract EigenDACertVerifier is
     /// which is also useful for optimistic rollup one step prover contracts.
     /// @dev Make sure to call this at a block number that is > RBN, otherwise this function will
     /// return an INVALID_CERT status code because of a require in the BLSSignatureChecker library that we use.
-    /// TODO(4.0.0): return (uint8, bytes) instead to include the revert reason.
     function checkDACert(bytes calldata abiEncodedCert) external view returns (uint8) {
         CT.EigenDACertV4 memory daCert;
         // We try catch this here because decoding error would appear as a Panic,
