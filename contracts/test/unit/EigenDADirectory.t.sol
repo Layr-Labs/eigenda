5780bb7f1af2bd1fc491c933211e47193178c7a9
ethenotethan <42627790+ethenotethan@users.noreply.github.com>
2025-12-09 23:30:00 +0700

    address llm pr comments

--- contracts/test/unit/EigenDADirectory.t.sol
+++ contracts/test/unit/EigenDADirectory.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.9;
 
 import {Test} from "lib/forge-std/src/Test.sol";
 import {EigenDADirectory} from "src/core/EigenDADirectory.sol";
-import {ConfigRegistryTypes} from "src/core/libraries/v3/config-registry/ConfigRegistryTypes.sol";
+import {AddressDirectoryConstants} from "src/core/libraries/v3/address-directory/AddressDirectoryConstants.sol";
 import {EigenDAAccessControl} from "src/core/EigenDAAccessControl.sol";
 import {IEigenDAAddressDirectory} from "src/core/interfaces/IEigenDADirectory.sol";
 
@@ -14,11 +14,11 @@ contract EigenDADirectoryTest is Test {
     address owner = makeAddr("owner");
     address nonOwner = makeAddr("nonOwner");
 
-    string constant CONFIG_NAME_BLOCKNUMBER = "testConfigBlockNumber";
-    string constant CONFIG_NAME_TIMESTAMP = "testConfigTimestamp";
+    address testAddress = makeAddr("testAddr");
+    string testNamedKey = "testNamedKey";
 
     function setUp() public {
-        // Deploy access control with owner
+        // Deploy AccessControl with owner
         accessControl = new EigenDAAccessControl(owner);
 
         // Deploy and initialize DA Directory
@@ -30,192 +30,165 @@ contract EigenDADirectoryTest is Test {
     // Address Directory: Basic Operations
     // ===========================
 
-    function test_addAddress_success() public {
-        address testAddress = address(0x1234);
-        string memory testName = "testAddress";
+    function test_initialize() public {
+        string[] memory names = directory.getAllNames();
+        assertNotEq(directory.getAddress(AddressDirectoryConstants.ACCESS_CONTROL_NAME), address(0x0), "AccessControl contract should have entry");
+        assertEq(names.length, 1, "Should have one name (AccessControl) after initialization");
 
+        vm.expectRevert("AlreadyInitialized()");
+        directory.initialize(address(0));
+    }
+
+    function test_addAddress_success() public {
         vm.prank(owner);
         vm.expectEmit(true, true, true, true);
-        emit IEigenDAAddressDirectory.AddressAdded(testName, keccak256(abi.encodePacked(testName)), testAddress);
-        directory.addAddress(testName, testAddress);
+        emit IEigenDAAddressDirectory.AddressAdded(testNamedKey, keccak256(abi.encodePacked(testNamedKey)), testAddress);
+        directory.addAddress(testNamedKey, testAddress);
 
-        assertEq(directory.getAddress(testName), testAddress, "Address should be set correctly");
+        assertEq(directory.getAddress(testNamedKey), testAddress, "Address should be set correctly");
     }
 
     function test_addAddress_revertZeroAddress() public {
-        string memory testName = "testAddress";
-
         vm.prank(owner);
         vm.expectRevert(IEigenDAAddressDirectory.ZeroAddress.selector);
-        directory.addAddress(testName, address(0));
+        directory.addAddress(testNamedKey, address(0));
     }
 
     function test_addAddress_revertAlreadyExists() public {
-        address testAddress = address(0x1234);
-        string memory testName = "testAddress";
-
         vm.startPrank(owner);
-        directory.addAddress(testName, testAddress);
+        directory.addAddress(testNamedKey, testAddress);
 
-        vm.expectRevert(abi.encodeWithSelector(IEigenDAAddressDirectory.AddressAlreadyExists.selector, testName));
-        directory.addAddress(testName, address(0x5678));
+        vm.expectRevert(abi.encodeWithSelector(IEigenDAAddressDirectory.AddressAlreadyExists.selector, testNamedKey));
+        directory.addAddress(testNamedKey, address(0x5678));
         vm.stopPrank();
     }
 
     function test_addAddress_revertNonOwner() public {
-        address testAddress = address(0x1234);
-        string memory testName = "testAddress";
-
         vm.prank(nonOwner);
         vm.expectRevert("Caller is not the owner");
-        directory.addAddress(testName, testAddress);
+        directory.addAddress(testNamedKey, testAddress);
     }
 
     function test_replaceAddress_success() public {
         address oldAddress = address(0x1234);
         address newAddress = address(0x5678);
-        string memory testName = "testAddress";
 
         vm.startPrank(owner);
-        directory.addAddress(testName, oldAddress);
+        directory.addAddress(testNamedKey, oldAddress);
 
         vm.expectEmit(true, true, true, true);
         emit IEigenDAAddressDirectory.AddressReplaced(
-            testName, keccak256(abi.encodePacked(testName)), oldAddress, newAddress
+            testNamedKey, keccak256(abi.encodePacked(testNamedKey)), oldAddress, newAddress
         );
-        directory.replaceAddress(testName, newAddress);
+        directory.replaceAddress(testNamedKey, newAddress);
         vm.stopPrank();
 
-        assertEq(directory.getAddress(testName), newAddress, "Address should be replaced");
+        assertEq(directory.getAddress(testNamedKey), newAddress, "Address should be replaced");
     }
 
     function test_replaceAddress_revertDoesNotExist() public {
-        string memory testName = "nonexistentAddress";
         address newAddress = address(0x5678);
 
         vm.prank(owner);
-        vm.expectRevert(abi.encodeWithSelector(IEigenDAAddressDirectory.AddressDoesNotExist.selector, testName));
-        directory.replaceAddress(testName, newAddress);
+        vm.expectRevert(abi.encodeWithSelector(IEigenDAAddressDirectory.AddressDoesNotExist.selector, testNamedKey));
+        directory.replaceAddress(testNamedKey, newAddress);
     }
 
     function test_replaceAddress_revertZeroAddress() public {
         address oldAddress = address(0x1234);
-        string memory testName = "testAddress";
 
         vm.startPrank(owner);
-        directory.addAddress(testName, oldAddress);
+        directory.addAddress(testNamedKey, oldAddress);
 
         vm.expectRevert(IEigenDAAddressDirectory.ZeroAddress.selector);
-        directory.replaceAddress(testName, address(0));
+        directory.replaceAddress(testNamedKey, address(0));
         vm.stopPrank();
     }
 
     function test_replaceAddress_revertSameValue() public {
-        address testAddress = address(0x1234);
-        string memory testName = "testAddress";
-
         vm.startPrank(owner);
-        directory.addAddress(testName, testAddress);
+        directory.addAddress(testNamedKey, testAddress);
 
         vm.expectRevert(abi.encodeWithSelector(IEigenDAAddressDirectory.NewValueIsOldValue.selector, testAddress));
-        directory.replaceAddress(testName, testAddress);
+        directory.replaceAddress(testNamedKey, testAddress);
         vm.stopPrank();
     }
 
     function test_replaceAddress_revertNonOwner() public {
         address oldAddress = address(0x1234);
         address newAddress = address(0x5678);
-        string memory testName = "testAddress";
 
         vm.prank(owner);
-        directory.addAddress(testName, oldAddress);
+        directory.addAddress(testNamedKey, oldAddress);
 
         vm.prank(nonOwner);
         vm.expectRevert("Caller is not the owner");
-        directory.replaceAddress(testName, newAddress);
+        directory.replaceAddress(testNamedKey, newAddress);
     }
 
     function test_removeAddress_success() public {
-        address testAddress = address(0x1234);
-        string memory testName = "testAddress";
 
         vm.startPrank(owner);
-        directory.addAddress(testName, testAddress);
+        directory.addAddress(testNamedKey, testAddress);
 
         vm.expectEmit(true, true, true, true);
-        emit IEigenDAAddressDirectory.AddressRemoved(testName, keccak256(abi.encodePacked(testName)));
-        directory.removeAddress(testName);
+        emit IEigenDAAddressDirectory.AddressRemoved(testNamedKey, keccak256(abi.encodePacked(testNamedKey)));
+        directory.removeAddress(testNamedKey);
         vm.stopPrank();
 
-        assertEq(directory.getAddress(testName), address(0), "Address should be removed");
+        assertEq(directory.getAddress(testNamedKey), address(0), "Address should be removed");
     }
 
     function test_removeAddress_revertDoesNotExist() public {
-        string memory testName = "nonexistentAddress";
-
         vm.prank(owner);
-        vm.expectRevert(abi.encodeWithSelector(IEigenDAAddressDirectory.AddressDoesNotExist.selector, testName));
-        directory.removeAddress(testName);
+        vm.expectRevert(abi.encodeWithSelector(IEigenDAAddressDirectory.AddressDoesNotExist.selector, testNamedKey));
+        directory.removeAddress(testNamedKey);
     }
 
     function test_removeAddress_revertNonOwner() public {
-        address testAddress = address(0x1234);
-        string memory testName = "testAddress";
-
         vm.prank(owner);
-        directory.addAddress(testName, testAddress);
+        directory.addAddress(testNamedKey, testAddress);
 
         vm.prank(nonOwner);
         vm.expectRevert("Caller is not the owner");
-        directory.removeAddress(testName);
+        directory.removeAddress(testNamedKey);
     }
 
     function test_getAddress_byString() public {
-        address testAddress = address(0x1234);
-        string memory testName = "testAddress";
-
         vm.prank(owner);
-        directory.addAddress(testName, testAddress);
+        directory.addAddress(testNamedKey, testAddress);
 
-        assertEq(directory.getAddress(testName), testAddress, "Should retrieve address by name");
+        assertEq(directory.getAddress(testNamedKey), testAddress, "Should retrieve address by name");
     }
 
     function test_getAddress_byBytes32() public {
-        address testAddress = address(0x1234);
-        string memory testName = "testAddress";
-        bytes32 nameDigest = keccak256(abi.encodePacked(testName));
+        address localTestAddress = address(0x1234);
+        string memory localTestKeyName = "testAddress";
+        bytes32 nameDigest = keccak256(abi.encodePacked(localTestKeyName));
 
         vm.prank(owner);
-        directory.addAddress(testName, testAddress);
+        directory.addAddress(localTestKeyName, localTestAddress);
 
-        assertEq(directory.getAddress(nameDigest), testAddress, "Should retrieve address by digest");
+        assertEq(directory.getAddress(nameDigest), localTestAddress, "Should retrieve address by digest");
     }
 
     function test_getAddress_nonexistent() public view {
-        string memory testName = "nonexistentAddress";
-        assertEq(directory.getAddress(testName), address(0), "Should return zero address for nonexistent name");
+        string memory unknownTestNameKey = "nonexistentAddress";
+        assertEq(directory.getAddress(unknownTestNameKey), address(0), "Should return zero address for nonexistent name");
     }
 
     function test_getName_success() public {
-        address testAddress = address(0x1234);
-        string memory testName = "testAddress";
-        bytes32 nameDigest = keccak256(abi.encodePacked(testName));
+        bytes32 nameDigest = keccak256(abi.encodePacked(testNamedKey));
 
         vm.prank(owner);
-        directory.addAddress(testName, testAddress);
+        directory.addAddress(testNamedKey, testAddress);
 
-        assertEq(directory.getName(nameDigest), testName, "Should retrieve name by digest");
+        assertEq(directory.getName(nameDigest), testNamedKey, "Should retrieve name by digest");
     }
 
     function test_getName_nonexistent() public view {
         bytes32 nonexistentDigest = keccak256(abi.encodePacked("nonexistent"));
         assertEq(directory.getName(nonexistentDigest), "", "Should return empty string for nonexistent digest");
-    }
-
-    function test_getAllNames_empty() public view {
-        string[] memory names = directory.getAllNames();
-        // Note: AccessControl is registered during initialization, so we expect 1 name
-        assertEq(names.length, 1, "Should have one name (AccessControl) after initialization");
     }
 
     function test_getAllNames_multipleAddresses() public {
@@ -253,7 +226,7 @@ contract EigenDADirectoryTest is Test {
         vm.stopPrank();
 
         string[] memory names = directory.getAllNames();
-        assertEq(names.length, 3, "Should have 3 names after removal (address1, address3, access control)");
+        assertEq(names.length, 3, "Should have 3 names after removal (address1, address3, AccessControl)");
 
         // Verify address2 is not present
         for (uint256 i = 0; i < names.length; i++) {
@@ -266,31 +239,26 @@ contract EigenDADirectoryTest is Test {
     // ===========================
 
     function test_addAndReplace_multipleTimes() public {
-        string memory testName = "testAddress";
-
         vm.startPrank(owner);
-        directory.addAddress(testName, address(0x1));
-        assertEq(directory.getAddress(testName), address(0x1), "First address should be set");
+        directory.addAddress(testNamedKey, address(0x1));
+        assertEq(directory.getAddress(testNamedKey), address(0x1), "First address should be set");
 
-        directory.replaceAddress(testName, address(0x2));
-        assertEq(directory.getAddress(testName), address(0x2), "Second address should be set");
+        directory.replaceAddress(testNamedKey, address(0x2));
+        assertEq(directory.getAddress(testNamedKey), address(0x2), "Second address should be set");
 
-        directory.replaceAddress(testName, address(0x3));
-        assertEq(directory.getAddress(testName), address(0x3), "Third address should be set");
+        directory.replaceAddress(testNamedKey, address(0x3));
+        assertEq(directory.getAddress(testNamedKey), address(0x3), "Third address should be set");
         vm.stopPrank();
     }
 
     function test_removeAndReAdd() public {
-        address testAddress = address(0x1234);
-        string memory testName = "testAddress";
-
         vm.startPrank(owner);
-        directory.addAddress(testName, testAddress);
-        directory.removeAddress(testName);
+        directory.addAddress(testNamedKey, testAddress);
+        directory.removeAddress(testNamedKey);
 
         // Should be able to add again after removal
-        directory.addAddress(testName, testAddress);
-        assertEq(directory.getAddress(testName), testAddress, "Should be able to re-add after removal");
+        directory.addAddress(testNamedKey, testAddress);
+        assertEq(directory.getAddress(testNamedKey), testAddress, "Should be able to re-add after removal");
         vm.stopPrank();
     }
 }
