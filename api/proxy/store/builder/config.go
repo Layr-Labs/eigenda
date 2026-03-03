c17c3fb174a6ad85e2593a8f996d068ef2cb2850
bxue-l2 <bxue@eigenlabs.org>
2026-03-03 10:52:19 -0800

    add linear backoff on putRetry for non-grpc error

--- api/proxy/store/builder/config.go
+++ api/proxy/store/builder/config.go
@@ -32,6 +32,9 @@ type Config struct {
 	// eth rpc retry count and delay
 	RetryCount int
 	RetryDelay time.Duration
+
+	// PutRetryDelay is the base time unit for linear backoff on blob dispersal retries.
+	PutRetryDelay time.Duration
 }
 
 // ReadConfig ... parses the Config from the provided flags or environment variables.
@@ -75,8 +78,9 @@ func ReadConfig(ctx *cli.Context) (Config, error) {
 		MemstoreConfig:  memstoreConfig,
 		MemstoreEnabled: ctx.Bool(memstore.EnabledFlagName),
 		S3Config:        s3.ReadConfig(ctx),
-		RetryCount:      ctx.Int(eigendaflags_v2.EthRPCRetryCountFlagName),
-		RetryDelay:      ctx.Duration(eigendaflags_v2.EthRPCRetryDelayIncrementFlagName),
+		RetryCount:    ctx.Int(eigendaflags_v2.EthRPCRetryCountFlagName),
+		RetryDelay:    ctx.Duration(eigendaflags_v2.EthRPCRetryDelayIncrementFlagName),
+		PutRetryDelay: ctx.Duration(eigendaflags_v2.PutRetryDelayIncrementFlagName),
 	}
 
 	return cfg, nil
